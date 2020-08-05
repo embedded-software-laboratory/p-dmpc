9891d5beda39e7f123092801f45a8480f85ec2ff
Julius Kahle <julius.kahle@rwth-aachen.de>
2020-08-05 19:18:53 +0200

    correct a star node selection

--- graph_search_algorithms/get_next_node_astar.m
+++ graph_search_algorithms/get_next_node_astar.m
@@ -2,6 +2,7 @@ function node = get_next_node_astar(search_graph, leaf_nodes)
 
     % Advance tree expansion to next node by choosing closest leaf to
     % target 
+    
     if (length(leaf_nodes) > 1)
 
         curr_values = search_graph.Nodes{leaf_nodes(1), 2};
@@ -10,7 +11,7 @@ function node = get_next_node_astar(search_graph, leaf_nodes)
 
         for i = 2:length(leaf_nodes)
             
-            curr_values = search_graph.Nodes{leaf_nodes(1), 2};
+            curr_values = search_graph.Nodes{leaf_nodes(i), 2};
             curr_value = sum(curr_values);
 
             if min_value > curr_value
