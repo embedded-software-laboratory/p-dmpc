0ea47ad58f9587ec6ee41913d2abe49791e50a9e
pkindermann <philip.kindermann@rwth-aachen.de>
2020-09-06 14:43:24 +0200

    Path array from numerical to cell

--- trajectory/path_between.m
+++ trajectory/path_between.m
@@ -1,25 +1,17 @@
-function search_paths = return_path(search_tree, motion_graph)
-%RETURN_PATH returns the path to the closest node
-    
+function search_path = path_between(cur_node,next_node, search_tree, motion_graph)
+%PATH_BETWEEN Return path as a cell array between two nodes
+
     n_veh = length(search_tree.Node{1, 1}.trims);
-    
-    end_node = search_tree.nnodes();
-    
-    path = findpath(search_tree, 1, end_node);
-    
-    path_length = length(path);
-    
+    search_path = cell(1, n_veh);
+        
     for i = 1:n_veh
-    
-        for j = 1 : (path_length - 1)
-            
-            cur_node = search_tree.get(path(j));
-            next_node  = search_tree.get(path(j + 1));
-            
+                         
             cur_trim = cur_node.trims(i);
             next_trim = next_node.trims(i);
             
             maneuver = motion_graph.motionGraphList(i).maneuvers{cur_trim, next_trim};
+            
+            assert(~isempty(maneuver),'manuevers{%d, %d} is empty.',cur_trim, next_trim);
             
             x = cur_node.xs(i);
             y = cur_node.ys(i);
@@ -36,18 +28,10 @@ function search_paths = return_path(search_tree, motion_graph)
             
             xs = xs.';
             ys = ys.';
-            
-            start_index = 1 + (j - 1)*length_maneuver;
-            end_index = start_index - 1 + length_maneuver;
-            
-            search_path(start_index:end_index,:) = [xs, ys, yaws, trims];
 
-        end
-        
-        search_paths(:,:,i) = search_path;
-        
+            search_path(i) = {[xs, ys, yaws, trims]};
+
     end
 
 end
-
 
