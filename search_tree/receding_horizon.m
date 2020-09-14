d425f0730a98e9f1d89115b969264bad2d28e0a3
pkindermann <philip.kindermann@rwth-aachen.de>
2020-09-14 15:47:46 +0200

    Added is_goal check for search_window

--- search_tree/receding_horizon.m
+++ search_tree/receding_horizon.m
@@ -3,7 +3,7 @@ function [search_tree, parents] = receding_horizon(init_poses, target_poses, tri
 
 % Initial search
 [search_window, leaf_nodes] = generate_tree(init_poses, target_poses, trim_indices, combined_graph, search_depth, is_collisionF, graph_searchF);
-node_id = get_next_node_weighted_astar(search_window, leaf_nodes);
+node_id = graph_searchF(search_window, leaf_nodes);
 
 % Construct resulting tree
 search_tree = tree(search_window.Node{1});
@@ -27,13 +27,28 @@ search_tree = search_tree.addnode(cur_depth, search_window.Node{next_node_id});
 cur_depth = cur_depth + 1;
 
 % Check if the vehicle reached the destination
-offset = [2 2];
+offset = 2 * ones(1, n_veh);
 is_goals = is_goal(poses, target_poses, offset);
 while(sum(is_goals) ~= n_veh)
     
     % Continue receding horizon search
     [search_window, leaf_nodes] = generate_tree(poses, target_poses, trims, combined_graph, search_depth, is_collisionF, graph_searchF);
     node_id = get_next_node_weighted_astar(search_window, leaf_nodes);
+    
+    % Check if we already reached our destination
+    final_node = search_window.Node{end};
+    for i = 1:n_veh
+        poses(i).x = final_node.xs(i);
+        poses(i).y = final_node.ys(i);
+        poses(i).yaw = final_node.yaws(i);
+    end
+    
+    is_goals = is_goal(poses, target_poses, offset); 
+    if(sum(is_goals) == n_veh)
+        length_tree = length(search_tree.Node);
+        search_tree = search_tree.graft(length_tree, search_window);
+        break;
+    end
     
     % Determine next node
     search_path = findpath(search_window, 1, node_id);
