121f887229bb9d47150cd16da731751c536428dc
pkindermann <philip.kindermann@rwth-aachen.de>
2020-11-12 13:39:55 +0100

    Improved run time slightly

--- graph_search_algorithms/get_next_node.m
+++ graph_search_algorithms/get_next_node.m
@@ -1,12 +1,12 @@
 function id = get_next_node(search_tree, leaf_nodes)    
 
     if (length(leaf_nodes) > 1)
-        cur_node = search_tree.get(leaf_nodes(1));
+        cur_node = search_tree.Node{leaf_nodes(1)};
         min_value = sum(cur_node.h_values);
         id = leaf_nodes(1);
         
         for i = 2:length(leaf_nodes)
-            cur_node = search_tree.get(leaf_nodes(i));
+            cur_node = search_tree.Node{leaf_nodes(i)};
             cur_value = sum(cur_node.h_values);
             
             if min_value > cur_value
