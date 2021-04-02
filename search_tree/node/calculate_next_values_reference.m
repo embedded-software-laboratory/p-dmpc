f5ecf3ac17d5cb832eac8e84ba9cae344a90a1b1
Patrick <patrick.scheffe@gmail.com>
2021-04-03 00:00:46 +0200

    trajectory tracking works

--- search_tree/node/calculate_next_values_reference.m
+++ search_tree/node/calculate_next_values_reference.m
@@ -1,23 +1,38 @@
 function [g_values, h_values] = calculate_next_values_reference(iter, cur_node, situation_costs, init_poses, target_poses, next_node, motion_graph)
     g_values = cur_node.g_values;
     % iter.reference is of size (scenario.Hp,2,scenario.nVeh)
-    x_ref = squeeze(iter.referenceTrajectoryPoints(next_node.depth,1,:))';
-    y_ref = squeeze(iter.referenceTrajectoryPoints(next_node.depth,2,:))';
+    x_ref_now = squeeze(iter.referenceTrajectoryPoints(next_node.depth,1,:))';
+    y_ref_now = squeeze(iter.referenceTrajectoryPoints(next_node.depth,2,:))';
     % Distance to reference trajectory points squared
-    d = sum((next_node.xs-x_ref).^2 + (next_node.ys-y_ref).^2);
-    g_values = g_values + d;
-    h_values = (1 + (next_node.depth == 14)) * cost_to_go(situation_costs, init_poses, target_poses, next_node, motion_graph);
-    % TODO adjust ctg
+%     d = sum((next_node.xs-x_ref_now).^2 + (next_node.ys-y_ref_now).^2);
+    % TODO multiple vehicles
+    
     nVeh = numel(next_node.xs);
+    for iVeh = 1:nVeh
+        g_values(iVeh) = g_values(iVeh)...
+            + norm( [next_node.xs(iVeh)-x_ref_now(iVeh); ...
+                     next_node.ys(iVeh)-y_ref_now(iVeh)] );
+    end
+%     h_values = (1 + (next_node.depth == 14)) * cost_to_go(situation_costs, init_poses, target_poses, next_node, motion_graph);
+    % TODO adjust ctg
     if (next_node.depth < h_p)
         % Distance to every reference trajectory point squared
         % subtract squared distance traveled for every timestep and vehicle
-        x_ref = squeeze(iter.referenceTrajectoryPoints(next_node.depth+1:h_p,1,:));
-        y_ref = squeeze(iter.referenceTrajectoryPoints(next_node.depth+1:h_p,2,:));
-        d_traveled_per_step = dt*v_max;
         time_steps_to_go = h_p - next_node.depth;
+        x_ref = reshape(iter.referenceTrajectoryPoints(next_node.depth+1:h_p,1,:), time_steps_to_go, nVeh);
+        y_ref = reshape(iter.referenceTrajectoryPoints(next_node.depth+1:h_p,2,:), time_steps_to_go, nVeh);
+        d_traveled_per_step = dt*v_max;
         total_steps = nVeh * time_steps_to_go*(time_steps_to_go+1)/2;
-        h_values = sum((next_node.xs-x_ref).^2 + (next_node.ys-y_ref).^2) - (d_traveled_per_step*total_steps)^2;
+        h_values = zeros(1,nVeh);
+        for iVeh = 1:nVeh
+            for i_t = 1:time_steps_to_go
+                h_values(iVeh) = h_values(iVeh) ...
+                    + norm( [next_node.xs(iVeh)-x_ref(i_t,iVeh);...
+                             next_node.ys(iVeh)-y_ref(i_t,iVeh)] ) ...
+                    - i_t * d_traveled_per_step;
+            end
+        end
+%         h_values = sum((next_node.xs-x_ref).^2 + (next_node.ys-y_ref).^2) - (d_traveled_per_step*total_steps)^2;
     else
         h_values = 0;
     end
