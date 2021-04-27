b5fa808440999d89a033e47834203f9fedfbab82
Julius Kahle <julius.kahle@rwth-aachen.de>
2021-04-27 13:52:49 +0200

    implement first version of result struct storing simulation results

---
+++ result/get_result_struct.m
@@ -0,0 +1,15 @@
+function result = get_result_struct(scenario, depth)
+    
+    ticks = 1e3;
+
+    result = struct;
+    
+    result.scenario = scenario;
+    result.controller_outputs = cell(depth,1);
+    result.iteration_structs = cell(depth,1);
+    result.vehicle_path_fullres = cell(scenario.nVeh,depth);
+    result.trajectory_predictions = cell(scenario.nVeh,depth);
+    result.controller_runtime = zeros(depth,1);
+    result.step_time = zeros(depth,1);
+end
+
