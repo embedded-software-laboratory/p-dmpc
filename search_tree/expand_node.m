5674fcac6e4d895353728b043ea4589b2d2af6c5
Patrick Scheffe <patrick.scheffe@rwth-aachen.de>
2021-05-06 10:48:28 +0200

    cost squared

--- search_tree/expand_node.m
+++ search_tree/expand_node.m
@@ -35,26 +35,23 @@ function [expanded_nodes] = expand_node(scenario, iter, cur_node, idx)
         for iVeh = 1:scenario.nVeh
             expanded_node(iVeh,idx.g) = expanded_node(iVeh,idx.g) ...
                 + norm( [expanded_node(iVeh,idx.x) - iter.referenceTrajectoryPoints(iVeh,expanded_node(1,idx.depth),1);...
-                        expanded_node(iVeh,idx.y) - iter.referenceTrajectoryPoints(iVeh,expanded_node(1,idx.depth),2)] );                     
+                        expanded_node(iVeh,idx.y) - iter.referenceTrajectoryPoints(iVeh,expanded_node(1,idx.depth),2)] )^2;                     
         end
         
         % Cost to go
         expanded_node(:,idx.h) = 0;
-        if (expanded_node(1,idx.depth) < scenario.Hp)
-            % Distance to every reference trajectory point squared
-            % subtract squared distance traveled for every timestep and vehicle
-            time_steps_to_go = scenario.Hp - expanded_node(1,idx.depth);
-            for iVeh = 1:scenario.nVeh
-                d_traveled_per_step = scenario.dt*iter.vRef;
-                for i_t = 1:time_steps_to_go
-                    expanded_node(iVeh,idx.h) = expanded_node(iVeh,idx.h)...
-                        + norm( [expanded_node(iVeh,idx.x)-iter.referenceTrajectoryPoints(iVeh,expanded_node(1,idx.depth)+i_t,1);...
-                                expanded_node(iVeh,idx.y)-iter.referenceTrajectoryPoints(iVeh,expanded_node(1,idx.depth)+i_t,2)] ) ...
-                        - d_traveled_per_step * i_t;
-                end
+        % Distance to every reference trajectory point squared
+        % subtract squared distance traveled for every timestep and vehicle
+        time_steps_to_go = scenario.Hp - expanded_node(1,idx.depth);
+        for iVeh = 1:scenario.nVeh
+            d_traveled_per_step = scenario.dt*iter.vRef;
+            for i_t = 1:time_steps_to_go
+                expanded_node(iVeh,idx.h) = expanded_node(iVeh,idx.h)...
+                    + (norm( [expanded_node(iVeh,idx.x)-iter.referenceTrajectoryPoints(iVeh,expanded_node(1,idx.depth)+i_t,1);...
+                            expanded_node(iVeh,idx.y)-iter.referenceTrajectoryPoints(iVeh,expanded_node(1,idx.depth)+i_t,2)] ) ...
+                        - d_traveled_per_step * i_t)^2;
             end
         end
-
 
         expanded_nodes{iTrim} = expanded_node;
     end
