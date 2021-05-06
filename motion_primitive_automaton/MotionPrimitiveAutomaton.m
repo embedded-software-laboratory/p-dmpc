911efa7e0955e65d1c03f7c3ff08c8620f32bad3
Patrick Scheffe <patrick.scheffe@rwth-aachen.de>
2021-05-06 18:40:54 +0200

    added time varying transition matrix for recursive feasibility

--- motion_primitive_automaton/MotionPrimitiveAutomaton.m
+++ motion_primitive_automaton/MotionPrimitiveAutomaton.m
@@ -2,23 +2,25 @@ classdef MotionPrimitiveAutomaton
     properties
         maneuvers % cell(n_trims, n_trims)
         trims % A struct array of the specified trim_inputs
-        transition_matrix_single % Matrix (nTrims x nTrims)
+        transition_matrix_single % Matrix (nTrims x nTrims x horizon_length)
         trim_tuple               % Matrix with trim indices ((nTrims1*nTrims2*...) x nVehicles)
-        transition_matrix        % binary Matrix (if maneuverTuple exist according to trims) (nTrimTuples x nTrimTuples)
-        distance_to_equilibrium % Distance in graph from current state to equilibrium state (nTrims x 1)
+        transition_matrix        % binary Matrix (if maneuverTuple exist according to trims) (nTrimTuples x nTrimTuples x horizon_length)
+        distance_to_equilibrium  % Distance in graph from current state to equilibrium state (nTrims x 1)
     end
     
     methods
-        function obj = MotionPrimitiveAutomaton(model, trim_set, offset, dt, nveh)
+        function obj = MotionPrimitiveAutomaton(model, trim_set, offset, dt, nveh, N)
             % Constructor
             % trim_inputs is a matrix of size (nTrims x nu)
             % trim_adjacency is a matrix of size (nTrims x nTrims), 
             %   read as: rows are start trims and columns are end trims
+            % N is the horizon length
 
             [trim_inputs, trim_adjacency] = choose_trims(trim_set);
             n_trims = length(trim_inputs);
             
-            obj.transition_matrix_single = trim_adjacency;
+            obj.transition_matrix_single = zeros([size(trim_adjacency),N]);
+            obj.transition_matrix_single(:,:,:) = repmat(trim_adjacency,1,1,N);
             
             % trim struct array
             % BicycleModel
@@ -37,7 +39,7 @@ classdef MotionPrimitiveAutomaton
             % maneuver cell/struct matrix
             for i = 1:n_trims
                 for j = 1:n_trims
-                    if obj.transition_matrix_single(i,j)
+                    if obj.transition_matrix_single(i,j,1)
                         obj.maneuvers{i,j} = generate_maneuver(model, obj.trims(i), obj.trims(j), offset, dt);
                     end
                 end
@@ -57,14 +59,25 @@ classdef MotionPrimitiveAutomaton
             [trim_index_list{:}] = deal(1:n_trims);
             obj.trim_tuple = cartprod(trim_index_list{:});
             
+            obj.transition_matrix_single = compute_time_varying_transition_matrix(obj);
+
             % compute maneuver matrix for trimProduct
-            obj.transition_matrix = compute_product_maneuver_matrix(obj,nveh);
+            obj.transition_matrix = compute_product_maneuver_matrix(obj,nveh,N);
             
         end
     
-        function max_speed = get_max_speed(obj)
-            % returns maximum speed per vehicle (nVeh x 1)
-            max_speed = max([obj.trims(:).speed]);
+        function max_speed = get_max_speed(obj, cur_trim_id)
+            % returns maximum speed, averaged over the timestep (nSamples x 1)
+            % TODO is not general, but works for current MPAs
+            N = size(obj.transition_matrix,3);
+            max_speed = zeros(N,1);
+            max_speed_last = obj.trims(cur_trim_id).speed;
+            for k = 1:N
+                successor_trim_ids = find(obj.transition_matrix_single(cur_trim_id, :, k));
+                [max_speed_next, cur_trim_id] = max([obj.trims(successor_trim_ids).speed]);
+                max_speed(k) = (max_speed_next + max_speed_last)/2; % assumes linear change
+                max_speed_last = max_speed_next;
+            end
         end
 
         
@@ -74,7 +87,7 @@ classdef MotionPrimitiveAutomaton
             else
                 dist_to_eq_state = dist_to_eq_state + 1;
                 min_dist = Inf;
-                for s = find(obj.transition_matrix_single(state,:))
+                for s = find(obj.transition_matrix_single(state,:,1))
                     if (ismember(s, visited_states))
                         next_dist = Inf;
                     else
@@ -89,12 +102,30 @@ classdef MotionPrimitiveAutomaton
             end
         end
 
-        function maneuver_matrix = compute_product_maneuver_matrix(obj,nveh)
-            % initialize product maneuver_matrix
-            maneuver_matrix = obj.transition_matrix_single;
-            % compute tensor product iteratively
-            for i = 2 : nveh
-                maneuver_matrix = kron(maneuver_matrix,obj.transition_matrix_single);
+
+        function transition_matrix_single = compute_time_varying_transition_matrix(obj)
+            N = size(obj.transition_matrix_single,3);
+            transition_matrix_single = obj.transition_matrix_single;
+            for k = 1:N
+                % Columns are end trims. Forbid trims whose distance 
+                % to an equilbrium state is too high
+                k_to_go = N-k;
+                transition_matrix_single(:,obj.distance_to_equilibrium>k_to_go,k) = 0;
+            end
+        end
+
+
+        function maneuver_matrix = compute_product_maneuver_matrix(obj,nveh,N)
+            nTrimTuples = size(obj.trim_tuple,1);
+            maneuver_matrix = zeros(nTrimTuples,nTrimTuples,N);
+            % TODO Account for Hp~=Hu
+            for k = 1:N
+                transition_matrix_slice = obj.transition_matrix_single(:,:,k);
+                % compute tensor product iteratively
+                for i = 2 : nveh
+                    transition_matrix_slice = kron(transition_matrix_slice,obj.transition_matrix_single(:,:,k));
+                end
+                maneuver_matrix(:,:,k) = transition_matrix_slice;
             end
         end
 
