b376716a204a26e4c1b8e2a942fcfe0ef27c479c
Julius Kahle <julius.kahle@rwth-aachen.de>
2021-05-07 19:06:05 +0200

    change calculation of plot limits

--- result/get_result_struct.m
+++ result/get_result_struct.m
@@ -8,17 +8,25 @@ function result = get_result_struct(scenario, depth)
     result.trajectory_predictions = cell(scenario.nVeh,depth);
     result.controller_runtime = zeros(depth,1);
     result.step_time = zeros(depth,1);
+    
     % create output directory
     directory_name = strrep([result.scenario.name, '_', result.scenario.controller_name],' ','_');
     output_path = strjoin(['output\', directory_name ,'\'],'');
     result.output_path = output_path;
-    switch scenario.nVeh
-        case 1
-            result.plot_limits = [-22,22;-7,7];
-        case 2
-            result.plot_limits = [-22,22;-7,7];
-        otherwise
-            result.plot_limits = [-22,22;-22,22];
+    
+    % set plot limits based on min and max values of reference trajectory
+    % of all involved vehicles + padding in all direction
+    max_x = 0;
+    min_x = 0;
+    max_y = 0;
+    min_y = 0;
+    pad = 2;
+    for veh = scenario.vehicles
+        max_x = max([veh.referenceTrajectory(:,1)+pad;max_x]);
+        min_x = min([veh.referenceTrajectory(:,1)-pad;min_x]);
+        max_y = max([veh.referenceTrajectory(:,2)+pad;max_y]);
+        min_y = min([veh.referenceTrajectory(:,2)-pad;min_y]);
     end
+    result.plot_limits = [min_x,max_x;min_y,max_y];
 end
 
