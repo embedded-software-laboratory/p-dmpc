686672ac6df5bfaee103904ddaceffefa8c6cc01
Julius Kahle <julius.kahle@rwth-aachen.de>
2021-05-09 19:57:08 +0200

    eliminate unnecessary change of plot

--- visualization/visualize_exploration.m
+++ visualization/visualize_exploration.m
@@ -1,12 +1,12 @@
 function visualize_exploration(exploration,scenario)
-        info = exploration.info;
-        t = [info.tree.Node{:}];
-        t = reshape(t,scenario.nVeh,8,[]);
-        leaf_idcs = find(t(1,info.tree.idx.depth,:)==scenario.Hp);
-        leaves = t(:,:,leaf_idcs);
-        t(:,:,leaf_idcs) = [];
-        exp = line(NaN,NaN,NaN, 'Marker','d', 'MarkerSize',6,'LineStyle','none');
-        exp.XData = reshape(leaves(:,info.tree.idx.x,:),1,[]);
-        exp.YData = reshape(leaves(:,info.tree.idx.y,:),1,[]);
-        exp.ZData = kron(reshape(sum(leaves(:,[info.tree.idx.g,info.tree.idx.h],:),[1,2]),1,[]),[1,1]);
+    info = exploration.info;
+    t = [info.tree.Node{:}];
+    t = reshape(t,scenario.nVeh,8,[]);
+    leaf_idcs = find(t(1,info.tree.idx.depth,:)==scenario.Hp);
+    leaves = t(:,:,leaf_idcs);
+    t(:,:,leaf_idcs) = [];
+    x = reshape(leaves(:,info.tree.idx.x,:),1,[]);
+    y = reshape(leaves(:,info.tree.idx.y,:),1,[]);
+    z = kron(reshape(sum(leaves(:,[info.tree.idx.g,info.tree.idx.h],:),[1,2]),1,[]),[1,1]);
+    line(x,y,z,'Marker','d', 'MarkerSize',6,'LineStyle','none');
 end
