c7965901e1965a1463365900aaf844b3d49d2164
Patrick Scheffe <patrick.scheffe@rwth-aachen.de>
2021-05-19 11:55:52 +0200

    return path even when no distance traveled

--- trajectory/path_between.m
+++ trajectory/path_between.m
@@ -3,25 +3,23 @@ function search_path = path_between(cur_node,next_node, search_tree, mpa)
     n_veh = length(search_tree.Node{1, 1}(:,search_tree.idx.trim));
     search_path = cell(1, n_veh);
     for iVeh = 1:n_veh        
-        if(cur_node(iVeh,search_tree.idx.x) ~= next_node(iVeh,search_tree.idx.x) || cur_node(iVeh,search_tree.idx.y) ~= next_node(iVeh,search_tree.idx.y))
-            maneuver = mpa.maneuvers{cur_node(iVeh,search_tree.idx.trim), next_node(iVeh,search_tree.idx.trim)};
-            assert(~isempty(maneuver),'manuevers{%d, %d} is empty.',cur_node(iVeh,search_tree.idx.trim), next_node(iVeh,search_tree.idx.trim));
-            
-            x = cur_node(iVeh,search_tree.idx.x);
-            y = cur_node(iVeh,search_tree.idx.y);
-            yaw = cur_node(iVeh,search_tree.idx.yaw);
-            
-            xs = maneuver.xs;
-            ys = maneuver.ys;
-            yaws = maneuver.yaws + yaw;       
-            
-            length_maneuver = length(xs);
-            trims = cur_node(iVeh,search_tree.idx.trim) * ones(length_maneuver, 1);
-            
-            [xs, ys] = translate_global(yaw, x, y, xs, ys);
-            
-            search_path(iVeh) = {[xs', ys', yaws', trims]};
-        end
+        maneuver = mpa.maneuvers{cur_node(iVeh,search_tree.idx.trim), next_node(iVeh,search_tree.idx.trim)};
+        assert(~isempty(maneuver),'manuevers{%d, %d} is empty.',cur_node(iVeh,search_tree.idx.trim), next_node(iVeh,search_tree.idx.trim));
+        
+        x = cur_node(iVeh,search_tree.idx.x);
+        y = cur_node(iVeh,search_tree.idx.y);
+        yaw = cur_node(iVeh,search_tree.idx.yaw);
+        
+        xs = maneuver.xs;
+        ys = maneuver.ys;
+        yaws = maneuver.yaws + yaw;       
+        
+        length_maneuver = length(xs);
+        trims = cur_node(iVeh,search_tree.idx.trim) * ones(length_maneuver, 1);
+        
+        [xs, ys] = translate_global(yaw, x, y, xs, ys);
+        
+        search_path(iVeh) = {[xs', ys', yaws', trims]};
     end
 end
 
