8c702e8c2f88c8ea247fd0b9bcbdeefa6f556b5e
Julius Kahle <julius.kahle@rwth-aachen.de>
2021-05-25 18:36:43 +0200

    adapt sub_controller naming

--- controller/centralized_controller.m
+++ controller/centralized_controller.m
@@ -1,7 +1,9 @@
 function [u, y_pred, info] = centralized_controller(scenario, iter)
+    sub_controller = @(scenario, iter)...
+        graph_search(scenario, iter);
     % falsifies controller_runtime slightly
     subcontroller_timer = tic;
-    [u, y_pred, info] = graph_search(scenario, iter);
+    [u, y_pred, info] = sub_controller(scenario, iter);
     % falsifies controller_runtime slightly
     info.subcontroller_runtime = toc(subcontroller_timer);
 end
