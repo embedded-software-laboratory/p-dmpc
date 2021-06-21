0e5824675b47cd34f8e3b42ebeb8955af1544b64
Julius Kahle <julius.kahle@rwth-aachen.de>
2021-06-21 16:49:43 +0200

    fix selection with previous choice

--- ui/selection.m
+++ ui/selection.m
@@ -1,30 +1,6 @@
-% MIT License
-% 
-% Copyright (c) 2021 Lehrstuhl Informatik 11 - RWTH Aachen University
-% 
-% Permission is hereby granted, free of charge, to any person obtaining a copy
-% of this software and associated documentation files (the "Software"), to deal
-% in the Software without restriction, including without limitation the rights
-% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-% copies of the Software, and to permit persons to whom the Software is
-% furnished to do so, subject to the following conditions:
-% 
-% The above copyright notice and this permission notice shall be included in all
-% copies or substantial portions of the Software.
-% 
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-% SOFTWARE.
-% 
-% This file is part of receding-horizon-graph-search.
-% 
-% Author: i11 - Embedded Software, RWTH Aachen University
+function options = selection(vehicle_amount_p,plot_option_p,strat_option_p)
+% SELECTION     Select options run of simulation in an UI.
 
-function options = selection(vehicle_amount,plot_option,strat_option)
 %% Specify amount of vehicles
 scenarios = {
     '1', pi+pi; ...
@@ -61,8 +37,8 @@ if nargin < 1
     if ~exist('vehicle_amount','var')
         vehicle_amount = 1;
     end
-    if ~exist('plot_option','var')
-        plot_option = 1;
+    if ~exist('plot_option','var') || plot_option == 3
+        plot_option = 2;
     end
     if ~exist('strat_option','var')
         strat_option = 1;
@@ -101,6 +77,10 @@ if nargin < 1
         error('Canceled');
     end
     
+else
+    vehicle_amount = vehicle_amount_p;
+    strat_option = strat_option_p;
+    plot_option = plot_option_p;
 end
 
 options.isPB = (strat_option == 2);
