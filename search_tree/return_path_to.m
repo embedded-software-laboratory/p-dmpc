f7c6910ef7fd9e082c7af5e396873075f865c357
Scheffe <scheffe@embedded.rwth-aachen.de>
2021-08-27 23:17:44 +0200

    fix visu

--- search_tree/return_path_to.m
+++ search_tree/return_path_to.m
@@ -2,13 +2,13 @@ function search_paths = return_path_to(iNode, tree, mpa)
 %RETURN_PATH returns the path as cell array to the closest node
 
     n_veh = size(tree.x,1);
-    path = path_to_root(tree, iNode);
-    path = fliplr(path);
-    path_length = length(path);
+    tree_path = path_to_root(tree, iNode);
+    tree_path = fliplr(tree_path);
+    path_length = length(tree_path);
     search_paths = cell(1, n_veh);
     
     for j = 1 : (path_length - 1)
-        search_path = path_between(j, j+1, tree, mpa);
+        search_path = path_between(tree_path(j), tree_path(j+1), tree, mpa);
         
         for i = 1:n_veh
             search_paths(i) = {[search_paths{i}; search_path{i}]};
