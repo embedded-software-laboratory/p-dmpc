cb6ea6445b9b5e5b6669bf6fa51653f47e81e601
Scheffe <scheffe@embedded.rwth-aachen.de>
2021-08-28 01:12:05 +0200

    priority_queue from c++

---
+++ mex/priority_queue_interface_mex.cpp
@@ -0,0 +1,135 @@
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+
+#include <cmath>
+#include <memory>
+#include <queue>
+#include <tuple>
+#include <vector>
+
+using matlab::mex::ArgumentList;
+using std::shared_ptr;
+using std::size_t;
+using std::vector;
+
+#define ID (0)
+#define PRIO_VAL (1)
+
+
+typedef std::tuple<size_t, double> queue_entry;
+
+
+// Custom comparator for queue_entry
+struct queue_entry_comparator
+{
+    inline bool operator() (const queue_entry &a, const queue_entry &b)
+    {
+        return (std::get<PRIO_VAL>(a) > std::get<PRIO_VAL>(b));
+    }
+};
+
+typedef std::priority_queue<queue_entry, std::vector<queue_entry>, queue_entry_comparator> prio_q;
+
+enum COMMAND {
+    NEW,
+    DELETE,
+    PUSH,
+    POP,
+    TOP,
+    SIZE
+};
+class MexFunction : public matlab::mex::Function {
+public:
+    inline void operator()(ArgumentList outputs, ArgumentList inputs) {
+        // checkArguments(outputs, inputs);
+        const int cmd = inputs[0][0];
+
+        if (cmd == NEW)
+        {
+            objects.push_back(shared_ptr<prio_q>(new prio_q));
+            outputs[0] = factory.createScalar<size_t>(objects.size()-1);
+            return;
+        }
+        const size_t iObj = inputs[1][0];
+        shared_ptr<prio_q> pq = objects[iObj];
+
+        if (cmd == DELETE)
+        {
+            pq.reset();
+            return;
+        }
+
+
+        if (cmd == PUSH)
+        {
+            size_t id = inputs[2][0];
+            double prio_val = inputs[3][0];
+            pq->push(queue_entry(id, prio_val));
+            return;
+        }
+
+        if (cmd == POP)
+        {
+            pq->pop();
+            return;
+        }
+        if (cmd == TOP)
+        {
+            queue_entry qe = pq->top();
+            // matlab::data::TypedArray<double> out = factory.createArray<double>(
+            //     {1, 2}, // dims
+            //     {std::get<ID>(qe), std::get<PRIO_VAL>(qe)} // data
+            // );
+            // outputs[0] = out;
+            outputs[0] = factory.createScalar<size_t>(std::get<ID>(qe));
+            if (outputs.size()>1)
+            {
+                outputs[1] = factory.createScalar<double>(std::get<PRIO_VAL>(qe));
+            }
+            return;
+        }
+        if (cmd == SIZE)
+        {
+            outputs[0] = factory.createScalar<size_t>(pq->size());
+            return;
+        }
+    }
+
+private:
+    vector<shared_ptr<prio_q>> objects;
+    // Create MATLAB data array factory
+    matlab::data::ArrayFactory factory;
+
+    void checkArguments(ArgumentList outputs, ArgumentList inputs) {
+        // Get pointer to engine
+        std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+
+        // Get array factory
+        matlab::data::ArrayFactory factory;
+
+        // Check first input argument
+        if (inputs[0].getType() != matlab::data::ArrayType::DOUBLE ||
+            inputs[0].getType() == matlab::data::ArrayType::COMPLEX_DOUBLE ||
+            inputs[0].getNumberOfElements() != 1)
+        {
+            matlabPtr->feval(u"error",
+                0,
+                vector<matlab::data::Array>({ factory.createScalar("First input must be scalar double") }));
+        }
+
+        // Check second input argument
+        if (inputs[1].getType() != matlab::data::ArrayType::DOUBLE ||
+            inputs[1].getType() == matlab::data::ArrayType::COMPLEX_DOUBLE)
+        {
+            matlabPtr->feval(u"error",
+                0,
+                vector<matlab::data::Array>({ factory.createScalar("Input must be double array") }));
+        }
+        // Check number of outputs
+        if (outputs.size() > 1) {
+            matlabPtr->feval(u"error",
+                0,
+                vector<matlab::data::Array>({ factory.createScalar("Only one output is returned") }));
+        }
+    }
+};
--- search_tree/graph_search.m
+++ search_tree/graph_search.m
@@ -19,14 +19,15 @@ function [u, y_pred, info] = graph_search(scenario, iter)
     info.tree = Tree(cur_node);
     
     % Array storing ids of nodes that may be expanded
-    open_nodes = 1;
-    open_values = sum_values(info.tree, open_nodes);
-        
+    
+    pq = slowPriorityQueue();
+    pq.push(1, 0);
+
     % Expand leaves of tree until depth or target is reached or until there 
     % are no leaves
     while true
         % Choose cheapest node for expansion
-        if numel(open_nodes) == 0
+        if pq.size() == 0
             ME = MException( ...
                 'MATLAB:graph_search:tree_exhausted' ...
                 ,'No more open nodes to explore' ...
@@ -34,12 +35,11 @@ function [u, y_pred, info] = graph_search(scenario, iter)
             throw(ME);
         end
         
-        cur_node_id = open_nodes(1);
+        cur_node_id = pq.top();
         cur_node = info.tree.node{cur_node_id};
         
         % remove parent node
-        open_nodes(1) = [];
-        open_values(1) = [];
+        pq.pop();
 
         % Eval edge 
         [is_valid, shapes] = eval_edge_exact(scenario, info.tree, cur_node_id);
@@ -54,8 +54,8 @@ function [u, y_pred, info] = graph_search(scenario, iter)
             info.shapes = return_path_area(shapes_tmp, info.tree, cur_node_id);
             info.tree_path = fliplr(path_to_root(info.tree, cur_node_id));
             info.trim_indices = info.tree.node{info.tree_path(2)}(:,NodeInfo.trim);
-            info.open_nodes = open_nodes;
-            info.open_values = open_values;
+%             info.open_nodes = open_nodes;
+%             info.open_values = open_values;
             break
         else
             % Expand chosen node
@@ -66,10 +66,11 @@ function [u, y_pred, info] = graph_search(scenario, iter)
             );
             parents = cur_node_id*ones(numel(expanded_nodes),1);
             [info.tree, new_open_nodes] = info.tree.add_nodes(parents, expanded_nodes);
+            new_open_values = sum_values(info.tree, new_open_nodes); % TODO expanded nodes
             % add child nodes
-            open_nodes = [open_nodes, new_open_nodes]; %#ok<AGROW>
-            open_values = [open_values, sum_values(info.tree, new_open_nodes)]; %#ok<AGROW>
-            [open_nodes, open_values] = sort_open_list(open_nodes, open_values);
+            for iNode = 1:numel(new_open_nodes)
+                pq.push(new_open_nodes(iNode),new_open_values(iNode));
+            end
 
             % % plot exploration
             % info.plot = visualize_exploration(scenario, info.tree, info.plot);
