84b6421238839e027a554883adf5e9a3dd5dbff9
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-10-14 18:26:50 +0200

    plot the commonroad in MATLAB simulation

---
+++ scenarios/plot_commonroad_lanelets.m
@@ -0,0 +1,12 @@
+function plot_commonroad_lanelets(commonroad_lanelets)
+
+Nlanelets = length(commonroad_lanelets{1});
+
+hold on
+for i = 1:Nlanelets
+    plot(commonroad_lanelets{1}{i},commonroad_lanelets{2}{i},'color','b')
+    plot(commonroad_lanelets{3}{i},commonroad_lanelets{4}{i},'color','b')
+end
+
+
+end
