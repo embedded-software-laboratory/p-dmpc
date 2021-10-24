f6c00722e26046d2c7c698b109f89c9e94730fd3
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-10-24 17:26:41 +0200

    changed the color

--- scenarios/plot_commonroad_lanelets.m
+++ scenarios/plot_commonroad_lanelets.m
@@ -4,8 +4,8 @@ Nlanelets = length(commonroad_lanelets{1});
 
 hold on
 for i = 1:Nlanelets
-    plot(commonroad_lanelets{1}{i},commonroad_lanelets{2}{i},'color','b')
-    plot(commonroad_lanelets{3}{i},commonroad_lanelets{4}{i},'color','b')
+    plot(commonroad_lanelets{1}{i},commonroad_lanelets{2}{i},'color','k')
+    plot(commonroad_lanelets{3}{i},commonroad_lanelets{4}{i},'color','k')
 end
 
 
