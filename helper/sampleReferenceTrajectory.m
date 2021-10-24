6ee58be1de8fdae5ebf1e212ffebeff48d70ea44
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-10-24 18:46:31 +0200

    now works for commonroad scenario to have a refpath continuously running in a loop

--- helper/sampleReferenceTrajectory.m
+++ helper/sampleReferenceTrajectory.m
@@ -1,4 +1,4 @@
-function [ ReferencePoints ] = sampleReferenceTrajectory(nSamples, referenceTrajectory, vehicle_x,vehicle_y, stepSize )
+function [ ReferencePoints ] = sampleReferenceTrajectory(scenarioName,nSamples, referenceTrajectory, vehicle_x,vehicle_y, stepSize )
 % SAMPLEREFERENCETRAJETORY  Computes equidistant points along a piecewise linear curve. The first
 % point is the point on the curve closest to the given point
 % (vehicle_x,vehicle_y). All following points are on the curve with a
@@ -18,34 +18,70 @@ function [ ReferencePoints ] = sampleReferenceTrajectory(nSamples, referenceTraj
     nLinePieces = size(referenceTrajectory,1);
     currentPoint = [x y];
     
-    % All line-segments are assumed to be longer than stepSize. Should it
-    % become necessary to have short line-segments this algorithm needs to
-    % be changed.
-%     for i=1:nLinePieces-1
-%         assert(norm(referenceTrajectory(i+1,:)-referenceTrajectory(i,:),2)>max(stepSize));
-%     end
+    if ~strcmp(scenarioName, 'Commonroad')   
     
-    for i=1:nSamples
-        % make a step
-        remainingLength = norm(currentPoint-referenceTrajectory(TrajectoryIndex,:),2);
-        if remainingLength > stepSize(i) || TrajectoryIndex == nLinePieces
-            currentPoint = currentPoint + stepSize(i)*normalize(referenceTrajectory(TrajectoryIndex,:)-referenceTrajectory(TrajectoryIndex-1,:));
-        else
-%             currentPoint = referenceTrajectory(TrajectoryIndex,:);
-%             TrajectoryIndex = min(TrajectoryIndex+1, nLinePieces);
-%             currentPoint = currentPoint + (stepSize(i)-remainingLength)*normalize(referenceTrajectory(TrajectoryIndex,:)-referenceTrajectory(TrajectoryIndex-1,:));
+        for i=1:nSamples
+            % make a step
+            remainingLength = norm(currentPoint-referenceTrajectory(TrajectoryIndex,:),2);
+            if remainingLength > stepSize(i) || TrajectoryIndex == nLinePieces
+                currentPoint = currentPoint + stepSize(i)*normalize(referenceTrajectory(TrajectoryIndex,:)-referenceTrajectory(TrajectoryIndex-1,:));
+            else
 
-            while remainingLength < stepSize(i) % works for line-segments shorter than the stepSize
-                reflength = remainingLength;
-                currentPoint = referenceTrajectory(TrajectoryIndex,:);
-                TrajectoryIndex = min(TrajectoryIndex+1, nLinePieces);
-                remainingLength = remainingLength + norm(currentPoint-referenceTrajectory(TrajectoryIndex,:),2);
-            end
-            currentPoint = currentPoint + (stepSize(i)-reflength)*normalize(referenceTrajectory(TrajectoryIndex,:)-referenceTrajectory(TrajectoryIndex-1,:));
+                while remainingLength < stepSize(i) % works for line-segments shorter than the stepSize
+                    reflength = remainingLength;
+                    currentPoint = referenceTrajectory(TrajectoryIndex,:);
+                    TrajectoryIndex = min(TrajectoryIndex+1, nLinePieces);
+                    remainingLength = remainingLength + norm(currentPoint-referenceTrajectory(TrajectoryIndex,:),2);
+                end
+                currentPoint = currentPoint + (stepSize(i)-reflength)*normalize(referenceTrajectory(TrajectoryIndex,:)-referenceTrajectory(TrajectoryIndex-1,:));
+            end      
+            % record step
+            ReferencePoints(i,:) = currentPoint;
         end
-        
-        % record step
-        ReferencePoints(i,:) = currentPoint;
+    
+    else
+        %% referenth path in a loop
+        % the last reference point is the first reference point
+        if TrajectoryIndex == nLinePieces
+            TrajectoryIndex = 1;
+            TrajectoryIndexLast = nLinePieces - 1;
+        else
+            TrajectoryIndexLast = TrajectoryIndex - 1;
+        end
+
+        for i=1:nSamples
+            % make a step
+            remainingLength = norm(currentPoint-referenceTrajectory(TrajectoryIndex,:),2);
+            if remainingLength > stepSize(i)
+
+                disp(['Trajectory Index * is: ', num2str(TrajectoryIndex)])
+                currentPoint = currentPoint + stepSize(i)*normalize(referenceTrajectory(TrajectoryIndex,:)-referenceTrajectory(TrajectoryIndexLast,:));
+            else
+    %             currentPoint = referenceTrajectory(TrajectoryIndex,:);
+    %             TrajectoryIndex = min(TrajectoryIndex+1, nLinePieces);
+    %             currentPoint = currentPoint + (stepSize(i)-remainingLength)*normalize(referenceTrajectory(TrajectoryIndex,:)-referenceTrajectory(TrajectoryIndex-1,:));
+
+                while remainingLength < stepSize(i) % works for line-segments shorter than the stepSize
+                    reflength = remainingLength;
+                    currentPoint = referenceTrajectory(TrajectoryIndex,:);
+                    TrajectoryIndexLast = TrajectoryIndex;
+                    TrajectoryIndex = min(TrajectoryIndex+1, nLinePieces);
+
+                    if TrajectoryIndex == nLinePieces
+                        TrajectoryIndex = 1;
+                    end
+
+                    remainingLength = remainingLength + norm(currentPoint-referenceTrajectory(TrajectoryIndex,:),2);
+                end
+                currentPoint = currentPoint + (stepSize(i)-reflength)*normalize(referenceTrajectory(TrajectoryIndex,:)-referenceTrajectory(TrajectoryIndexLast,:));
+            disp(['Trajectory Index is: ', num2str(TrajectoryIndex)])
+            end
+
+            % record step
+            ReferencePoints(i,:) = currentPoint;
+            disp(['ReferencePoint',num2str(i) ' is: ', num2str(currentPoint)])
+        end
+
     end
 end
 
