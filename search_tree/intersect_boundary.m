1f8d804e027d20bd69ed1f6149d034d3737ae74a
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-10-30 23:02:18 +0200

    comment the disp

--- search_tree/intersect_boundary.m
+++ search_tree/intersect_boundary.m
@@ -2,18 +2,19 @@ function collision = intersect_boundary(shape, boundary)
 % INTERSECT_BOUNDARY     Wrapper for INTERSECT_SAT calls for a scenario boundary and check if there is intersection between the shape and the boundary.
 
     collision = false;
-    nboundary = size(boundary,2); % number of boundary segments
-    for i = 1:nboundary
-        npoint = length(boundary{1,nboundary});% number of data points of the boundary segments
+    n_boundary = size(boundary,2); % number of boundary segments
+    
+    for i = 1:n_boundary
+        n_point = length(boundary{1,n_boundary});% number of data points of the boundary segments
         
-        for n = 1:npoint-1
-            if intersect_sat(shape,boundary{1,nboundary}(:,i:i+1))
+        for n = 1:n_point-1
+            if intersect_sat(shape,boundary{1,n_boundary}(:,i:i+1))
                 collision = true;
-                disp('There is collision with the boundary')
+%                 disp('There is collision with the boundary')
                 return;
             end
         end
-        disp('There is no collision with the boundary')
+%         disp('There is no collision with the boundary')
 
     end
 end
