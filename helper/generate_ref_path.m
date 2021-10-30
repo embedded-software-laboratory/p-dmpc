253808ba99d86c29865998cc8586a6f7a74ea8f1
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-10-30 22:55:58 +0200

    modified the function based on commonroad_lanelets function

--- helper/generate_ref_path.m
+++ helper/generate_ref_path.m
@@ -1,322 +1,91 @@
 function refPath = generate_ref_path(vehid)
+% GENERATE_REF_PATH    It returns reference path based on lanelets index
+% for later trajectory planning
 
-% commonroad_data = readstruct('LabMapCommonRoad.xml');
-load('commonroad_data.mat')
 
 
-switch vehid
+    switch vehid
+
+        case 1
+            lanelets_index = [5,9,13,97,93,89,113,109,162,63,59,51,47,43,1];
+
+        case 2
+            lanelets_index = [9,13,97,93,89,113,109,162,63,59,51,47,43,1,5];
+
+        case 3
+            lanelets_index = [13,97,93,89,113,109,162,63,59,51,47,43,1,5,9];
+            
+        case 4 
+            lanelets_index = [97,93,89,113,109,162,63,59,51,47,43,1,5,9,13];
+
+        case 5 
+            lanelets_index = [93,89,113,109,162,63,59,51,47,71,67,36,105,101];
+
+        case 6
+            lanelets_index = [89,85,127,131,135,139,55,51,47,71,67,36,105,101,93];
+
+        case 7
+            lanelets_index = [85,127,131,135,139,55,51,47,71,67,36,105,101,93,89];
+
+        case 8
+            lanelets_index = [127,131,135,143,147,78,25,29,5,9,13,97,93,89,85];
+
+        case 9
+            lanelets_index = [131,135,143,147,78,25,29,5,9,13,97,93,89,85,127];
+
+        case 10
+            lanelets_index = [135,139,55,51,47,43,1,5,9,17,21,120,151,155,131];
+
+        case 11
+            lanelets_index = [139,55,51,47,43,1,5,9,17,21,120,151,155,131,135];
+
+        case 12
+            lanelets_index = [55,51,47,71,67,36,105,101,93,89,85,127,131,135,139];
+
+        case 13
+            lanelets_index = [51,47,71,67,36,105,101,93,89,85,127,131,135,139,55];
+
+        case 14
+            lanelets_index = [47,71,67,36,105,101,93,89,113,109,162,63,59,51];  
+
+        case 15
+            lanelets_index = [43,1,5,9,20,22,33,75,64,62,51,47];
+
+        case 16
+            lanelets_index = [42,84,63,59,51,47,71,67,82,166,151,155,131,135,146,148,159,117,106,104,93,89,113,109,124,40,25,29,5,9,20,23];
+
+        case 17
+            lanelets_index = [106,104,93,89,113,109,124,40,25,29,5,9,20,23,42,84,63,59,51,47,71,67,82,166,151,155,131,135,146,148,159,117];
+
+        case 18
+            lanelets_index = [124,40,25,29,5,9,20,23,42,84,63,59,51,47,71,67,82,166,151,155,131,135,146,148,159,117,106,104,93,89,113,109];
+
+         case 19
+            lanelets_index = [151,155,131,135,146,148,159,117,106,104,93,89,113,109,124,40,25,29,5,9,20,23,42,84,63,59,51,47,71,67,82,166];
+
+         case 20
+            lanelets_index = [159,117,106,104,93,89,113,109,124,40,25,29,5,9,20,23,42,84,63,59,51,47,71,67,82,166,151,155,131,135,146,148];
+
+    end 
+
     
-    case 1
-        lanelets = [6,10,14,98,96,89,113,109,162,63,59,51,49,44,2];
-        refPath = [];
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-        
-    case 2
-        lanelets = [11,20,23,120,151,155,131,135,143,147,78,25,29,8];
-        refPath = [];
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-       
-        
-    case 3
-        lanelets = [14,98,96,89,113,109,162,63,59,51,49,44,2,6,10];
-        refPath = [];
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-        
-    case 4 
-        lanelets = [98,96,89,113,109,162,63,59,51,49,44,2,6,10,14];
-        refPath = [];
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-        
-    case 5 
-        lanelets = [93,89,113,109,162,63,59,51,47,71,67,36,105,101];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-            
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-        
-    case 6
-        lanelets = [90,88,127,131,135,139,55,51,47,43,1,5,9,20,22,33,75,64,62,51,47,43,1,5,9,20];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-              
-        
-    case 7
-        lanelets = [90,86,128,132,137,146,148,79,26,31,5,9,13,97,95];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
+    refPath = [];
+    [lanelets, ~] = commonroad_lanelets(); 
+    
+    for nlanelets = 1:length(lanelets_index)
+        % choose the center line of the lanelet as reference path
+        refPath_x = lanelets{lanelets_index(nlanelets)}(:,LaneletInfo.cx);
+        refPath_y = lanelets{lanelets_index(nlanelets)}(:,LaneletInfo.cy);
+        refPath_next = [refPath_x(1:end),refPath_y(1:end)];
+        refPath = [refPath; refPath_next];
 
-                
-    case 8
-        lanelets = [128,132,137,146,148,79,26,31,5,9,13,97,95,90,86];
-
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-                
-    case 9
-        lanelets = [132,137,146,148,79,26,31,5,9,13,97,95,90,86,128];
-
-        refPath = [];
-
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-                
-    case 10
-        lanelets = [135,139,55,51,49,44,2,6,11,20,23,120,151,155,131];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-        
-    case 11
-        lanelets = [140,56,52,48,2,6,11,20,23,120,151,155,131,138];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-
-        
-    case 12
-        lanelets = [55,51,47,71,67,36,105,101,93,89,85,127,131,135,139];
-        refPath = [];
-
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-        
-    case 13
-        lanelets = [54,47,71,67,36,105,101,93,89,85,127,131,138,140,56];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-       
-        
-        
-    case 14
-        lanelets = [47,71,67,36,105,101,93,89,113,109,162,63,59,51];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-      
-       
-    case 15
-        lanelets = [43,1,5,9,20,22,33,75,64,62,51,47];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-        
-    case 16
-        lanelets = [42,84,63,59,51,47,71,67,82,166,151,155,131,135,146,148,159,117,106,104,93,89,113,109,124,40,25,29,5,9,20,23];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end       
-
-    case 17
-        lanelets = [106,104,93,89,113,109,124,40,25,29,5,9,20,23,42,84,63,59,51,47,71,67,82,166,151,155,131,135,146,148,159,117];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-        
-    case 18
-        lanelets = [124,40,25,29,5,9,20,23,42,84,63,59,51,47,71,67,82,166,151,155,131,135,146,148,159,117,106,104,93,89,113,109];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-        
-     case 19
-        lanelets = [151,155,131,135,146,148,159,117,106,104,93,89,113,109,124,40,25,29,5,9,20,23,42,84,63,59,51,47,71,67,82,166];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-        
-     case 20
-        lanelets = [159,117,106,104,93,89,113,109,124,40,25,29,5,9,20,23,42,84,63,59,51,47,71,67,82,166,151,155,131,135,146,148];
-        refPath = [];
-        
-        for nlanelets = 1:length(lanelets)
-            
-            refPath_x = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.x) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.x));
-            refPath_y = 1/2*(horzcat(commonroad_data.lanelet(lanelets(nlanelets)).leftBound.point.y) + horzcat(commonroad_data.lanelet(lanelets(nlanelets)).rightBound.point.y));
-      
-            refPath_next = [refPath_x(1:end);refPath_y(1:end)];
-            refPath = [refPath, refPath_next];
-           
-        end 
-        
-             
-end 
-
+    end 
 
 % figure
-% plot(refPath(1,:),refPath(2,:))
+% plot(refPath(:,1),refPath(:,2))
 % 
 % xlim([0,4.5])
 % ylim([0,4])
-
-
-refPath = refPath';
-
-
 
 end
 
