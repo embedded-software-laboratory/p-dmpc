f32397b951a219b1409b8b5bd404342597ee6abb
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-11-22 00:39:48 +0100

    topological priority

---
+++ priority/topo_priority.m
@@ -0,0 +1,32 @@
+classdef  topo_priority < interface_priority
+% random_priority  Instance of experiment interface used for simulation in matlab.
+    
+    properties (Access=private)
+        isDAG
+        topo_groups
+        
+    end
+    
+    methods 
+        function obj = topo_priority(scenario)
+            obj.scenario = scenario;
+            
+            if obj.scenario.assignPrios || isempty(obj.scenario.directed_coupling)
+                [obj.isDAG, obj.topo_groups] = topological_sorting_coloring(obj.scenario.adjacency);
+            else
+                [obj.isDAG, obj.topo_groups] = kahn(obj.scenario.directed_coupling);
+            end
+            
+        end
+        
+        function groups = priority(obj)
+            assert( obj.isDAG, 'Coupling matrix is not a DAG' );
+            groups = PB_predecessor_groups(obj.topo_groups);
+            
+        end
+        
+    end
+    
+
+
+end
