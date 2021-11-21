0ebcde5c755b29917bd8e2a53a64ba6e49973dab
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-11-22 00:49:00 +0100

    added groups property and priority method

--- priority/interface_priority.m
+++ priority/interface_priority.m
@@ -2,9 +2,16 @@ classdef (Abstract) interface_priority < handle
 % interface_priority    Abstract class used for defining properties and methods used by priority based distributed controller.
     
     properties (Access=protected)
-        % struct used for every iteration
         scenario
+        groups
     end
     
 
+    methods (Abstract) 
+        priority(obj)
+    end
+    
+    
+    
+    
 end
