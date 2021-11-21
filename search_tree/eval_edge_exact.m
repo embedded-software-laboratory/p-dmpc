dff04dc3a1014a0d022bc8edb137ed34282ae85a
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-11-22 00:59:55 +0100

    changed the condition for checking collision with boundary

--- search_tree/eval_edge_exact.m
+++ search_tree/eval_edge_exact.m
@@ -5,6 +5,7 @@ function [is_valid, shapes] = eval_edge_exact(scenario, tree, iNode)
     % maneuver shapes correspond to movement TO node
     node_id_parent = get_parent(tree, iNode);
     shapes = cell(scenario.nVeh,1);
+    shapes_without_offset = cell(scenario.nVeh,1);
     if ~node_id_parent % root node without parent
         return;
     end
@@ -28,10 +29,16 @@ function [is_valid, shapes] = eval_edge_exact(scenario, tree, iNode)
         shape_y = s*maneuver.area(1,:) + c*maneuver.area(2,:) + pY(iVeh);
         shapes{iVeh} = [shape_x;shape_y];
         
+        shape_x_without_offset = c*maneuver.area_without_offset(1,:) - s*maneuver.area_without_offset(2,:) + pX(iVeh);
+        shape_y_without_offset = s*maneuver.area_without_offset(1,:) + c*maneuver.area_without_offset(2,:) + pY(iVeh);
+        shapes_without_offset{iVeh} = [shape_x_without_offset;shape_y_without_offset];
+        
+        
         iStep = cK;
         
-        if collision_with(iVeh, shapes, scenario, iStep)
+        if collision_with(iVeh, shapes, shapes_without_offset, scenario, iStep)
             is_valid = false;
+%             disp('there is collision between two vehicles')
             return;
         end
     end
