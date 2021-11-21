9bbeeaba43c371beb6d27cb1ff96da640e4ce11b
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-11-22 00:58:57 +0100

    changed the shape to shape without offset

--- search_tree/intersect_boundary.m
+++ search_tree/intersect_boundary.m
@@ -1,5 +1,5 @@
-function collision = intersect_boundary(shape, boundary)
-% INTERSECT_BOUNDARY     Wrapper for INTERSECT_SAT calls for a scenario boundary and check if there is intersection between the shape and the boundary.
+function collision = intersect_boundary(shapes_without_offset, boundary)
+% INTERSECT_BOUNDARY     Wrapper for INTERSECT_SAT calls for a scenario boundary and check if there is intersection between the shape and the lanelets boundary.
 % Returns true iff the shape intersect with the boundary
 
     collision = false;
@@ -9,7 +9,7 @@ function collision = intersect_boundary(shape, boundary)
         n_point = length(boundary{1,i});% number of data points of the boundary segments
         
         for n = 1:n_point-1
-            if intersect_sat(shape,boundary{1,i}(:,n:n+1))
+            if intersect_sat(shapes_without_offset,boundary{1,i}(:,n:n+1))
                 collision = true;
 %                 disp('There is collision with the boundary')
                 return;
