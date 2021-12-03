ccc65d0577c5991435994ce4309b6b37238abe6d
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-12-03 23:28:56 +0100

    modified for fallback

--- main.m
+++ main.m
@@ -21,6 +21,7 @@ if is_sim_lab
         otherwise
             options = selection();
     end
+    vehicle_ids = [1,2,6,8,16,17];
 else
     disp('cpmlab')
     options = struct;
@@ -28,10 +29,11 @@ else
     options.amount = numel(vehicle_ids);
     options.isPB = true;
 end
-    
+  
 
 scenario = commonroad(options.amount,vehicle_ids,options.isPB);
 
+% scenario = lanelet_scenario4(options.isPB);
 
 % scenario = circle_scenario(options.amount,options.isPB);
 
@@ -40,6 +42,7 @@ if is_sim_lab
 else
     exp = CPMLab(scenario, vehicle_ids);
 end
+
 
 %% Setup
 % Initialize
@@ -50,24 +53,34 @@ k = 1;
 result = get_result_struct(scenario);
 
 exp.setup();
-
 %% Main control loop
 while (~got_stop)
+    
     result.step_timer = tic;
     % Measurement
     % -------------------------------------------------------------------------
-    [ x0, trim_indices ] = exp.measure();
-    
+    [ x0, trim_indices ] = exp.measure();% trim_indices： which trim  
+
+%     disp('x0 is:')
+%     disp(x0)
     try
         % Control 
         % ----------------------------------------------------------------------
         % Sample reference trajectory
         iter = rhc_init(scenario,x0,trim_indices);
+        
+        if ~isempty(scenario.lanelets)
+            scenario.adjacency = coupling_adjacency(scenario,iter);
+        end
+        
+%         disp('adjacency_matrix is:')
+%         disp(scenario.adjacency)
         scenario_tmp = get_next_dynamic_obstacles_scenario(scenario, k);
+        result.iter_runtime(k) = toc(result.step_timer);
         
         controller_timer = tic;
             [u, y_pred, info] = scenario.controller(scenario_tmp, iter);
-            
+
         result.controller_runtime(k) = toc(controller_timer);
         result.iteration_structs{k} = iter;
         % save controller outputs in result struct
@@ -81,14 +94,42 @@ while (~got_stop)
         
         % Apply control action f/e veh
         % -------------------------------------------------------------------------
+        apply_timer = tic;
         exp.apply(u, y_pred, info, result, k);
-
+        result.apply_runtime(k) = toc(apply_timer);
+        result.total_runtime(k) = toc(result.step_timer);
+        
     % catch case where graph search could not find a new node
     catch ME
         switch ME.identifier
         case 'MATLAB:graph_search:tree_exhausted'
+%             warning([ME.message, ', ending search...']);
+
+            disp('ME, fallback to last priority...............................')  
             warning([ME.message, ', ending search...']);
-            got_stop = true;
+            controller_timer = tic;
+            [u, y_pred, info] = pb_controller_fallback(scenario, u, y_pred, info);
+
+            result.controller_runtime(k) = toc(controller_timer);
+            result.iteration_structs{k} = iter;
+            % save controller outputs in result struct
+            result.trajectory_predictions(:,k) = y_pred;
+            result.controller_outputs{k} = u;
+            result.subcontroller_runtime(:,k) = info.subcontroller_runtime;
+            % store vehicles path in higher resolution
+            result.vehicle_path_fullres(:,k) = info.vehicle_fullres_path(:);
+            result.n_expanded(k) = info.n_expanded;
+            result.step_time(k) = toc(result.step_timer);
+
+            % Apply control action f/e veh
+            % -------------------------------------------------------------------------
+            apply_timer = tic;
+            exp.apply(u, y_pred, info, result, k);
+            result.apply_runtime(k) = toc(apply_timer);
+            result.total_runtime(k) = toc(result.step_timer);
+            disp('plotting finished ........................................');  
+          
+%             got_stop = true;
         otherwise
             rethrow(ME)
         end
@@ -96,16 +137,13 @@ while (~got_stop)
     
     % Check for stop signal
     % -------------------------------------------------------------------------
-%     got_stop = exp.is_stop() || got_stop;
+    got_stop = exp.is_stop() || got_stop;
     
     % increment interation counter
     k = k+1;
 end
-
 %% save results
 save(fullfile(result.output_path,'data.mat'),'result');
-
+exportVideo( result );
 exp.end_run()
-
-
 end
