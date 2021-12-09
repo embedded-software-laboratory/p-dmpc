7993452a008cce5bfcf1a6db53dbbd9c6ca399c4
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-12-10 00:39:43 +0100

    right of way works now

--- priority/right_of_way.m
+++ priority/right_of_way.m
@@ -31,7 +31,7 @@ classdef  right_of_way < interface_priority
             priority = ones(1,nVeh); 
             lanelets_idx = zeros(nVeh, Hp);
             Veh = 1:nVeh;
-            
+            veh_at_intersection = [];
 
             
             % identify vehicles at intersection
@@ -39,61 +39,75 @@ classdef  right_of_way < interface_priority
                 
                 lanelets_idx(veh_idx,:) = lanelets_index(veh_idx,obj.iter,obj.scenario);
                 is_at_intersection = (sum(ismember(lanelets_idx(veh_idx,:), obj.scenario.intersection_lanelets)) > 0);     
+%                 disp('lanelet index')
+%                 disp(lanelets_idx(veh_idx,:))
+
                 if is_at_intersection
+%                     disp('is_at_intersection')
+%                     disp(is_at_intersection)
                     veh_at_intersection = [veh_at_intersection,veh_idx];
-                    disp('there are vehicles at intersection')
                 end
 
             end
             
+     
             % check the distance to the center of the intersection and
             % assign higher priority to vehicles with smaller distance
-            if is_at_intersection
+            if ~isempty(veh_at_intersection)
+                
+                disp('there are vehicles at intersection')
+                disp(veh_at_intersection)
+                
                 n_veh_at_intersection = length(veh_at_intersection);
             
-                first_referenceTrajectoryPoints = [obj.iter.referenceTrajectoryPoints(veh,1,1),obj.iter.referenceTrajectoryPoints(veh,1,2)];
-                diff = first_referenceTrajectoryPoints-intersection_center;
+                first_referenceTrajectoryPoints = [obj.iter.referenceTrajectoryPoints(veh_at_intersection,1,1),obj.iter.referenceTrajectoryPoints(veh_at_intersection,1,2)];
+                diff = first_referenceTrajectoryPoints - intersection_center;
                 distance_to_center = sqrt(diff(:,1).^2 + diff(:,2).^2);
-                [~, index] = sort(distance_to_center,ascend);
+                [~, index] = sort(distance_to_center,'ascend');
+                disp('index')
+                disp(index)
                 veh_at_intersection = veh_at_intersection(index);
+                disp(veh_at_intersection)
                 priority(veh_at_intersection)= 1:n_veh_at_intersection;
+                
+                disp('priority')
+                disp(priority)
 
                 % assign priority to vehicles that are not at intersection
                 prio = n_veh_at_intersection + 1;
-                veh_not_at_intersection = setdiff(Veh,veh_at_intersection);
-            else
-                veh_at_intersection = [];
-                veh_not_at_intersection = setdiff(Veh,veh_at_intersection);
                 
             end
             
-            ordered_adjacent_group = [];
+            
+            veh_not_at_intersection = setdiff(Veh,veh_at_intersection);
+            
+            
+            ordered_adjacent_group_expanded = [];
             veh_iterated = [];
             
             for veh_i = veh_not_at_intersection
-                               
+                
+                ordered_adjacent_group = [];               
                 ordered_group = adjacent_group_ordering(obj.scenario, obj.iter, veh_i, veh_at_intersection);
                 ordered_adjacent_group = [ordered_adjacent_group,ordered_group]; 
                 
-
                 
                 % if the first and the last one are not iterated yet,
                 % further check these two first
-                veh_first = ordered_adjacent_group(1);
-                veh_end = ordered_adjacent_group(end);
+                veh_first = ordered_group(1);
+                veh_end = ordered_group(end);
                 
                 while ~ismember(veh_end, veh_iterated) || ~ismember(veh_first, veh_iterated)
                     if ~ismember(veh_end, veh_iterated)      
                         ordered_group = adjacent_group_ordering(obj.scenario, obj.iter, veh_end, veh_at_intersection);
                         ordered_adjacent_group = [ordered_adjacent_group,ordered_group];                   
                         veh_iterated = [veh_iterated, veh_end];
+                    end
 
-                    else
-
+                    if ~ismember(veh_first, veh_iterated)
                         ordered_group = adjacent_group_ordering(obj.scenario, obj.iter, veh_first, veh_at_intersection);
                         ordered_adjacent_group = [ordered_group,ordered_adjacent_group];             
                         veh_iterated = [veh_iterated, veh_first];
-
                     end
                     
                     % remove the repeat elements and keep original orders
@@ -103,18 +117,28 @@ classdef  right_of_way < interface_priority
                     veh_end = ordered_adjacent_group(end);
                     
                 end 
+                ordered_adjacent_group_expanded = [ordered_adjacent_group_expanded, ordered_adjacent_group];
+                disp('ordered_adjacent_group_expanded')
+                disp(ordered_adjacent_group_expanded)
+                
+                [~, idx, ~] = unique(ordered_adjacent_group_expanded);
+                ordered_adjacent_group_expanded = ordered_adjacent_group_expanded(sort(idx));
+                
                 
                 veh_iterated = [veh_iterated, veh_i];
                 
         
                 % if all vehicles are ordered, then stop iteration in advance
-                if length(ordered_adjacent_group) == length(veh_not_at_intersection)
+                if length(ordered_adjacent_group_expanded) == length(veh_not_at_intersection)
                     break
                 end
                       
             end
             
-            priority(ordered_adjacent_group) = prio:nVeh;
+            priority(ordered_adjacent_group_expanded) = prio:nVeh;
+            
+            disp('priority')
+            disp(priority)
             
             [~,veh_index] = sort(priority);
 
