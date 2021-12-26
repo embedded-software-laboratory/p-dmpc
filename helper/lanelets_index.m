74cdf33e2ba7c511ff1e32dfad90eecd002932a8
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-12-26 15:54:23 +0100

    modified the lanelets index

--- helper/lanelets_index.m
+++ helper/lanelets_index.m
@@ -2,13 +2,14 @@ function lanelets_idx = lanelets_index(veh_idx,iter,scenario)
 % lanelets_index returns the lanelets index based on their iteration reference
 % trajectory points
     
-    lanelets_idx = [];
-    Hp = size(iter.referenceTrajectoryPoints,2);
-    ref_points_index = reshape(iter.referenceTrajectoryIndex(veh_idx,:,:),1,Hp);
+    nlanelets_idx = [];
+    ref_points_index = iter.referenceTrajectoryIndex(veh_idx,:);
 
     % the lanelet index within prediction horizon
     for n = ref_points_index
-        lanelets_idx = [ lanelets_idx, sum(n > scenario.vehicles(1,veh_idx).points_index)+1];
+        nlanelets_idx = [ nlanelets_idx, sum(n > scenario.vehicles(1,veh_idx).points_index)+1];
     end 
+    
+    lanelets_idx = scenario.vehicles(1,veh_idx).lanelets_index(nlanelets_idx);
         
 end           
