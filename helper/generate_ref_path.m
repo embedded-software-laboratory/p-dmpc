cfe493035758e287768ffe551956e98b8fa4f18c
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-12-26 15:47:57 +0100

    updated the ref paths

--- helper/generate_ref_path.m
+++ helper/generate_ref_path.m
@@ -10,7 +10,6 @@ ref_path = struct;
 
         case 1
             ref_path.lanelets_index = [3,5,7,59,57,55,67,65,98,37,35,31,29,27,1];
-%             ref_path.lanelets_index = [3,5,7,59,57,74,68,66,99,38,36,49,29,27,1];
 
         case 2
             ref_path.lanelets_index = [5,7,59,57,55,67,65,98,37,35,31,29,27,1,3];
@@ -25,7 +24,7 @@ ref_path = struct;
             ref_path.lanelets_index = [57,55,67,65,98,37,35,31,29,27,1,3,5,7,59];
 
         case 6
-            ref_path.lanelets_index = [57,55,53,79,81,83,85,33,31,29,41,39,20,63,61];
+            ref_path.lanelets_index = [29,41,39,20,63,61,57,55,53,79,81,83,85,33,31];
 
         case 7
             ref_path.lanelets_index = [53,79,81,83,85,33,31,29,41,39,20,63,61,57,55];
@@ -37,7 +36,7 @@ ref_path = struct;
             ref_path.lanelets_index = [81,83,87,89,46,13,15,3,5,7,59,57,55,53,79];
 
         case 10
-            ref_path.lanelets_index = [81,83,85,33,31,29,27,1,3,5,9,11,72,91,93];
+            ref_path.lanelets_index = [9,11,72,91,93,81,83,85,33,31,29,27,1,3,5];
 
         case 11
             ref_path.lanelets_index = [85,33,31,29,27,1,3,5,9,11,72,91,93,81,83];
@@ -49,7 +48,7 @@ ref_path = struct;
             ref_path.lanelets_index = [31,29,41,39,20,63,61,57,55,53,79,81,83,85,33];
 
         case 14
-            ref_path.lanelets_index = [31,29,41,39,20,63,61,57,55,53,79,81,83,85,33];  
+            ref_path.lanelets_index = [29,41,39,20,63,61,57,55,53,79,81,83,85,33,31];  
 
         case 15
             ref_path.lanelets_index = [27,1,3,5,9,11,26,52,37,35,31,29];
@@ -67,13 +66,13 @@ ref_path = struct;
             ref_path.lanelets_index = [91,93,81,83,85,33,31,29,41,39,50,102];
 
          case 20
-            ref_path.lanelets_index = [69,64,62,75,55,53,79,81,101,88,90,95];
+            ref_path.lanelets_index = [78,63,61,57,55,53,79,81,83,87,89,104];
 
     end 
 
     % reference path
     path = [];
-    [lanelets, ~,~,~] = commonroad_lanelets(); 
+    [lanelets, ~,~,~,~] = commonroad_lanelets(); 
     
     for nlanelets = 1:length(ref_path.lanelets_index)
         % choose the center line of the lanelet as reference path
