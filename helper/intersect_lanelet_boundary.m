cfe8d167cd149b78ce4d67774006840817e039b4
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-12-30 23:53:38 +0100

    updated the indexing for cell

--- helper/intersect_lanelet_boundary.m
+++ helper/intersect_lanelet_boundary.m
@@ -1,21 +1,32 @@
-function collision = intersect_lanelet_boundary(shapes_without_offset, boundary)
+function collision = intersect_lanelet_boundary(shapes, boundary)
 % INTERSECT_LANELET_BOUNDARY     check if there is intersection between the shape and the lanelets boundary.
 % Returns true iff the shape intersect with the boundary
 
     collision = false;
-    boundary_points = cell2mat(boundary);
-    n_point = size(boundary_points,1);% number of data points of the boundary segments
-
-    for n = 1:n_point-1
-        %check leftBoundary
-        if intersect_sat(shapes_without_offset,boundary_points(n:n+1,1:2)') || intersect_sat(shapes_without_offset,boundary_points(n:n+1,3:4)')
-            
+    boundary_points_left = boundary{1,1}';
+    boundary_points_right = boundary{1,2}';
+    n_point_left = length(boundary_points_left);% number of data points of the boundary segments
+    n_point_right = length(boundary_points_right);
+    
+    for n = 1:n_point_left-1
+        %check left boundary
+        if intersect_sat(shapes,boundary_points_left(:,n:n+1))     
             collision = true;
 %             disp('There is collision with the boundary')
             return;
         end
         
     end
-%         disp('There is no collision with the boundary')
+    
+    for i = 1:n_point_right-1
+        %check right boundary
+        if intersect_sat(shapes,boundary_points_right(:,i:i+1))    
+            collision = true;
+%             disp('There is collision with the boundary')
+            return;
+        end
+        
+    end    
+    
 
 end
