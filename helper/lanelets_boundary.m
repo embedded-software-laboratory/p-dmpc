7035d92476c240c5fd2995f34c6a0b21a061d617
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-12-30 23:54:59 +0100

    use scenario.lanelet_boundary for checking out the lanelets boundary

--- helper/lanelets_boundary.m
+++ helper/lanelets_boundary.m
@@ -1,14 +1,15 @@
 function lanelet_boundary = lanelets_boundary(scenario, iter)
 
-% lanelets_boundary  l. 
+% lanelets_boundary  return the lanelet boundaries for each vehicles
     
     nVeh = size(iter.referenceTrajectoryPoints,1);
     Hp = size(iter.referenceTrajectoryPoints,2);
     lanelets = scenario.lanelets;
-    lanelet_boundary = {};
+    lanelet_boundary = cell(1,nVeh);
 
     
     for i = 1:nVeh
+        % check the lanelet index based on the reference points
         nlanelets_i = [];     
         ref_points_index_i = reshape(iter.referenceTrajectoryIndex(i,:,:),1,Hp);
         
@@ -20,49 +21,16 @@ function lanelet_boundary = lanelets_boundary(scenario, iter)
         [~, idx, ~] = unique(lanelets_index);
         lanelets_index = lanelets_index(sort(idx));
         
-        boundary = [];
-        
+        % determine the boundaries for each vehicle
+        left_bound = [];
+        right_bound = [];
         for index = lanelets_index
-            
-            bound = [];     
-            adjacentLeft = scenario.commonroad_data.lanelet(index).adjacentLeft;
-            adjacentRight = scenario.commonroad_data.lanelet(index).adjacentRight;
-
-            % if the lanelet has adjacent left lanelet, the boundary
-            % should be the leftBound of adjacentLeft and rightBound of
-            % the current lanelet
-            if isfield(adjacentLeft,'refAttribute')
-                adjacentLeft_index = horzcat(adjacentLeft.refAttribute);
-                left_bound_x = lanelets{ adjacentLeft_index }(:,LaneletInfo.lx);
-                left_bound_y = lanelets{ adjacentLeft_index }(:,LaneletInfo.ly);
-                left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
-
-                right_bound_x = lanelets{ index }(:,LaneletInfo.rx);
-                right_bound_y = lanelets{ index }(:,LaneletInfo.ry);
-                right_bound = [right_bound_x(1:end),right_bound_y(1:end)];                           
-            end
-
-            if isfield(adjacentRight,'refAttribute')
-                adjacentRight_index = horzcat(adjacentRight.refAttribute);
-                left_bound_x = lanelets{ index }(:,LaneletInfo.lx);
-                left_bound_y = lanelets{ index }(:,LaneletInfo.ly);
-                left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
-
-                right_bound_x = lanelets{ adjacentRight_index }(:,LaneletInfo.rx);
-                right_bound_y = lanelets{ adjacentRight_index }(:,LaneletInfo.ry);
-                right_bound = [right_bound_x(1:end),right_bound_y(1:end)];
-            end
-            bound = [left_bound,right_bound];
-
-            boundary = [boundary; bound];
+            left_bound = [left_bound; scenario.lanelet_boundary{1,index}{1,1}];
+            right_bound = [right_bound;scenario.lanelet_boundary{1,index}{1,2}];
         end
         
-        lanelet_boundary{i} = boundary;
-%         disp('lanelet_boudnary')
-%         disp(lanelet_boundary(1,i))
+        lanelet_boundary{i} = {left_bound,right_bound};
 
     end 
-    
-            
 
 end
