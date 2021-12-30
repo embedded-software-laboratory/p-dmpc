9dd65389d9f64fc240d09ab2f0e5f6f9caa9e7f5
Bingyi Su <bingyi.su@rwth-aachen.de>
2021-12-30 23:49:00 +0100

    added output lanelet_boundary

--- lanelets/commonroad_lanelets.m
+++ lanelets/commonroad_lanelets.m
@@ -1,4 +1,4 @@
-function [lanelets,adjacency,intersection_lanelets,boundary,commonroad] = commonroad_lanelets()
+function [lanelets,adjacency,intersection_lanelets,boundary,commonroad,lanelet_boundary] = commonroad_lanelets()
 
 % COMMONROAD_LANELETS  returns the lanelets information, collision pair matrix and the boundary information
 
@@ -16,9 +16,11 @@ function [lanelets,adjacency,intersection_lanelets,boundary,commonroad] = common
 %     commonroad_data = readstruct('LabMapCommonRoad_Update.xml');
 %     save('commonroad_data.mat','commonroad_data')
     
+    %% commonroad data
     load('commonroad_data.mat');
+    commonroad = commonroad_data;
     
-
+    %% lanelets
     lanelets = cell(1,0);
     Nlanelets = length(commonroad_data.lanelet); % number of lanelets
 
@@ -37,13 +39,11 @@ function [lanelets,adjacency,intersection_lanelets,boundary,commonroad] = common
 
     end
      
-    commonroad = commonroad_data;
-    %% commonroad scenario boundary 
-                     
+    
+    %% boundary                     
     boundary = cell(1,0);
     
-    % use the rightBound of the lanelets as the inner boundary
-    % lanelets index of the inner boundary
+    % use the rightBound of the lanelets as the inner boundary lanelets index of the inner boundary
     inner_boundary_lanelets = {[14,16,22,23,10,12,18] ... 
                                [49,48,42,40,44,38,36] ...  
                                [88,90,96,92,94,100,101] ... 
@@ -62,9 +62,7 @@ function [lanelets,adjacency,intersection_lanelets,boundary,commonroad] = common
     
     end  
     
-    % use the leftBound of the lanelets as the outer boundary
-    
-    % lanelets index of the outer boundary
+    % use the leftBound of the lanelets as the outer boundary lanelets index of the outer boundary
     outer_boundary_lanelets = {[2,4,6,8,60,58,56,54,80,82,84,86,34,32,30,28]};    
     for n_outer_boundary = 1:size(outer_boundary_lanelets,2)
         boundary_segment = [];
@@ -80,9 +78,7 @@ function [lanelets,adjacency,intersection_lanelets,boundary,commonroad] = common
     end
         
       
-    % use the leftBound of the lanelets as the outer turning boundary
-    
-    % lanelets index of the outer turning boundary
+    % use the leftBound of the lanelets as the outer turning boundary lanelets index of the outer turning boundary
     turning_boundary_lanelets = {[11],[63],[13],[39],[37],[89],[65],[91]};
     for n_turning_boundary = 1:size(turning_boundary_lanelets,2)
         boundary_segment = [];
@@ -108,7 +104,7 @@ function [lanelets,adjacency,intersection_lanelets,boundary,commonroad] = common
 %     end
 %     
     
-    %% adjacency pair matrix
+    %% adjacency
     
     adj = zeros(Nlanelets, Nlanelets);
  
@@ -143,6 +139,21 @@ function [lanelets,adjacency,intersection_lanelets,boundary,commonroad] = common
                 if isfield(predecessor_successor,'refAttribute')
                    predecessor_successor_index = horzcat(predecessor_successor.refAttribute);
                    adjacent_index = [adjacent_index,predecessor_successor_index];
+                   
+                   for idx = predecessor_successor_index
+                       predecessor_successor_adjacentLeft = commonroad_data.lanelet(idx).adjacentLeft;
+                       predecessor_successor_adjacentRight = commonroad_data.lanelet(idx).adjacentRight;
+                       if isfield(predecessor_successor_adjacentLeft,'refAttribute')
+                           predecessor_successor_adjacentLeft_index = horzcat(predecessor_successor_adjacentLeft.refAttribute);
+                           
+                           adjacent_index = [adjacent_index,predecessor_successor_adjacentLeft_index];
+                       end
+                       if isfield(predecessor_successor_adjacentRight,'refAttribute')
+                           predecessor_successor_adjacentRight_index = horzcat(predecessor_successor_adjacentRight.refAttribute);
+                           adjacent_index = [adjacent_index,predecessor_successor_adjacentRight_index];
+                       end                      
+                   end
+                   
                 end
             end
         end
@@ -169,46 +180,42 @@ function [lanelets,adjacency,intersection_lanelets,boundary,commonroad] = common
                 % check the predecessor of successor to include merging lanelets
                 if isfield(successor_predecessor,'refAttribute')
                    successor_predecessor_index = horzcat(successor_predecessor.refAttribute);
-                   
-                   % successor_predecessor's adjacentLeft and adjacentRight
-                   
-                   
-                   
-                   
                    adjacent_index = [adjacent_index,successor_predecessor_index];
+                   
+                   for idx = successor_predecessor_index
+                       successor_predecessor_adjacentLeft = commonroad_data.lanelet(idx).adjacentLeft;
+                       successor_predecessor_adjacentRight = commonroad_data.lanelet(idx).adjacentRight;
+                       
+                       if isfield(successor_predecessor_adjacentLeft,'refAttribute')
+                           successor_predecessor_adjacentLeft_index = horzcat(successor_predecessor_adjacentLeft.refAttribute);
+                           adjacent_index = [adjacent_index,successor_predecessor_adjacentLeft_index];
+                       end
+                       
+                       if isfield(successor_predecessor_adjacentRight,'refAttribute')
+                           successor_predecessor_adjacentRight_index = horzcat(successor_predecessor_adjacentRight.refAttribute);
+                           adjacent_index = [adjacent_index,successor_predecessor_adjacentRight_index];
+                       end  
+                   end
                 end
-                
             end
-            
         end
         
-        if isfield(adjacentLeft,'refAttribute')
-            adjacentLeft_index = horzcat(adjacentLeft.refAttribute);
-            adjacent_index = [adjacent_index,adjacentLeft_index];
-        end
-        
-        if isfield(adjacentRight,'refAttribute')
-            adjacentRight_index = horzcat(adjacentRight.refAttribute);
-            adjacent_index = [adjacent_index,adjacentRight_index];
-        end
+%         if isfield(adjacentLeft,'refAttribute')
+%             adjacentLeft_index = horzcat(adjacentLeft.refAttribute);
+%             adjacent_index = [adjacent_index,adjacentLeft_index];
+%         end
+%         
+%         if isfield(adjacentRight,'refAttribute')
+%             adjacentRight_index = horzcat(adjacentRight.refAttribute);
+%             adjacent_index = [adjacent_index,adjacentRight_index];
+%         end
 
         adj(i,adjacent_index) = 1;
      
     end
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-       
+  
+    %% intersection_lanelets   
     % check the lanelets at intersection and assign them to be adjacent
     
     Nintersections = length(commonroad_data.intersection); 
@@ -234,6 +241,140 @@ function [lanelets,adjacency,intersection_lanelets,boundary,commonroad] = common
     adjacency = (adjacency>0);
     
 %     save('adjacency.mat','adjacency')
+
+    %% lanelets boundary
+    lanelet_boundary = cell(1,Nlanelets);
+    for lanelet = 1:Nlanelets
+        left_bound = [];
+        right_bound = [];
+        adjacentLeft = commonroad_data.lanelet(lanelet).adjacentLeft;
+        adjacentRight = commonroad_data.lanelet(lanelet).adjacentRight;
+
+        % if the lanelet has adjacent left lanelet, the boundary
+        % should be the leftBound of adjacentLeft and rightBound of
+        % the current lanelet
+        if isfield(adjacentLeft,'refAttribute')
+            adjacentLeft_index = horzcat(adjacentLeft.refAttribute);
+            left_bound_x = lanelets{ adjacentLeft_index }(:,LaneletInfo.lx);
+            left_bound_y = lanelets{ adjacentLeft_index }(:,LaneletInfo.ly);
+            left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
+
+            right_bound_x = lanelets{ lanelet }(:,LaneletInfo.rx);
+            right_bound_y = lanelets{ lanelet }(:,LaneletInfo.ry);
+            right_bound = [right_bound_x(1:end),right_bound_y(1:end)];                           
+        end
+
+        if isfield(adjacentRight,'refAttribute')
+            adjacentRight_index = horzcat(adjacentRight.refAttribute);
+            left_bound_x = lanelets{ lanelet }(:,LaneletInfo.lx);
+            left_bound_y = lanelets{ lanelet }(:,LaneletInfo.ly);
+            left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
+
+            right_bound_x = lanelets{ adjacentRight_index }(:,LaneletInfo.rx);
+            right_bound_y = lanelets{ adjacentRight_index }(:,LaneletInfo.ry);
+            right_bound = [right_bound_x(1:end),right_bound_y(1:end)];
+        end
+
+        lanelet_boundary{lanelet} = {left_bound,right_bound};
+    end
+    
+    % update the boundary for lanelets at the turning corner
+    corner1 = [3,4,22];
+    corner2 = [5,6,23];
+    corner3 = [29,30,48];
+    corner4 = [31,32,49];
+    corner5 = [57,58,75];
+    corner6 = [55,56,74];
+    corner7 = [81,82,100];
+    corner8 = [83,84,101];
+    
+    for lanelet = corner1
+        left_bound_x = lanelets{ 4 }(:,LaneletInfo.lx);
+        left_bound_y = lanelets{ 4 }(:,LaneletInfo.ly);
+        left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
+
+        right_bound_x = lanelets{ 22 }(:,LaneletInfo.rx);
+        right_bound_y = lanelets{ 22 }(:,LaneletInfo.ry);
+        right_bound = [right_bound_x(1:end),right_bound_y(1:end)];  
+        lanelet_boundary{lanelet} = {left_bound,right_bound};
+    end
+    
+    for lanelet = corner2
+        left_bound_x = lanelets{ 6 }(:,LaneletInfo.lx);
+        left_bound_y = lanelets{ 6 }(:,LaneletInfo.ly);
+        left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
+
+        right_bound_x = lanelets{ 23 }(:,LaneletInfo.rx);
+        right_bound_y = lanelets{ 23 }(:,LaneletInfo.ry);
+        right_bound = [right_bound_x(1:end),right_bound_y(1:end)];  
+        lanelet_boundary{lanelet} = {left_bound,right_bound};
+    end
+    
+    for lanelet = corner3
+        left_bound_x = lanelets{ 30 }(:,LaneletInfo.lx);
+        left_bound_y = lanelets{ 30 }(:,LaneletInfo.ly);
+        left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
+
+        right_bound_x = lanelets{ 48 }(:,LaneletInfo.rx);
+        right_bound_y = lanelets{ 48 }(:,LaneletInfo.ry);
+        right_bound = [right_bound_x(1:end),right_bound_y(1:end)];   
+        lanelet_boundary{lanelet} = {left_bound,right_bound};
+    end
+    
+    for lanelet = corner4
+        left_bound_x = lanelets{ 32 }(:,LaneletInfo.lx);
+        left_bound_y = lanelets{ 32 }(:,LaneletInfo.ly);
+        left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
+
+        right_bound_x = lanelets{ 49 }(:,LaneletInfo.rx);
+        right_bound_y = lanelets{ 49 }(:,LaneletInfo.ry);
+        right_bound = [right_bound_x(1:end),right_bound_y(1:end)]; 
+        lanelet_boundary{lanelet} = {left_bound,right_bound};
+    end    
+    
+    for lanelet = corner5
+        left_bound_x = lanelets{ 58 }(:,LaneletInfo.lx);
+        left_bound_y = lanelets{ 58 }(:,LaneletInfo.ly);
+        left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
+
+        right_bound_x = lanelets{ 75 }(:,LaneletInfo.rx);
+        right_bound_y = lanelets{ 75 }(:,LaneletInfo.ry);
+        right_bound = [right_bound_x(1:end),right_bound_y(1:end)]; 
+        lanelet_boundary{lanelet} = {left_bound,right_bound};
+    end    
+
+    for lanelet = corner6        
+        left_bound_x = lanelets{ 56 }(:,LaneletInfo.lx);
+        left_bound_y = lanelets{ 56 }(:,LaneletInfo.ly);
+        left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
+
+        right_bound_x = lanelets{ 74 }(:,LaneletInfo.rx);
+        right_bound_y = lanelets{ 74 }(:,LaneletInfo.ry);
+        right_bound = [right_bound_x(1:end),right_bound_y(1:end)]; 
+        lanelet_boundary{lanelet} = {left_bound,right_bound};
+    end    
+
+    for lanelet = corner7       
+        left_bound_x = lanelets{ 82 }(:,LaneletInfo.lx);
+        left_bound_y = lanelets{ 82 }(:,LaneletInfo.ly);
+        left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
+
+        right_bound_x = lanelets{ 100 }(:,LaneletInfo.rx);
+        right_bound_y = lanelets{ 100 }(:,LaneletInfo.ry);
+        right_bound = [right_bound_x(1:end),right_bound_y(1:end)]; 
+        lanelet_boundary{lanelet} = {left_bound,right_bound};
+    end 
+    
+    for lanelet = corner8         
+        left_bound_x = lanelets{ 84 }(:,LaneletInfo.lx);
+        left_bound_y = lanelets{ 84 }(:,LaneletInfo.ly);
+        left_bound = [left_bound_x(1:end),left_bound_y(1:end)];
+
+        right_bound_x = lanelets{ 101 }(:,LaneletInfo.rx);
+        right_bound_y = lanelets{ 101 }(:,LaneletInfo.ry);
+        right_bound = [right_bound_x(1:end),right_bound_y(1:end)]; 
+        lanelet_boundary{lanelet} = {left_bound,right_bound};
+    end   
     
 end
 
