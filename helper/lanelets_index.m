feb4b05250082606d28ff1734aa6f131ab581cb9
Bingyi Su <bingyi.su@rwth-aachen.de>
2022-01-01 22:22:26 +0100

    modified the way to calculate lanelet index for each vehicle

--- helper/lanelets_index.m
+++ helper/lanelets_index.m
@@ -11,5 +11,23 @@ function lanelets_idx = lanelets_index(veh_idx,iter,scenario)
     end 
     
     lanelets_idx = scenario.vehicles(1,veh_idx).lanelets_index(nlanelets_idx);
+    
+    
+    
+%     nlanelets_i = [];
+%     stop_flag = false;
+% 
+%     ref_points_index_i = reshape(iter.referenceTrajectoryIndex(i,:,:),Hp,1);
+%     ref_points_i = reshape(iter.referenceTrajectoryPoints(i,:,:),Hp,2);
+% 
+%     for n = 1:length(ref_points_index_i)
+%         nlanelets_i = [ nlanelets_i, sum(ref_points_index_i(n) > scenario.vehicles(1,i).points_index)+1];
+%     end              
+%     nlanelets_i = unique(nlanelets_i);
+%     lanelets_index_i = scenario.vehicles(1,i).lanelets_index(nlanelets_i);
+    
+    
+    
         
-end           
+end     
+
