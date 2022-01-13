c0c42ee937c4cc9a340e70c9a29fdfc795e15541
Bingyi Su <bingyi.su@rwth-aachen.de>
2022-01-13 04:13:24 +0100

    priority at intersection keeps relatively constant

--- priority/right_of_way_update.m
+++ priority/right_of_way_update.m
@@ -27,7 +27,7 @@ classdef  right_of_way_update < interface_priority
             Veh = 1:nVeh;
             veh_at_intersection = [];
             
-            %% identify vehicles at intersection
+            %% identify vehicles at intersection and assign priorities accordingly
             for veh_idx = 1:nVeh   
                 lanelets_idx(veh_idx,:) = lanelets_index(veh_idx,obj.iter,obj.scenario);
                 is_at_intersection = (sum(ismember(lanelets_idx(veh_idx,:), obj.scenario.intersection_lanelets)) > 0);
@@ -42,15 +42,15 @@ classdef  right_of_way_update < interface_priority
             % assign priorities to vehicles at the intersection
             if ~isempty(veh_at_intersection)
                 
-                if isempty(last_veh_at_intersection)
+                if isempty(last_veh_at_intersection) 
                     disp('come on !!!!!!!!!!!!')
+                    % if there were no vehicles at intersection at last step
+                    % sort the vehicles at intersection based on their distance to the intersection center
+                    % and assign higher priority to the vehicle closer to the intersection center
                     first_referenceTrajectoryPoints = [obj.iter.referenceTrajectoryPoints(veh_at_intersection,1,1),obj.iter.referenceTrajectoryPoints(veh_at_intersection,1,2)];
                     diff = first_referenceTrajectoryPoints - intersection_center;
                     distance_to_center = sqrt(diff(:,1).^2 + diff(:,2).^2);
                     [~, index] = sort(distance_to_center,'ascend');
-
-                    % sort the vehicles at intersection based on their distance to the intersection center
-                    % and assign higher priority to the vehicle closer to the intersection center
                     veh_at_intersection = veh_at_intersection(index);
                     priority(veh_at_intersection)= 1:n_veh_at_intersection;
 
@@ -58,8 +58,9 @@ classdef  right_of_way_update < interface_priority
                     prio = n_veh_at_intersection + 1;
                 else
                     disp(['last_veh_at_intersection: ', num2str(last_veh_at_intersection)])
-                    % all remained vehicles at intersection keep higher priorities, and then assign priorities to new vehicles
-                    % driving into intersection
+                    % if there were vehicles at intersection at last step 
+                    % all remained vehicles at intersection at current step keep higher priorities, and then assign lower priorities 
+                    % to new vehicles coming into intersection based on their distance to the intersection center
                     [~, idx,~] = intersect(last_veh_at_intersection,veh_at_intersection);
                     remained_veh_at_intersection = last_veh_at_intersection(sort(idx));
                     n_remained_veh_at_intersection = length(remained_veh_at_intersection);
@@ -70,7 +71,7 @@ classdef  right_of_way_update < interface_priority
                     % new incoming vehicles at intersection
                     new_veh_at_intersection = setdiff(veh_at_intersection, remained_veh_at_intersection);
 
-                    % assign priorities to new vehicles at intersection
+                    % assign priorities to new vehicles at intersection based on their distance to the intersection center
                     if ~isempty(new_veh_at_intersection)
                         n_new_veh_at_intersection = length(new_veh_at_intersection);
                         first_referenceTrajectoryPoints = [obj.iter.referenceTrajectoryPoints(new_veh_at_intersection,1,1),obj.iter.referenceTrajectoryPoints(new_veh_at_intersection,1,2)];
@@ -92,8 +93,9 @@ classdef  right_of_way_update < interface_priority
                 
             end
             
-            %%
-            % update the adjacency matrix for vehicles not at the intersection
+            %% assign priorities to vehicles not at intersection
+            
+            % adjacency matrix
             adjacency= obj.scenario.adjacency(:,:,end);         
 
             
@@ -106,27 +108,26 @@ classdef  right_of_way_update < interface_priority
                     continue
                 end
                 
+                % check adjacent vehicles (only vehicles not at intersection)
                 veh_adjacent = find(adjacency(nveh,:));
                 veh_adjacent_not_at_intersection = setdiff(veh_adjacent, veh_at_intersection);
-                %check the vehicles whose index is larger than the current
-                %vehicle, no repeated check
+                
+                %check the vehicles whose index is larger than the current vehicle, no repeated check
                 veh_adjacent_not_at_intersection = veh_adjacent_not_at_intersection(veh_adjacent_not_at_intersection > nveh);
-                for iveh = veh_adjacent_not_at_intersection
-                    
+                
+                for iveh = veh_adjacent_not_at_intersection  
                     is_leading_vehicle = check_driving_order(obj.iter, nveh, iveh);
                     if is_leading_vehicle
                         adjacency(nveh, iveh) = 1;
                         adjacency(iveh, nveh) = 0;
                     else
                         adjacency(nveh, iveh) = 0;
-                        adjacency(iveh, nveh) = 1;
-                        
-                    end
-                    
-                end
-            
+                        adjacency(iveh, nveh) = 1;            
+                    end                  
+                end         
             end
             
+            % update the directed adajacency matrix 
             directed_adjacency_not_at_intersection = adjacency;            
             directed_adjacency_not_at_intersection(veh_at_intersection,:) = [];        
             directed_adjacency_not_at_intersection(:,veh_at_intersection) = [];
@@ -138,10 +139,24 @@ classdef  right_of_way_update < interface_priority
             end
 
                 
-            Graph = digraph(directed_adjacency_not_at_intersection,names);
+            Graph = digraph(directed_adjacency_not_at_intersection);
             
+            %check if there is any cycles in the directed graph
+            cycles = allcycles(Graph);
+            if ~isempty(cycles)
+                % break the cycle
+                disp('there are cycles in the directed graph')
+                cyclic_vehicles = cycles{:};
+                i = cyclic_vehicles(1);
+                j = cyclic_vehicles(end);
+                directed_adjacency_not_at_intersection(i,j) = 0;
+                directed_adjacency_not_at_intersection(j,i) = 0;
+                Graph = digraph(directed_adjacency_not_at_intersection);
+                priority_index  = toposort(Graph);% compare the function from helper/kahn.m
+            else
+                priority_index  = toposort(Graph);
+            end
             
-            priority_index  = toposort(Graph);
             priority_not_intersection = veh_not_at_intersection(priority_index); % ordered vehicle index w.r.t. priority
             
             priority(priority_not_intersection) = prio:nVeh;
