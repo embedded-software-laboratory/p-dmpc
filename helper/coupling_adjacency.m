d2f112ad6692a7bc442640480b703ab6353422d0
Bingyi Su <bingyi.su@rwth-aachen.de>
2022-02-02 20:23:51 +0100

    modified the output of commonroad_lanelets accordingly

--- helper/coupling_adjacency.m
+++ helper/coupling_adjacency.m
@@ -1,16 +1,17 @@
-function adjacency = coupling_adjacency(scenario, iter)
+function [adjacency, semi_adjacency] = coupling_adjacency(scenario, iter)
 % COUPLING_ADJACENCY returns the adjacency matrix based on predicted
 % trajectories. If the future trajectories are in two ajacent lanelets, the two vehicles are considered as adjacent.
     
     nVeh = size(iter.referenceTrajectoryPoints,1);
     Hp = size(iter.referenceTrajectoryPoints,2);
     adjacency = zeros(nVeh,nVeh);
-    [~,adjacent,~] = commonroad_lanelets();
+    semi_adjacency = zeros(nVeh,nVeh);
+    [~,adjacent,semi_adjacent,~,~,~,~] = commonroad_lanelets();
 
-    for i = 1:nVeh-1
-        nlanelets_i = [];
-        stop_flag = false;
-        
+    for i = 1:(nVeh-1)
+%         stop_flag = false;
+        nlanelets_i = [];   
+%         disp(['i is: ', num2str(i)])
         ref_points_index_i = reshape(iter.referenceTrajectoryIndex(i,:,:),Hp,1);
         ref_points_i = reshape(iter.referenceTrajectoryPoints(i,:,:),Hp,2);
         
@@ -19,8 +20,11 @@ function adjacency = coupling_adjacency(scenario, iter)
         end              
         nlanelets_i = unique(nlanelets_i);
         lanelets_index_i = scenario.vehicles(1,i).lanelets_index(nlanelets_i);
+%         disp(['lanelets i: ',num2str(lanelets_index_i)])
         
-        for j = nVeh:-1:i+1
+        for j = (i+1):nVeh
+            stop_flag = false;
+%             disp(['j is: ', num2str(j)])
             nlanelets_j = [];
             ref_points_index_j = reshape(iter.referenceTrajectoryIndex(j,:,:),Hp,1);
             ref_points_j = reshape(iter.referenceTrajectoryPoints(j,:,:),Hp,2);           
@@ -29,14 +33,18 @@ function adjacency = coupling_adjacency(scenario, iter)
             end              
             nlanelets_j = unique(nlanelets_j);
             lanelets_index_j = scenario.vehicles(1,j).lanelets_index(nlanelets_j);
-            for k_i = 1:length(lanelets_index_i)                
-                for k_j = 1:length(lanelets_index_j)                   
+%             disp(['lanelets j: ',num2str(lanelets_index_j)])
+            
+            for k_i = 1:length(lanelets_index_i)  
+                for k_j = 1:length(lanelets_index_j)   
+                    stop_flag = false;
                     % check if lanelets are adjacent
                     if adjacent(lanelets_index_i(k_i),lanelets_index_j(k_j)) == 1 
                         % check if predicted references are within defined distance
-                        for k = 1:Hp            
+                        for k = 1:Hp 
+%                             disp(['k is: ',num2str(k)])
                             dist = norm((ref_points_i(k,:)-ref_points_j(k,:)),2);                    
-                            if dist < 1.2
+                            if dist < 1.5
                                 adjacency(i,j) = 1;
                                 adjacency(j,i) = 1;
                                 stop_flag = true;
@@ -44,16 +52,35 @@ function adjacency = coupling_adjacency(scenario, iter)
                             if stop_flag
                                 break
                             end
-                        end  
-                        stop_flag = true;                        
+                        end                        
                     end
+                    
+                    % check if lanelets are semi_adjacent
+                    if semi_adjacent(lanelets_index_i(k_i),lanelets_index_j(k_j)) == 1 
+                        % check if predicted references are within defined distance
+                        for k = 1:Hp 
+%                             disp(['k is: ',num2str(k)])
+                            dist = norm((ref_points_i(k,:)-ref_points_j(k,:)),2);                    
+                            if dist < 1.5
+                                semi_adjacency(i,j) = 1;
+                                semi_adjacency(j,i) = 1;
+                                stop_flag = true;
+                            end
+                            if stop_flag
+                                break
+                            end
+                        end                        
+                    end
+                    
                     if stop_flag
                         break
                     end
-                end                
+                end   
+%                 disp('i and j are adjacent .............................')
                 if stop_flag
                     break
                 end
+                
             end           
         end
     end
