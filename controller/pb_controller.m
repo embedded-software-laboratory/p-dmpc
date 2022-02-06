f50c80a03e2a44fa4da6f95dfc1935adfe9b3206
Bingyi Su <bingyi.su@rwth-aachen.de>
2022-02-06 20:27:03 +0100

    added FCA

--- controller/pb_controller.m
+++ controller/pb_controller.m
@@ -9,7 +9,9 @@ function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(sc
 %     obj = topo_priority(scenario);
 %     groups = obj.priority();   
 %     obj = random_priority(scenario);
-%     groups = obj.priority();  
+%     groups = obj.priority(); 
+%     obj = FCA(scenario,iter);
+%     [veh_at_intersection, groups] = obj.priority();
 
     obj = right_of_way_update3(scenario,iter);
     [veh_at_intersection, groups] = obj.priority(last_veh_at_intersection); 
@@ -36,14 +38,20 @@ function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(sc
     info.n_expanded = 0;
     
     sub_controller = @(scenario, iter)...
-        graph_search(scenario, iter);    
+        graph_search(scenario, iter); 
+    adjacency = scenario.adjacency(:,:,end);
 
     for grp_idx = 1:length(groups)
         group = groups(grp_idx);
         for grp_member_idx = 1:length(group.members) 
             subcontroller_timer = tic;
             vehicle_idx = group.members(grp_member_idx);
-%             disp(['the vehicle is going to search: ',num2str(vehicle_idx)])
+            disp(['the vehicle is going to search: ',num2str(vehicle_idx)])
+            
+%             % Filter out vehicles that are not adjacent
+            veh_adjacent = find(scenario.adjacency(vehicle_idx,:,end));
+            group.predecessors = intersect(group.predecessors,veh_adjacent);
+            
             % Filter out vehicles with lower or same priority.
             priority_filter = false(1,scenario.nVeh);
             priority_filter(group.predecessors) = true; % keep all with higher priority
@@ -58,6 +66,23 @@ function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(sc
             % add predicted trajecotries as obstacle
             [scenario_v, iter_v] = vehicles_as_obstacles(scenario_filtered, iter_filtered, v2o_filter, info.shapes(group.predecessors,:));
 
+            
+            % add adjacent vehicles with lower priorities as static obstacles
+%            
+            adjacent_vehicle_lower_priority = setdiff(veh_adjacent,group.predecessors);
+            
+            for i = 1:length(adjacent_vehicle_lower_priority)
+                veh_index = adjacent_vehicle_lower_priority(i);
+                x0 = iter.x0(veh_index,1);
+                y0 = iter.x0(veh_index,2);
+                yaw0 = iter.x0(veh_index,3);
+                veh = Vehicle();
+                x_locals = [-1, -1,  1,  1] * (veh.Length/2+0.01);
+                y_locals = [-1,  1,  1, -1] * (veh.Width/2+0.01);
+                [x_globals,y_globals] = translate_global(yaw0, x0, y0, x_locals, y_locals);
+                scenario_v.obstacles{end+1} = [x_globals;y_globals];
+            end
+            
             % execute sub controller for 1-veh scenario
             [u_v,y_pred_v,info_v, MEflag] = sub_controller(scenario_v, iter_v);
 %             disp('Search finished')
@@ -80,6 +105,9 @@ function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(sc
             y_pred{vehicle_idx,1} = y_pred_v{:};
             u(vehicle_idx) = u_v(1);
         end
+%         if grp_idx > 4
+%             a
+%         end
 
         if MEflag 
             break
