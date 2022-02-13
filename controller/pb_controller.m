7b2ed48bb063e47d5e66343bb2409fa410df0366
Bingyi Su <bingyi.su@rwth-aachen.de>
2022-02-13 21:23:25 +0100

    modified variable names

--- controller/pb_controller.m
+++ controller/pb_controller.m
@@ -1,4 +1,4 @@
-function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(scenario, iter, last_veh_at_intersection)
+function [u, y_pred, info, priority_list,veh_at_intersection,computation_levels,edge_to_break] = pb_controller(scenario, iter, last_veh_at_intersection)
 % PB_CONTROLLER    Plan trajectory for one time step using a priority-based controller.
 %     Controller simulates multiple distributed controllers.
 
@@ -14,7 +14,11 @@ function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(sc
 %     [veh_at_intersection, groups] = obj.priority();
 
     obj = right_of_way_update3(scenario,iter);
-    [veh_at_intersection, groups] = obj.priority(last_veh_at_intersection); 
+    [veh_at_intersection, groups,edge_to_break] = obj.priority(last_veh_at_intersection); 
+    
+    computation_levels = length(groups);
+    disp(['computation_levels: ',num2str(computation_levels)])
+    
     
     members_list = horzcat(groups.members);
     nVeh = length(members_list); 
@@ -25,6 +29,8 @@ function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(sc
         priority_list(iVeh) = prio;
         prio = prio + 1;
     end
+%     disp(['member list: ', num2str(members_list)])
+%     disp(['priority list: ', num2str(priority_list)])
 
 
     y_pred = cell(scenario.nVeh,1);
@@ -39,7 +45,6 @@ function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(sc
     
     sub_controller = @(scenario, iter)...
         graph_search(scenario, iter); 
-    adjacency = scenario.adjacency(:,:,end);
 
     for grp_idx = 1:length(groups)
         group = groups(grp_idx);
@@ -47,14 +52,14 @@ function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(sc
             subcontroller_timer = tic;
             vehicle_idx = group.members(grp_member_idx);
             disp(['the vehicle is going to search: ',num2str(vehicle_idx)])
-            
+%             disp(['group.predecessores: ',num2str(group.predecessors)])
 %             % Filter out vehicles that are not adjacent
             veh_adjacent = find(scenario.adjacency(vehicle_idx,:,end));
-            group.predecessors = intersect(group.predecessors,veh_adjacent);
-            
+            predecessors = intersect(group.predecessors,veh_adjacent);
+%             disp(['group.predecessores: ',num2str(group.predecessors)])
             % Filter out vehicles with lower or same priority.
             priority_filter = false(1,scenario.nVeh);
-            priority_filter(group.predecessors) = true; % keep all with higher priority
+            priority_filter(predecessors) = true; % keep all with higher priority
             priority_filter(vehicle_idx) = true; % keep self
             scenario_filtered = filter_scenario(scenario, priority_filter);
             iter_filtered = filter_iter(iter, priority_filter);
@@ -64,12 +69,12 @@ function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(sc
             v2o_filter(self_index) = false;
 
             % add predicted trajecotries as obstacle
-            [scenario_v, iter_v] = vehicles_as_obstacles(scenario_filtered, iter_filtered, v2o_filter, info.shapes(group.predecessors,:));
+            [scenario_v, iter_v] = vehicles_as_obstacles(scenario_filtered, iter_filtered, v2o_filter, info.shapes(predecessors,:));
 
             
             % add adjacent vehicles with lower priorities as static obstacles
-%            
-            adjacent_vehicle_lower_priority = setdiff(veh_adjacent,group.predecessors);
+           
+            adjacent_vehicle_lower_priority = setdiff(veh_adjacent,predecessors);
             
             for i = 1:length(adjacent_vehicle_lower_priority)
                 veh_index = adjacent_vehicle_lower_priority(i);
@@ -114,6 +119,9 @@ function [u, y_pred, info, priority_list,veh_at_intersection] = pb_controller(sc
         end
 
     end
-  
     
+  
+%    a=1; 
+%    b=a;
+   
 end
