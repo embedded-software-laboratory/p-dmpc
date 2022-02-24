bb7b246f14213cedeea4d8d350e0bfd9bdf981da
Bingyi Su <bingyi.su@rwth-aachen.de>
2022-02-24 06:37:42 +0100

    changed reference point to check driving order

--- helper/check_driving_order.m
+++ helper/check_driving_order.m
@@ -10,23 +10,23 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
         current_point_iveh_y = iter.x0(iveh,2);
         current_point_iveh = [current_point_iveh_x,current_point_iveh_y];
         
-        intersection1 = [20,21,78,69,70,77];
-        intersection1_end = [scenario.lanelets{69}(end,LaneletInfo.lx),scenario.lanelets{69}(end,LaneletInfo.ly)];
-        intersection2 = [18,25,19,24,46,47];
-        intersection2_end = [scenario.lanelets{19}(end,LaneletInfo.lx),scenario.lanelets{19}(end,LaneletInfo.ly)];
-        intersection3 = [44,51,43,52,98,99];
-        intersection3_end = [scenario.lanelets{43}(end,LaneletInfo.lx),scenario.lanelets{43}(end,LaneletInfo.ly)];
-        intersection4 = [96,103,97,102,72,73];
-        intersection4_end = [scenario.lanelets{97}(end,LaneletInfo.lx),scenario.lanelets{97}(end,LaneletInfo.ly)];
+        intersection1 = [20,21,78,69,104,95,70,77];
+        intersection1_end = [scenario.lanelets{69}(end,LaneletInfo.rx),scenario.lanelets{69}(end,LaneletInfo.ry)];
+        intersection2 = [18,25,19,24,71,76,46,47];
+        intersection2_end = [scenario.lanelets{19}(end,LaneletInfo.rx),scenario.lanelets{19}(end,LaneletInfo.ry)];
+        intersection3 = [44,51,43,52,17,26,98,99];
+        intersection3_end = [scenario.lanelets{43}(end,LaneletInfo.rx),scenario.lanelets{43}(end,LaneletInfo.ry)];
+        intersection4 = [96,103,97,102,45,50,72,73];
+        intersection4_end = [scenario.lanelets{97}(end,LaneletInfo.rx),scenario.lanelets{97}(end,LaneletInfo.ry)];
         
         merging1 = [1,2,15,16,3,4,22];
-        merging1_end = [scenario.lanelets{3}(end,LaneletInfo.lx),scenario.lanelets{3}(end,LaneletInfo.ly)];
+        merging1_end = [scenario.lanelets{4}(end,LaneletInfo.cx),scenario.lanelets{4}(end,LaneletInfo.cy)];
         merging2 = [59,60,61,62,57,58,75];
-        merging2_end = [scenario.lanelets{57}(end,LaneletInfo.lx),scenario.lanelets{57}(end,LaneletInfo.ly)];
+        merging2_end = [scenario.lanelets{58}(end,LaneletInfo.cx),scenario.lanelets{58}(end,LaneletInfo.cy)];
         merging3 = [79,80,93,94,81,82,100];
-        merging3_end = [scenario.lanelets{81}(end,LaneletInfo.lx),scenario.lanelets{81}(end,LaneletInfo.ly)];
+        merging3_end = [scenario.lanelets{82}(end,LaneletInfo.cx),scenario.lanelets{82}(end,LaneletInfo.cy)];
         merging4 = [33,34,35,36,31,32,49];
-        merging4_end = [scenario.lanelets{31}(end,LaneletInfo.lx),scenario.lanelets{31}(end,LaneletInfo.ly)];
+        merging4_end = [scenario.lanelets{32}(end,LaneletInfo.cx),scenario.lanelets{32}(end,LaneletInfo.cy)];
         
 
 %         % check the lanelet index for nveh
@@ -40,6 +40,7 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
 %         nlanelets_nveh = unique(nlanelets_nveh);
 %         lanelets_index_nveh = scenario.vehicles(1,nveh).lanelets_index(nlanelets_nveh); 
         lanelets_index_nveh = scenario.vehicles(1,nveh).predicted_lanelets;
+%         disp(['lanelets_index_nveh',num2str(lanelets_index_nveh)])
        
 %         % check the lanelet index for iveh
 %         nlanelets_iveh = [];   
@@ -52,64 +53,85 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
 %         lanelets_index_iveh = scenario.vehicles(1,iveh).lanelets_index(nlanelets_iveh);
         lanelets_index_iveh = scenario.vehicles(1,iveh).predicted_lanelets;
         
-        if sum(ismember(lanelets_index_nveh,intersection1)) && sum(ismember(lanelets_index_iveh,intersection1))
+        if sum(ismember(lanelets_index_nveh,intersection1))>0 && sum(ismember(lanelets_index_iveh,intersection1))>0
+%             disp('11111111111111111111111111111111111111111111111111111111111111111111111')
             %check the distance to the end of center of lanelets 69 and 78
             distance_nveh = norm(intersection1_end-current_point_nveh);
             distance_iveh = norm(intersection1_end-current_point_iveh);
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
             end
-        elseif sum(ismember(lanelets_index_nveh,intersection2)) && sum(ismember(lanelets_index_iveh,intersection2))
+        elseif sum(ismember(lanelets_index_nveh,intersection2))>0 && sum(ismember(lanelets_index_iveh,intersection2))>0
+%             disp('22222222222222222222222222222222222222222222222222222222222222222222222')
             %check the distance to the end of center of lanelets 69 and 78
             distance_nveh = norm(intersection2_end-current_point_nveh);
             distance_iveh = norm(intersection2_end-current_point_iveh);
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
             end            
-        elseif sum(ismember(lanelets_index_nveh,intersection3)) && sum(ismember(lanelets_index_iveh,intersection3))
+        elseif sum(ismember(lanelets_index_nveh,intersection3))>0 && sum(ismember(lanelets_index_iveh,intersection3))>0
+%             disp('333333333333333333333333333333333333333333333333333333333333333333333333')
             %check the distance to the end of center of lanelets 69 and 78
             distance_nveh = norm(intersection3_end-current_point_nveh);
             distance_iveh = norm(intersection3_end-current_point_iveh);
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
             end            
-        elseif sum(ismember(lanelets_index_nveh,intersection4)) && sum(ismember(lanelets_index_iveh,intersection4))
+        elseif sum(ismember(lanelets_index_nveh,intersection4))>0 && sum(ismember(lanelets_index_iveh,intersection4))>0
+%             disp('4444444444444444444444444444444444444444444444444444444444444444444444444')
             %check the distance to the end of center of lanelets 69 and 78
             distance_nveh = norm(intersection4_end-current_point_nveh);
             distance_iveh = norm(intersection4_end-current_point_iveh);
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
             end  
-        elseif sum(ismember(lanelets_index_nveh,merging1)) && sum(ismember(lanelets_index_iveh,merging1))
+        elseif sum(ismember(lanelets_index_nveh,merging1))>0 && sum(ismember(lanelets_index_iveh,merging1))>0
             %check the distance to the end of center of lanelets 1 and 2
             distance_nveh = norm(merging1_end-current_point_nveh);
             distance_iveh = norm(merging1_end-current_point_iveh);
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
             end  
+%             disp('55555555555555555555555555555555555555555555555555555555555555555555555555')
+%             if current_point_nveh_x > current_point_iveh_x 
+%                 is_leading_vehicle = true;
+%             end 
         elseif sum(ismember(lanelets_index_nveh,merging2)) && sum(ismember(lanelets_index_iveh,merging2))
             %check the distance to the end of center of lanelets 59 and 60
             distance_nveh = norm(merging2_end-current_point_nveh);
             distance_iveh = norm(merging2_end-current_point_iveh);
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
-            end             
+            end    
+%             disp('66666666666666666666666666666666666666666666666666666666666666666666666666')
+%             if current_point_nveh_y < current_point_iveh_y 
+%                 is_leading_vehicle = true;
+%             end   
         elseif sum(ismember(lanelets_index_nveh,merging3)) && sum(ismember(lanelets_index_iveh,merging3))
             %check the distance to the end of center of lanelets 79 and 80
             distance_nveh = norm(merging3_end-current_point_nveh);
             distance_iveh = norm(merging3_end-current_point_iveh);
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
-            end             
+            end            
+%             disp('7777777777777777777777777777777777777777777777777777777777777777777777777777777')
+%             if current_point_nveh_x < current_point_iveh_x
+%                 is_leading_vehicle = true;
+%             end 
         elseif sum(ismember(lanelets_index_nveh,merging4)) && sum(ismember(lanelets_index_iveh,merging4))
             %check the distance to the end of center of lanelets 33 and 34
             distance_nveh = norm(merging4_end-current_point_nveh);
             distance_iveh = norm(merging4_end-current_point_iveh);
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
-            end             
-            
+            end    
+%             disp('88888888888888888888888888888888888888888888888888888888888888888888888888888888')
+%             if current_point_nveh_y > current_point_iveh_y
+%                 is_leading_vehicle = true;
+%             end 
+%             
         else
+%             disp('啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦')
             first_refPoint_nveh_x = iter.referenceTrajectoryPoints(nveh,1,1);
             first_refPoint_nveh_y = iter.referenceTrajectoryPoints(nveh,1,2);
             first_refPoint_nveh = [first_refPoint_nveh_x,first_refPoint_nveh_y];
