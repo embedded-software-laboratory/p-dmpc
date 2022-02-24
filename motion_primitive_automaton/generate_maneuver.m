5837e8d45b23191fff01830b9308f0478bc2bbb9
Bingyi Su <bingyi.su@rwth-aachen.de>
2022-02-24 06:44:05 +0100

    added maneuver for check lanelet boundary collision at the last step of the prediction horizon

--- motion_primitive_automaton/generate_maneuver.m
+++ motion_primitive_automaton/generate_maneuver.m
@@ -88,6 +88,32 @@ function maneuver = generate_maneuver(model, trim1, trim2, offset_length, offset
     maneuver.area_without_offset = maneuver_area_without_offset;
     
     
+    x_rec1_without_offset = [-1, -1,  1,  1] * (veh.Length/2 + 0.05);
+    y_rec1_without_offset = [-1,  1,  1, -1] * (veh.Width/2 + 0.01);
+        
+    % calculate displacement of model shape
+    [x_rec2_without_offset, y_rec2_without_offset] = translate_global(maneuver.dyaw, maneuver.dx, maneuver.dy, x_rec1_without_offset, y_rec1_without_offset);
+    
+    signum = sign(maneuver.dyaw);
+    
+    switch signum
+        case 0
+            maneuver_area_without_offset = [   x_rec1_without_offset(1) x_rec1_without_offset(2) x_rec2_without_offset(3) x_rec2_without_offset(4); ...
+                                               y_rec1_without_offset(1) y_rec1_without_offset(2) y_rec2_without_offset(3) y_rec2_without_offset(4)   ];
+        case 1
+            lastX = x_rec2_without_offset(4);
+            lastY = y_rec1_without_offset(4);
+            maneuver_area_without_offset = [   x_rec1_without_offset(1) x_rec1_without_offset(2) x_rec2_without_offset(3) x_rec2_without_offset(4) lastX; ...
+                                               y_rec1_without_offset(1) y_rec1_without_offset(2) y_rec2_without_offset(3) y_rec2_without_offset(4) lastY   ];
+        case -1
+            lastX = x_rec2_without_offset(3);
+            lastY = y_rec1_without_offset(3);
+            maneuver_area_without_offset = [   x_rec1_without_offset(1) x_rec1_without_offset(2) lastX x_rec2_without_offset(3) x_rec2_without_offset(4); ...
+                                               y_rec1_without_offset(1) y_rec1_without_offset(2) lastY y_rec2_without_offset(3) y_rec2_without_offset(4)   ];
+    end
+    
+    maneuver.area_boundary_check = maneuver_area_without_offset;
+    
     
     
     
