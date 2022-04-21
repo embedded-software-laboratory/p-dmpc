d2134199b5eb952f13bf7ac9bd8b719e22593083
Bingyi Su <bingyi.su@rwth-aachen.de>
2022-04-21 05:10:22 +0200

    modified the comments

--- helper/check_driving_order.m
+++ helper/check_driving_order.m
@@ -1,4 +1,12 @@
 function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
+% CHECK_DRIVING_ORDER
+% 1. for vehicles merging into one lanelet on the highway or at the
+% intersection, the driving order is determined by cheking the distance to
+% the reference point; 2. for other cases, a general method of using the
+% dot product is used to determine the driving order
+
+% Return: is_leading_vehicle  --> true if nveh is driving ahead of iveh;
+%                                 false otherwise
         
         is_leading_vehicle = false;
         current_point_nveh_x = iter.x0(nveh,1);
@@ -10,6 +18,7 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
         current_point_iveh_y = iter.x0(iveh,2);
         current_point_iveh = [current_point_iveh_x,current_point_iveh_y];
         
+        % define the reference point for checking driving order
         intersection1 = [20,21,78,69,104,95,70,77];
         intersection1_end = [scenario.lanelets{69}(end,LaneletInfo.rx),scenario.lanelets{69}(end,LaneletInfo.ry)];
         intersection2 = [18,25,19,24,71,76,46,47];
@@ -29,32 +38,11 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
         merging4_end = [scenario.lanelets{32}(end,LaneletInfo.cx),scenario.lanelets{32}(end,LaneletInfo.cy)];
         
 
-%         % check the lanelet index for nveh
-%         nlanelets_nveh = [];   
-%         Hp = size(iter.referenceTrajectoryPoints,2);
-%         ref_points_index_i = reshape(iter.referenceTrajectoryIndex(nveh,:,:),Hp,1);
-%         
-%         for n = 1:length(ref_points_index_i)
-%             nlanelets_nveh = [ nlanelets_nveh, sum(ref_points_index_i(n) > scenario.vehicles(1,nveh).points_index)+1];
-%         end              
-%         nlanelets_nveh = unique(nlanelets_nveh);
-%         lanelets_index_nveh = scenario.vehicles(1,nveh).lanelets_index(nlanelets_nveh); 
-        lanelets_index_nveh = scenario.vehicles(1,nveh).predicted_lanelets;
-%         disp(['lanelets_index_nveh',num2str(lanelets_index_nveh)])
-       
-%         % check the lanelet index for iveh
-%         nlanelets_iveh = [];   
-%         ref_points_index_i = reshape(iter.referenceTrajectoryIndex(iveh,:,:),Hp,1);
-%         
-%         for n = 1:length(ref_points_index_i)
-%             nlanelets_iveh = [ nlanelets_iveh, sum(ref_points_index_i(n) > scenario.vehicles(1,iveh).points_index)+1];
-%         end              
-%         nlanelets_iveh = unique(nlanelets_iveh);
-%         lanelets_index_iveh = scenario.vehicles(1,iveh).lanelets_index(nlanelets_iveh);
+        lanelets_index_nveh = scenario.vehicles(1,nveh).predicted_lanelets;       
         lanelets_index_iveh = scenario.vehicles(1,iveh).predicted_lanelets;
         
+        
         if sum(ismember(lanelets_index_nveh,intersection1))>0 && sum(ismember(lanelets_index_iveh,intersection1))>0
-%             disp('11111111111111111111111111111111111111111111111111111111111111111111111')
             %check the distance to the end of center of lanelets 69 and 78
             distance_nveh = norm(intersection1_end-current_point_nveh);
             distance_iveh = norm(intersection1_end-current_point_iveh);
@@ -62,7 +50,6 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
                 is_leading_vehicle = true;
             end
         elseif sum(ismember(lanelets_index_nveh,intersection2))>0 && sum(ismember(lanelets_index_iveh,intersection2))>0
-%             disp('22222222222222222222222222222222222222222222222222222222222222222222222')
             %check the distance to the end of center of lanelets 69 and 78
             distance_nveh = norm(intersection2_end-current_point_nveh);
             distance_iveh = norm(intersection2_end-current_point_iveh);
@@ -70,7 +57,6 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
                 is_leading_vehicle = true;
             end            
         elseif sum(ismember(lanelets_index_nveh,intersection3))>0 && sum(ismember(lanelets_index_iveh,intersection3))>0
-%             disp('333333333333333333333333333333333333333333333333333333333333333333333333')
             %check the distance to the end of center of lanelets 69 and 78
             distance_nveh = norm(intersection3_end-current_point_nveh);
             distance_iveh = norm(intersection3_end-current_point_iveh);
@@ -78,7 +64,6 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
                 is_leading_vehicle = true;
             end            
         elseif sum(ismember(lanelets_index_nveh,intersection4))>0 && sum(ismember(lanelets_index_iveh,intersection4))>0
-%             disp('4444444444444444444444444444444444444444444444444444444444444444444444444')
             %check the distance to the end of center of lanelets 69 and 78
             distance_nveh = norm(intersection4_end-current_point_nveh);
             distance_iveh = norm(intersection4_end-current_point_iveh);
@@ -92,10 +77,6 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
             end  
-%             disp('55555555555555555555555555555555555555555555555555555555555555555555555555')
-%             if current_point_nveh_x > current_point_iveh_x 
-%                 is_leading_vehicle = true;
-%             end 
         elseif sum(ismember(lanelets_index_nveh,merging2)) && sum(ismember(lanelets_index_iveh,merging2))
             %check the distance to the end of center of lanelets 59 and 60
             distance_nveh = norm(merging2_end-current_point_nveh);
@@ -103,10 +84,7 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
             end    
-%             disp('66666666666666666666666666666666666666666666666666666666666666666666666666')
-%             if current_point_nveh_y < current_point_iveh_y 
-%                 is_leading_vehicle = true;
-%             end   
+
         elseif sum(ismember(lanelets_index_nveh,merging3)) && sum(ismember(lanelets_index_iveh,merging3))
             %check the distance to the end of center of lanelets 79 and 80
             distance_nveh = norm(merging3_end-current_point_nveh);
@@ -114,10 +92,6 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
             end            
-%             disp('7777777777777777777777777777777777777777777777777777777777777777777777777777777')
-%             if current_point_nveh_x < current_point_iveh_x
-%                 is_leading_vehicle = true;
-%             end 
         elseif sum(ismember(lanelets_index_nveh,merging4)) && sum(ismember(lanelets_index_iveh,merging4))
             %check the distance to the end of center of lanelets 33 and 34
             distance_nveh = norm(merging4_end-current_point_nveh);
@@ -125,13 +99,7 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
             if distance_nveh < distance_iveh
                 is_leading_vehicle = true;
             end    
-%             disp('88888888888888888888888888888888888888888888888888888888888888888888888888888888')
-%             if current_point_nveh_y > current_point_iveh_y
-%                 is_leading_vehicle = true;
-%             end 
-%             
         else
-%             disp('啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦')
             first_refPoint_nveh_x = iter.referenceTrajectoryPoints(nveh,1,1);
             first_refPoint_nveh_y = iter.referenceTrajectoryPoints(nveh,1,2);
             first_refPoint_nveh = [first_refPoint_nveh_x,first_refPoint_nveh_y];
@@ -142,8 +110,6 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
 
     %         driving_direction = first_refPoint - current_point;
             driving_direction_nveh = end_refPoint_nveh - current_point_nveh;
-
-
 
             first_refPoint_iveh_x = iter.referenceTrajectoryPoints(iveh,1,1);
             first_refPoint_iveh_y = iter.referenceTrajectoryPoints(iveh,1,2);
@@ -163,7 +129,7 @@ function is_leading_vehicle = check_driving_order(scenario,iter, nveh, iveh)
             vehicle_relative_direction = current_point_nveh - current_point_iveh;
     %         vehicle_relative_direction = first_refPoint - first_refPoint_adj;
 
-            % check if driving consecutively
+            % check if driving ahead
             is_leading_vehicle = (dot(driving_direction_ref, vehicle_relative_direction) > 0);
 
         end
