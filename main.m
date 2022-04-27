5cd4a892d014255ca93d4096d5c452ba692f70a9
Bingyi Su <bingyi.su@rwth-aachen.de>
2022-04-27 04:45:30 +0200

    merge from main_update

--- main.m
+++ main.m
@@ -1,5 +1,7 @@
-function result = main(varargin)
-% MAIN  main function for graph-based receeding horizon control
+function result = main_update(varargin)
+% MAIN  main function for graph-based receeding horizon control with
+% right-of-way priority assignment. Vehicles at intersection keep
+% higher priority and do not change their relative priority until they leave intersection
 
 if verLessThan('matlab','9.10')
     warning("Code is developed in MATLAB 2021a, prepare for backward incompatibilities.")
@@ -12,31 +14,47 @@ is_sim_lab = (nargin == 0 || (nargin > 0 && strcmp(varargin{1},'sim')));
 
 if is_sim_lab
     switch nargin
+        case 6
+            options = selection(varargin{2},varargin{3},varargin{4},varargin{5},varargin{6});
+        case 5
+            options = selection(varargin{2},varargin{3},varargin{4},varargin{5},1);
         case 4
-            options = selection(varargin{2},varargin{3},varargin{4});
+            options = selection(varargin{2},varargin{3},varargin{4},1,1);            
         case 3
-            options = selection(varargin{2},varargin{3},1);
+            options = selection(varargin{2},varargin{3},1,1,1);
         case 2
-            options = selection(varargin{2},2,1);
+            options = selection(varargin{2},2,1,1,1);
         otherwise
             options = selection();
     end
+    vehicle_ids = 1:20;
+    
 else
     disp('cpmlab')
     options = struct;
     vehicle_ids = [varargin{:}];
     options.amount = numel(vehicle_ids);
-    options.isPB = false;
+    options.isPB = true;
 end
-    
+  
 
-scenario = circle_scenario(options.amount,options.isPB);
+
+switch options.scenario
+    case 'Circle_scenario'
+        scenario = circle_scenario(options.amount,options.isPB);
+    case 'Commonroad'
+        scenario = commonroad(options.amount,vehicle_ids,options.isPB);  
+end
+scenario.name = options.scenario;
+scenario.priority_option = options.priority;
+
 
 if is_sim_lab
     exp = SimLab(scenario, options);
 else
     exp = CPMLab(scenario, vehicle_ids);
 end
+
 
 %% Setup
 % Initialize
@@ -47,45 +65,117 @@ k = 1;
 result = get_result_struct(scenario);
 
 exp.setup();
+fallback=0;
 
 %% Main control loop
 while (~got_stop)
+    
     result.step_timer = tic;
     % Measurement
     % -------------------------------------------------------------------------
-    [ x0, trim_indices ] = exp.measure();
-    
+    [ x0, trim_indices ] = exp.measure();% trim_indices： which trim  
+    scenario.k = k;
+
     try
         % Control 
         % ----------------------------------------------------------------------
+        
         % Sample reference trajectory
         iter = rhc_init(scenario,x0,trim_indices);
-        scenario_tmp = get_next_dynamic_obstacles_scenario(scenario, k);
         
+        % update the boundary information of each vehicle
+        if strcmp(scenario.name, 'Commonroad')
+            lanelet_boundary = lanelets_boundary(scenario, iter);
+            for iveh = 1:options.amount
+                scenario.vehicles(1,iveh).lanelet_boundary = lanelet_boundary{1,iveh};
+            end
+            % update the coupling information
+            scenario = coupling_adjacency(scenario,iter);
+        end
+        
+        % calculate the distance 
+        distance = zeros(options.amount,options.amount);
+        adjacency = scenario.adjacency(:,:,end);
+
+        for vehi = 1:options.amount-1
+            adjacent_vehicle = find(adjacency(vehi,:));
+            adjacent_vehicle = adjacent_vehicle(adjacent_vehicle > vehi);
+            for vehn = adjacent_vehicle
+                distance(vehi,vehn) = check_distance(iter,vehi,vehn);
+            end
+        end
+        result.distance(:,:,k) = distance;
+        
+        % dynamic scenario
+        scenario_tmp = get_next_dynamic_obstacles_scenario(scenario, k);
+        result.iter_runtime(k) = toc(result.step_timer);
+        
+        
+        % The controller computes plans
         controller_timer = tic;
-            [u, y_pred, info] = scenario.controller(scenario_tmp, iter);
-            
+        [u, y_pred, info] = scenario.controller(scenario_tmp, iter);
+        scenario.last_veh_at_intersection = info.veh_at_intersection;
         result.controller_runtime(k) = toc(controller_timer);
-        result.iteration_structs{k} = iter;
+        
         % save controller outputs in result struct
+        result.scenario = scenario;
+        result.iteration_structs{k} = iter;
         result.trajectory_predictions(:,k) = y_pred;
         result.controller_outputs{k} = u;
         result.subcontroller_runtime(:,k) = info.subcontroller_runtime;
-        % store vehicles path in higher resolution
         result.vehicle_path_fullres(:,k) = info.vehicle_fullres_path(:);
         result.n_expanded(k) = info.n_expanded;
+        result.priority(:,k) = info.priority_list;
+        result.computation_levels(k) = info.computation_levels;
+        result.edges_to_break{k} = info.edge_to_break;
         result.step_time(k) = toc(result.step_timer);
         
-        % Apply control action f/e veh
+        % Apply control action
         % -------------------------------------------------------------------------
         exp.apply(u, y_pred, info, result, k);
-
+        
+        fallback_update = 0;
+        
     % catch case where graph search could not find a new node
     catch ME
         switch ME.identifier
         case 'MATLAB:graph_search:tree_exhausted'
-            warning([ME.message, ', ending search...']);
-            got_stop = true;
+            warning([ME.message, ', ME, fallback to last priority.............']);
+            
+            fallback = fallback + 1;
+            fallback_update = fallback_update + 1;
+            disp(['fallback: ', num2str(fallback)])
+
+            % fallback to last plan
+            controller_timer = tic;
+            [u, y_pred, info] = pb_controller_fallback(scenario, u, y_pred, info);
+            result.controller_runtime(k) = toc(controller_timer);
+            
+            % save controller outputs in result struct
+            result.scenario = scenario;
+            result.iteration_structs{k} = iter;
+            result.trajectory_predictions(:,k) = y_pred;
+            result.controller_outputs{k} = u;
+            result.subcontroller_runtime(:,k) = info.subcontroller_runtime;
+            result.vehicle_path_fullres(:,k) = info.vehicle_fullres_path(:);
+            result.n_expanded(k) = info.n_expanded;
+            result.priority(:,k) = info.priority_list;
+            result.computation_levels(k) = info.computation_levels;
+            result.edges_to_break{k} = info.edge_to_break;
+            result.step_time(k) = toc(result.step_timer);
+            result.fallback = fallback;
+
+            % Apply control action
+            % -------------------------------------------------------------------------
+            exp.apply(u, y_pred, info, result, k); 
+            
+            % if fallback to last plan Hp times continuously, the vehicle
+            % will stop at the current position, terminate the simulation
+            if fallback_update == scenario.Hp
+                got_stop = true;
+                disp('Already fallback Hp times, terminate the simulation')
+            end
+            
         otherwise
             rethrow(ME)
         end
@@ -98,11 +188,8 @@ while (~got_stop)
     % increment interation counter
     k = k+1;
 end
-
 %% save results
 save(fullfile(result.output_path,'data.mat'),'result');
-
+% exportVideo( result );
 exp.end_run()
-
-
 end
