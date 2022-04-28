a06b1758e5956f97a18d5e8ec126067811e070cc
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-04-28 09:02:33 +0200

    choose trims

--- motion_primitive_automaton/choose_trims.m
+++ motion_primitive_automaton/choose_trims.m
@@ -37,5 +37,44 @@ function [trim_inputs, trim_adjacency] = choose_trims(trim_set)
         trim_adjacency(2:end,2:end) = trim_adjacency(2:end,2:end) ...
             - triu(ones(ntrims-1),2)...
             - tril(ones(ntrims-1),-2);
+    case 12
+        %% Two speeds (0 and 0.6 m/s), 12 steering
+        steering = (-2:0.5:2) * pi/18;
+        ntrims = numel(steering)+3;
+        trim_inputs = zeros(ntrims,2);
+        trim_inputs(2,1) = -0.6;
+        trim_inputs(2,2) = 0.05;
+        trim_inputs(end,1) = 0.6;
+        trim_inputs(end,2) = 0.05;
+        trim_inputs(3:end-1,1) = steering;
+        trim_inputs(3:end-1,2) = 0.6;
+        trim_adjacency = ones(ntrims);
+        % equilibrium is state 1
+        % equilibrium is reachable from all states
+        % and can reach all states, so all 1s is good
+        % other states are connected by one hop        
+        trim_adjacency(2:end,2:end) = trim_adjacency(2:end,2:end) ...
+            - triu(ones(ntrims-1),2)...
+            - tril(ones(ntrims-1),-2);
+        
+    case 13
+        %% Two speeds (0, 0.6 m/s), 12 steering
+        steering = [-2,-2,-1.5,-1.5,-1,-1,-0.5,-0.5,0,0,0.5,0.5,1,1,1.5,1.5,2,2] * pi/18;
+        ntrims = numel(steering)+3;
+        trim_inputs = zeros(ntrims,2);
+        trim_inputs(2,1) = -0.6;
+        trim_inputs(2,2) = 0.05;
+        trim_inputs(end,1) = 0.6;
+        trim_inputs(end,2) = 0.05;
+        trim_inputs(3:end-1,1) = steering;
+        trim_inputs(3:end-1,2) = [0.6,1.0,0.6,1.0,0.6,1.0,0.6,1.0,0.6,1.0,0.6,1.0,0.6,1.0,0.6,1.0,0.6,1.0];
+        trim_adjacency = ones(ntrims);
+        % equilibrium is state 1
+        % equilibrium is reachable from all states
+        % and can reach all states, so all 1s is good
+        % other states are connected by one hop       
+        trim_adjacency(2:end,2:end) = trim_adjacency(2:end,2:end) ...
+            - triu(ones(ntrims-1),3)...
+            - tril(ones(ntrims-1),-4);
     end
 end
