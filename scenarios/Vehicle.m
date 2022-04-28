aedb94c51aaccac17b8f1c55920d50d16a1492f3
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-04-28 08:59:54 +0200

    Vehicle

--- scenarios/Vehicle.m
+++ scenarios/Vehicle.m
@@ -14,6 +14,7 @@ classdef Vehicle
         Width               = 0.1;   % Vehicle width [m]
         Lf                  = 0.1;   % Distance between vehicle center and front axle center [m]
         Lr                  = 0.1;   % Distance between vehicle center and rear axle center [m]
+        ID                  = -1;    % vehicle ID (should be positive integer)
     end
     
     methods
