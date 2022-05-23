963f41d5e255b6431bb6ce1b5e4e98edc6c2159d
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-05-23 12:00:08 +0200

    add reachability analysis

--- motion_primitive_automaton/MotionPrimitiveAutomaton.m
+++ motion_primitive_automaton/MotionPrimitiveAutomaton.m
@@ -9,8 +9,9 @@ classdef MotionPrimitiveAutomaton
         transition_matrix        % binary Matrix (if maneuverTuple exist according to trims) (nTrimTuples x nTrimTuples x horizon_length)
         distance_to_equilibrium  % Distance in graph from current state to equilibrium state (nTrims x 1)
         recursive_feasibility
-        reachable_sets           % Store all the reachable sets of each trim (possibly non-convex)
-        reachable_sets_conv;     % Convexified reachable sets of each trim
+        local_reachable_sets           % Reachable sets of each trim (possibly non-convex); 
+                                            % It's local because the position and yaw angle of vehicles are not considered
+        local_reachable_sets_conv;     % Convexified reachable sets of each trim
     end
     
     methods
@@ -22,7 +23,7 @@ classdef MotionPrimitiveAutomaton
             % N is the horizon length
 
             % path of the MPA
-            [file_path,name,ext] = fileparts(mfilename('fullpath'));
+            [file_path,~,~] = fileparts(mfilename('fullpath'));
             folder_target = [file_path,filesep,'library'];
             mpa_instance_name = ['MPA_','trims',num2str(trim_set),'_Hp',num2str(N),'.mat'];
             mpa_full_path = [folder_target,filesep,mpa_instance_name];
@@ -84,12 +85,12 @@ classdef MotionPrimitiveAutomaton
             obj.transition_matrix = compute_product_maneuver_matrix(obj,nveh,N);
             
             % variables to store reachable sets in different time steps
-            obj.reachable_sets = cell(n_trims,N);
-            obj.reachable_sets_conv = cell(n_trims,N);
+            obj.local_reachable_sets = cell(n_trims,N);
+            obj.local_reachable_sets_conv = cell(n_trims,N);
                 
             % compute the reachable sets of each trim
             
-            [obj.reachable_sets, obj.reachable_sets_conv] = reachability_analysis_offline(obj,N);
+            [obj.local_reachable_sets, obj.local_reachable_sets_conv] = reachability_analysis_offline(obj,N);
             
             save_mpa(obj,mpa_full_path); % save mpa to library
             
@@ -141,6 +142,79 @@ classdef MotionPrimitiveAutomaton
             mpa = obj;
             save(mpa_full_path,'mpa');
         end
+
+        function emergency_braking_distance = get_emergency_braking_distance(obj, cur_trim_id, time_step)
+            % returns the emergency braking distance starting from the current trim
+            emergency_braking_distance = 0;
+            speed_cur = obj.trims(cur_trim_id).speed;
+
+            % compute the shortest path from the current trim to the equilibrium trim
+            equilibrium_trim = find([obj.trims.speed]==0);
+            assert(length(equilibrium_trim)==1) % if there are multiple equilibrium states, this function should be then adapted
+            graph_trims = graph(obj.transition_matrix_single(:,:,1));
+            shortest_path_to_equilibrium = shortestpath(graph_trims,cur_trim_id,equilibrium_trim); % shortest path between current trim and equilibrium trim
+
+            for iTrim=shortest_path_to_equilibrium(2:end)
+                speed_next = obj.trims(iTrim).speed;
+                speed_mean = (speed_cur+speed_next)/2; % assume linear change
+                emergency_braking_distance = emergency_braking_distance + speed_mean*time_step;
+                speed_cur = speed_next; % update the current speed for the next iteration
+            end
+        end
+        
+        function shortest_time_to_arrive = get_the_shortest_time_to_arrive(obj, cur_trim_id, distance_destination, time_step)
+            % Returns the shortest time to arive a given distance starting from the current trim
+            % Noted that this is only a lower bound time because the
+            % steering angle is not considered, namely we assume the
+            % vehicle drives straight to arrive the goal destination.
+            shortest_time_to_arrive = 0;
+            distance_remained = distance_destination;
+            distance_acceleration = 0; % acceleration distance
+            % compute the shortest path from the current trim to the
+            % trim(s) with maximum speed
+            max_speed = max([obj.trims.speed]);
+            max_speed_trims = find([obj.trims.speed]==max_speed); % find all the trims with the maximum speed
+
+            graph_trims = graph(obj.transition_matrix_single(:,:,1));
+            shortest_distances_to_max_speed = distances(graph_trims,cur_trim_id,max_speed_trims); % shortest path between two single nodes
+            % find the one which has the minimal distance to the trims with the maximum speed
+            [min_distance,idx] = min(shortest_distances_to_max_speed); 
+            if min_distance==0 % if the current trim has already the maximum speed, no acceleration is needed
+                shortest_time_to_arrive = distance_remained/max_speed;
+            else % acceleration to maximum speed
+                max_speed_trim = max_speed_trims(idx);
+                shortest_path_to_max_speed = shortestpath(graph_trims,cur_trim_id,max_speed_trim); % shortest path between two single nodes
+                for i=1:length(shortest_path_to_max_speed)
+                    trim_current = shortest_path_to_max_speed(i);
+                    
+                    speed_cur = obj.trims(trim_current).speed;
+                    if i+1<=length(shortest_path_to_max_speed)
+                        trim_next = shortest_path_to_max_speed(i+1);
+                        speed_next = obj.trims(trim_next).speed;
+                    else
+                        speed_next = max_speed;
+                    end
+                    mean_speed = (speed_cur+speed_next)/2;
+                    distance_acceleration = distance_acceleration + mean_speed*time_step;
+                    if distance_acceleration > distance_destination % if the vehicle arrives the detination when accelerating
+                        shortest_time_to_arrive = shortest_time_to_arrive + distance_remained/mean_speed; % time accumulates
+                        break
+                    else
+                        shortest_time_to_arrive = shortest_time_to_arrive + time_step; % time accumulates
+                        distance_remained = distance_destination - distance_acceleration;
+                    end
+                end
+
+                % if the detination is still not arrived after acceleration, calculate the remaining time using the maximum speed
+                if distance_remained>0
+                    shortest_time_to_arrive = shortest_time_to_arrive + distance_remained/max_speed;
+                end
+
+            end
+        end
+
+
+        
 
     end
 end
