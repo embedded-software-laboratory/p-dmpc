7c80b6ec49d003c865b43736283236c4afe9d751
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-05-23 11:58:30 +0200

    coupling

--- helper/coupling_adjacency.m
+++ helper/coupling_adjacency.m
@@ -9,37 +9,19 @@
     semi_adjacency = zeros(nVeh,nVeh);
     [~,adjacent,semi_adjacent,~,~,~,~] = get_road_data();
     
-
     for i = 1:(nVeh-1)
-        nlanelets_i = [];   
-        ref_points_index_i = reshape(iter.referenceTrajectoryIndex(i,:,:),Hp,1);
         ref_points_i = reshape(iter.referenceTrajectoryPoints(i,:,:),Hp,2);
-        
-        for n = 1:length(ref_points_index_i)
-            nlanelets_i = [ nlanelets_i, sum(ref_points_index_i(n) > scenario.vehicles(1,i).points_index)+1];
-        end              
-        nlanelets_i = unique(nlanelets_i);
-        lanelets_index_i = scenario.vehicles(1,i).lanelets_index(nlanelets_i);
-        scenario.vehicles(1,i).predicted_lanelets = lanelets_index_i;
-
-        
+        predicted_lanelets_i = iter.predicted_lanelets{i}; 
         for j = (i+1):nVeh
             stop_flag = false;
-            nlanelets_j = [];
-            ref_points_index_j = reshape(iter.referenceTrajectoryIndex(j,:,:),Hp,1);
             ref_points_j = reshape(iter.referenceTrajectoryPoints(j,:,:),Hp,2);           
-            for m = 1:length(ref_points_index_j)
-                nlanelets_j = [ nlanelets_j, sum(ref_points_index_j(m) > scenario.vehicles(1,j).points_index)+1];
-            end              
-            nlanelets_j = unique(nlanelets_j);
-            lanelets_index_j = scenario.vehicles(1,j).lanelets_index(nlanelets_j);
-            scenario.vehicles(1,j).predicted_lanelets = lanelets_index_j;
+            predicted_lanelets_j = iter.predicted_lanelets{j};
             
-            for k_i = 1:length(lanelets_index_i)  
-                for k_j = 1:length(lanelets_index_j)   
+            for k_i = 1:length(predicted_lanelets_i)  
+                for k_j = 1:length(predicted_lanelets_j)   
                     stop_flag = false;
                     % check if lanelets are adjacent
-                    if adjacent(lanelets_index_i(k_i),lanelets_index_j(k_j)) == 1 
+                    if adjacent(predicted_lanelets_i(k_i),predicted_lanelets_j(k_j)) == 1 
                         % check if predicted trajectories are within defined distance
                         for k = 1:Hp 
                             dist = norm((ref_points_i(k,:)-ref_points_j(k,:)),2);                    
@@ -55,7 +37,7 @@
                     end
                     
                     % check if lanelets are semi_adjacent
-                    if semi_adjacent(lanelets_index_i(k_i),lanelets_index_j(k_j)) == 1 
+                    if semi_adjacent(predicted_lanelets_i(k_i),predicted_lanelets_j(k_j)) == 1 
                         % check if predicted references are within defined distance
                         for k = 1:Hp 
                             dist = norm((ref_points_i(k,:)-ref_points_j(k,:)),2);                    
