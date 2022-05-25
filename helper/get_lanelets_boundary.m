bb8e826fbebfaee0c5b7806d4f0941ae43daa1e8
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-05-25 21:37:43 +0200

    improve get_lanelets_boundary

--- helper/get_lanelets_boundary.m
+++ helper/get_lanelets_boundary.m
@@ -2,14 +2,18 @@ function lanelet_boundary = get_lanelets_boundary(predicted_lanelets, lanelet_bo
 % GET_LANELETS_BOUNDARY Return the lanelet boundaries based on the
 % predicted lanelets
     
+    % extract the target lanelets
+    lanelet_boundaries = lanelet_boundaries(predicted_lanelets);
+
     % determine the boundaries for each vehicle
-    left_bound = [];
-    right_bound = [];
-    for index_lanelet = predicted_lanelets
-        left_bound = [left_bound; lanelet_boundaries{index_lanelet}{1}];
-        right_bound = [right_bound; lanelet_boundaries{index_lanelet}{2}];
-    end
+    left_bound = cellfun(@(c)c{1}', lanelet_boundaries, 'UniformOutput',false);
+    left_bound = [left_bound{:}];
+
+    right_bound = cellfun(@(c)c{2}', lanelet_boundaries, 'UniformOutput',false);
+    right_bound = [right_bound{:}];
     
     lanelet_boundary = {left_bound,right_bound};
 
 end
+
+
