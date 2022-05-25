558df234165c462fcb8d3725dd60ab2830bd0287
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-05-25 21:39:21 +0200

    change name for saved result

--- helper/get_result_struct.m
+++ helper/get_result_struct.m
@@ -1,4 +1,4 @@
-function result = get_result_struct(scenario)
+function result = get_result_struct(scenario, options)
 % GET_RESULT_STRUCT     Initialize result struct returned by simulation.
 
     result = struct;
@@ -16,11 +16,28 @@ function result = get_result_struct(scenario)
     result.belonging_vector = cell(0,1); % belonging vector indicates to which groups one vehicle belongs
     
     % create output directory
+
+
     directory_name = strrep(strcat(result.scenario.name, '_', result.scenario.controller_name),' ','_');
-    output_path = fullfile('results', directory_name);
-    result.output_path = output_path;
-    if ~isfolder(output_path)
-        mkdir(output_path);
+
+    [file_path,~,~] = fileparts(mfilename('fullpath')); % get the path of the current file
+    idcs = strfind(file_path,filesep); % find all positions of '/'
+    one_folder_up = file_path(1:idcs(end)-1); % one folder up
+
+    folder_target = fullfile(one_folder_up,'results',directory_name);
+    if ~isfolder(folder_target)
+        % create target folder if not exist
+        mkdir(folder_target)
     end
+    if options.isParl
+        file_name = ['trims',num2str(scenario.trim_set),'_Hp',num2str(scenario.Hp),'_nVeh',num2str(scenario.nVeh),'_T',num2str(scenario.T_end),'_maxCLs',num2str(scenario.max_num_CLs),'.mat'];
+    else
+        file_name = ['trims',num2str(scenario.trim_set),'_Hp',num2str(scenario.Hp),'_nVeh',num2str(scenario.nVeh),'_T',num2str(scenario.T_end),'.mat'];
+    end
+    output_path = fullfile(folder_target,file_name);
+    
+    result.output_path = output_path;
+    result.output_folder = folder_target;
+
 end
 
