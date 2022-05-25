62ee2fd0654186b702dcaa0986cd7ae3c7889db4
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-05-25 21:38:45 +0200

    non-convex reachable sets

--- helper/get_reachable_sets.m
+++ helper/get_reachable_sets.m
@@ -1,4 +1,4 @@
-function reachable_sets = get_reachable_sets(x0, y0, yaw0, trim_current, local_reachable_sets, predicted_lanelet_boundary)
+function reachable_sets = get_reachable_sets(x0, y0, yaw0, local_reachable_sets, predicted_lanelet_boundary)
 % GET_REACHABLE_SETS Calculate the reachable sets based on the current pose
 % and trim of the vehicle by tranlating the local reachable sets. If
 % vehicle's predicted lanelet boundary is available, the reachable sets
@@ -18,29 +18,50 @@ function reachable_sets = get_reachable_sets(x0, y0, yaw0, trim_current, local_r
     Hp = size(local_reachable_sets, 2);
     reachable_sets = cell(1, Hp);
 
+    is_convexify_reachable_sets = true; % whether convexify the reachable sets
+
     % if vehicle has predicted the lanelet boundary, the reachable sets would be the intersection of the full reachable sets and the lanelet boundary 
     if ~isempty(predicted_lanelet_boundary)
-        boundary_x = [predicted_lanelet_boundary{1}(:,1); predicted_lanelet_boundary{2}(end:-1:1,1)];
-        boundary_y = [predicted_lanelet_boundary{1}(:,2); predicted_lanelet_boundary{2}(end:-1:1,2)];
+        boundary_x = [predicted_lanelet_boundary{1}(1,:), predicted_lanelet_boundary{2}(1,end:-1:1)];
+        boundary_y = [predicted_lanelet_boundary{1}(2,:), predicted_lanelet_boundary{2}(2,end:-1:1)];
         poly_boundary = polyshape(boundary_x, boundary_y, 'Simplify', true);
     end
 
     for t = 1:Hp
-        % translate the offline calculated lacal reachable sets to global coordinate
+        % translate the offline calculated local reachable sets to global coordinate
         [reachable_set_x, reachable_set_y] = ...
-            translate_global(yaw0, x0, y0, local_reachable_sets{trim_current,t}.Vertices(:,1)', local_reachable_sets{trim_current,t}.Vertices(:,2)');
+            translate_global(yaw0, x0, y0, local_reachable_sets{t}.Vertices(:,1)', local_reachable_sets{t}.Vertices(:,2)');
         ploy_reachable_sets = polyshape(reachable_set_x, reachable_set_y);
-        if ~isempty(predicted_lanelet_boundary)           
-            reachable_sets_constrained = intersect(ploy_reachable_sets, poly_boundary);
-            reachable_sets_constrained_conv = convhull(reachable_sets_constrained);
-            reachable_sets{t}  = reachable_sets_constrained_conv; % convexify reachable sets since the axis separate theorem works only for convex polygon
+
+%         % MATLAB build-in function to rotate (around origin)
+%         local_reachable_sets_rotated = rotate(local_reachable_sets{t},rad2deg(yaw0),[0,0]);
+%         % MATLAB build-in function to translate 
+%         ploy_reachable_sets = translate(local_reachable_sets_rotated,[x0,y0]);
+
+        % constrain the reachable sets by lanelet boundary
+        if ~isempty(predicted_lanelet_boundary)
+            % The fourth cell in predicted_lanelet_boundary is the narrowed boundary while the third cell is the original boundary 
+            % Without narrowing, two lanelets are considered as overlap if they only share the same boundary
+            ploy_reachable_sets = intersect(ploy_reachable_sets, poly_boundary);
+            
+            if ploy_reachable_sets.NumRegions > 1
+                % Remove unexpected multiple regions coming from the intersection of the lanelet boundaries and reachable sets
+                polysort = sortboundaries(ploy_reachable_sets,'numsides','descend');
+                ploy_reachable_sets = rmboundary(polysort,2:polysort.NumRegions);
+            end
+
 %             plot(ploy_reachable_sets)
 %             plot(poly_boundary)
-%             plot(reachable_sets{t})
+%             plot(reachable_sets{t})            
+        end
+
+        if is_convexify_reachable_sets
+            % convexify reachable sets since the axis separate theorem works only for convex polygon
+            reachable_sets{t} = convhull(ploy_reachable_sets); 
         else
-            % else the reachable sets are the full reachable sets
-            reachable_sets{t} = convhull(ploy_reachable_sets); % convexify reachable sets since the axis separate theorem works only for convex polygon
+            reachable_sets{t} = ploy_reachable_sets;
         end
     end
 
 end
+
