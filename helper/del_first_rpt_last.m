bda06966779f727bbb00675fe087d95b7350a57a
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-05-27 09:12:03 +0200

    change input

--- helper/del_first_rpt_last.m
+++ helper/del_first_rpt_last.m
@@ -1,16 +1,24 @@
-function [data_out] = del_first_rpt_last(data_in)
+function [data_out] = del_first_rpt_last(data_in, repeat_times)
 % DEL_FIRST_RPT_LAST Delete the first column and repeat the last column
+% 
+% INPUT:
+%   data_in: input data, can be either matrix or cell array
+% 
+%   repeat_times: how many columns in front should be deleted and how many times
+%   that the last column should be repeated. Default to 1.
+% 
+% OUTPUT:
+%   data_out: output data
+%   
 % Example:
 %      data_in = [1 2 3;
 %                 4 5 6]
 %     data_out = [2 3 3;
 %                 5 6 6]
 
-    type = class(data_in);
-    switch type
-        case 'cell' % cell
-            data_out = {data_in{:,2:end} data_in{:,end}};
-        otherwise % array
-            data_out = [data_in(:,2:end) data_in(:,end)];
+    if nargin == 1
+        repeat_times = 1; % default value
     end
+
+    data_out = horzcat(data_in(:,1+repeat_times:end), repmat(data_in(:,end),1,repeat_times));
 end
