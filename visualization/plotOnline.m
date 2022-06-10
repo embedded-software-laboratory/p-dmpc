60a00e99162b06d9355caef246e1a2f751acd9eb
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-06-10 09:33:18 +0200

    plot coupling lines

--- visualization/plotOnline.m
+++ visualization/plotOnline.m
@@ -84,47 +84,25 @@ function plotOnline(result,step_idx,tick_now,exploration)
 %         
         % plot the vehicle index
         text(x(1)+0.1,x(2)+0.1,num2str(v),'FontSize', 16, 'LineWidth',1,'Color','m');
+
+%         % plot the vehicle ID
+%         text(x(1)+0.1,x(2)+0.1,num2str(veh.ID),'FontSize', 16, 'LineWidth',1,'Color','m');
 % 
     end
 
 
     % plot scenario adjacency
-%     adj = cell(nVeh,nVeh);
-%     for v=1:nVeh
-%         pos_step = result.trajectory_predictions{v,step_idx};
-%         x = pos_step(tick_now,:);
-%         % directed coupling
-%         if ~isempty(scenario.directed_coupling)
-%             adjacent_vehicles = find(scenario.directed_coupling(v,:) ~= 0);
-%    
-%             for adj_v = adjacent_vehicles
-%                 
-%                 adj_pos_step = result.trajectory_predictions{adj_v,end};
-%                 adj_x = adj_pos_step(tick_now,:);
-%                 % plot adjacency
-%                 if ~isempty(result.belonging_vector) && result.belonging_vector{step_idx}(v) ~= result.belonging_vector{step_idx}(adj_v)
-%                     % if there are parallel groups, couplings inside a group will be shown in red lines, while couplings between
-%                     % groups will be shown in blue dashed lines.
-%                     quiver(x(1),x(2),adj_x(1)-x(1),adj_x(2)-x(2),'AutoScale','off','LineWidth',1,'Color','b','LineStyle','--')
-%                 else
-%                     quiver(x(1),x(2),adj_x(1)-x(1),adj_x(2)-x(2),'AutoScale','off','LineWidth',1,'Color','r','LineStyle','-');
-%                 end
-%                 % plot distance
-%         %         dis{v,adj_v}=text((iter.x0(v,1)+iter.x0(adj_v,1))/2,(iter.x0(v,2)+iter.x0(adj_v,2))/2,...
-%         %             num2str(round(result.distance(v,adj_v,step_idx),2)),'FontSize', 12, 'LineWidth',1,'Color','b');
-%             end
-%         elseif ~isempty(scenario.adjacency)
-%             adjacent_vehicles = find(scenario.adjacency(v,:,end));
-%             adjacent_vehicles = adjacent_vehicles(adjacent_vehicles>v);
-%             for adj_v = adjacent_vehicles
-%                 adj{v,adj_v}=line([x(1),adj_x(1)],[x(2),adj_x(2)], coupling_line_options);
-%                 % plot distance
-%         %         dis{v,adj_v}=text((iter.x0(v,1)+iter.x0(adj_v,1))/2,(iter.x0(v,2)+iter.x0(adj_v,2))/2,...
-%         %             num2str(round(result.distance(v,adj_v,step_idx),2)),'FontSize', 12, 'LineWidth',1,'Color','b');
-%             end
-%         end
-%     end
-    
+    x0 = cellfun(@(c)c(tick_now,:), result.trajectory_predictions(:,step_idx), 'UniformOutput', false);
+    x0 = cell2mat(x0);
+    if ~isempty(scenario.coupling_weights)
+        plot_coupling_lines(scenario.coupling_weights, x0, scenario.belonging_vector, 'ShowWeights', true)
+    else
+        plot_coupling_lines(scenario.directed_coupling, x0, [], 'ShowWeights', false)
+    end
+
+%     plot distance
+%     text((iter.x0(v,1)+iter.x0(adj_v,1))/2,(iter.x0(v,2)+iter.x0(adj_v,2))/2,...
+%         num2str(round(result.distance(v,adj_v,step_idx),2)),'FontSize', 12, 'LineWidth',1,'Color','b');
 
     % Obstacle rectangle
     for obs = 1:nObst
