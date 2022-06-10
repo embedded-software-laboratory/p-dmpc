b7c1979e62104b87de2f442d39cfe8c4c15b3a3c
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-06-10 09:05:26 +0200

    communicate after fallback

--- controller/pb_controller_fallback.m
+++ controller/pb_controller_fallback.m
@@ -1,4 +1,4 @@
-function [u, y_pred, info] = pb_controller_fallback(scenario, u, y_pred, info, options)
+function [u, y_pred, info, scenario] = pb_controller_fallback(scenario, iter, u, y_pred, info, options)
 % pb_controller_fallback    planning by using last priority and trajectories directly
 
 
@@ -20,8 +20,9 @@ function [u, y_pred, info] = pb_controller_fallback(scenario, u, y_pred, info, o
         info.next_node = set_node(info.next_node,vehicle_idx,info_v);
         info.vehicle_fullres_path(vehicle_idx) = path_between(info_v.tree_path(1),info_v.tree_path(2),info_v.tree,scenario.mpa);
         info.trim_indices(vehicle_idx,:) = info_v.trim_indices;
-        info.shapes(vehicle_idx,:) = [info.shapes(vehicle_idx,2:end),info.shapes(vehicle_idx,end)];% the last conlumn is used to keep the size
-        info.tree_path(vehicle_idx,:) = [info.tree_path(vehicle_idx,2:end),info.tree_path(vehicle_idx,end)];% the last element is used to keep the size
+        info.shapes(vehicle_idx,:) = [info.shapes(vehicle_idx,2:end),info.shapes(vehicle_idx,end)]; % the last conlumn is used to keep the size
+        info.tree_path(vehicle_idx,:) = [info.tree_path(vehicle_idx,2:end),info.tree_path(vehicle_idx,end)]; % the last element is used to keep the size
+        info.predicted_trims(vehicle_idx,:) = [info.predicted_trims(vehicle_idx,2:end),info.predicted_trims(vehicle_idx,end)]; % the last element is used to keep the size
         y_pred{vehicle_idx,1} = [y_pred{vehicle_idx,1}((scenario.tick_per_step+1)+1:end,:);y_pred{vehicle_idx,1}((scenario.tick_per_step+1)*(scenario.Hp-1)+1:end,:)];
 
    
@@ -35,9 +36,18 @@ function [u, y_pred, info] = pb_controller_fallback(scenario, u, y_pred, info, o
     end
 
     if options.isParl
-        info.belonging_vector = info.belonging_vector;
+        % send message
+
         info.subcontroller_runtime_all_grps = 0;
+        for iVeh = 1:nVeh
+            predicted_trims = info.predicted_trims(iVeh,:);
+            trim_current = predicted_trims(1);
+            x0 = info.vehicle_fullres_path{iVeh}(1,indices().x);
+            y0 = info.vehicle_fullres_path{iVeh}(1,indices().y);
+            [predicted_lanelets,~,~] = get_predicted_lanelets(scenario.vehicles(iVeh), trim_current, x0, y0, scenario.mpa, scenario.dt);
+            % send message
+            send_message(scenario.vehicles(iVeh).communicate, scenario.k, predicted_trims, predicted_lanelets, info.shapes(iVeh,:));
+        end
     end
         
- 
 end
