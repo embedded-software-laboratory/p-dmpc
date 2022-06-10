385c55ed4d4713f7fcf8a75556c02b78d624e1e2
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-06-10 23:07:52 +0200

    distinguish lanelet boundary and crossing area

--- helper/vectorize_all_obstacles.m
+++ helper/vectorize_all_obstacles.m
@@ -1,4 +1,4 @@
-function [vehicle_obstacles, lanelet_obstacles] = vectorize_all_obstacles(scenario)
+function [vehicle_obstacles, lanelet_boundary, lanelet_crossing_areas] = vectorize_all_obstacles(scenario)
 % VECTORIZE_ALL_OBSTACLES This function vectorizes all obstacles, including
 % static and dynamic obstacles as well as lanelet boundaries, to 
 % a single two-row matrix. The first row is for x-coordinates and the
@@ -11,12 +11,12 @@ function [vehicle_obstacles, lanelet_obstacles] = vectorize_all_obstacles(scenar
 %   all vehicle obstacles, such as current occupied area, predicted
 %   occupied areas and reachable sets, in a certain prediction horizon. 
 % 
-%   lanelet_obstacles: cell array with Hp subcells. Each subcell
-%   corresponds to all lanelet obstacles, such as lanelet boundaries and
+%   lanelet_obstacles: 2-by-Hp cell array. Each subcell in the first row
+%   corresponds to lanelet, such as lanelet boundaries and
 %   crossing area of lanelets, in a certain prediction horizon. 
 % 
     vehicle_obstacles = cell(1,scenario.Hp);
-    lanelet_obstacles = cell(1,scenario.Hp);
+    lanelet_crossing_areas = cell(1,scenario.Hp);
 
     % get static occupied areas of the considered vehicles
     current_occupied_areas = scenario.obstacles;
@@ -24,13 +24,13 @@ function [vehicle_obstacles, lanelet_obstacles] = vectorize_all_obstacles(scenar
     % Preprocess crossing areas of lanelets
     % 1. Repeat the last column to enclose the shape.
     % 2. Add column [nan;nan] to separate different obstacles 
-    lanelet_crossing_areas = cellfun(@(c)[c,c(:,1),[nan;nan]],scenario.lanelet_crossing_areas,'UniformOutput',false); 
+    lanelet_crossing_areas_tmp = cellfun(@(c)[c,c(:,1),[nan;nan]],scenario.lanelet_crossing_areas,'UniformOutput',false); 
 
     % Preprocess lanelet boundary
     % 1. Add column [nan;nan] to separate left and right boundaries 
     % Note that lanelet boundaries are considered as curves and thus not
     % needed to be enclosed.
-    lanelet_boundary = cellfun(@(c)[c,[nan;nan]],scenario.vehicles.lanelet_boundary,'UniformOutput',false); 
+    lanelet_boundary_tmp = cellfun(@(c)[c,[nan;nan]],scenario.vehicles.lanelet_boundary,'UniformOutput',false); 
 
     [~, n_occupiedAreas_Hp] = size(scenario.dynamic_obstacle_area);
     [~, n_reachableSets_Hp] = size(scenario.dynamic_obstacle_reachableSets);
@@ -56,6 +56,7 @@ function [vehicle_obstacles, lanelet_obstacles] = vectorize_all_obstacles(scenar
     
         % all obstacles, include static obstacles, dynamic obstacles and lanelet boundaries
         vehicle_obstacles{iStep} = [veh_obstacles_polygons{:}];
-        lanelet_obstacles{iStep} = [lanelet_boundary{:}, lanelet_crossing_areas{:}];
+        lanelet_boundary = [lanelet_boundary_tmp{:}];
+        lanelet_crossing_areas{iStep} = [lanelet_crossing_areas_tmp{:}];
     end
 end
