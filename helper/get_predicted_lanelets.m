3a7613d7550ca52bcb3d875d65d807d70052e5a3
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-06-18 18:28:01 +0200

    add addition input for get_predicted_lanelets

--- helper/get_predicted_lanelets.m
+++ helper/get_predicted_lanelets.m
@@ -1,4 +1,4 @@
-function [predicted_lanelets, reference, v_ref] = get_predicted_lanelets(vehicle, trim_current, x0, y0, mpa, dt)
+function [predicted_lanelets, reference, v_ref] = get_predicted_lanelets(vehicle, trim_current, x0, y0, mpa, dt, isParl)
 % GET_PREDICTED_LANELETS This function calculate the predicted lanelets
 % based on vehile's current states and reference path. 
 % 
@@ -47,15 +47,18 @@ function [predicted_lanelets, reference, v_ref] = get_predicted_lanelets(vehicle
     
     predicted_lanelets_idx = unique(predicted_lanelets_idx,'stable'); % use 'stable' to keep the order
 
-    if length(predicted_lanelets_idx) == 1
-        % at least predict two lanelets to avoid that the endpoint of the
-        % reference path being too close to the end of the predicted lanelets
-        predicted_lanelets_idx = [predicted_lanelets_idx,predicted_lanelets_idx+1];
-        if predicted_lanelets_idx(end) > length(vehicle.lanelets_index)
-            % loop back to the first lanelet
-            predicted_lanelets_idx(end) = 1;
+    if isParl
+        % at least two lanelets needed to predicted if parallel computation is used
+        if length(predicted_lanelets_idx) == 1
+            % at least predict two lanelets to avoid that the endpoint of the
+            % reference path being too close to the end of the predicted lanelets
+            predicted_lanelets_idx = [predicted_lanelets_idx,predicted_lanelets_idx+1];
+            if predicted_lanelets_idx(end) > length(vehicle.lanelets_index)
+                % loop back to the first lanelet
+                predicted_lanelets_idx(end) = 1;
+            end
         end
     end
-
+    
     predicted_lanelets = vehicle.lanelets_index(predicted_lanelets_idx);
 end
