caee526a9bfa76ae2f2a019eef4df500fe142b57
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-06-19 00:05:36 +0200

    improve get_lanelets_boundary

--- helper/get_lanelets_boundary.m
+++ helper/get_lanelets_boundary.m
@@ -1,4 +1,4 @@
-function lanelet_boundary = get_lanelets_boundary(predicted_lanelets, lanelet_boundaries, lanelets_index)
+function lanelet_boundary = get_lanelets_boundary(target_lanelet_boundaries)
 % GET_LANELETS_BOUNDARY Return the lanelet boundaries based on the
 % predicted lanelets
     
@@ -6,32 +6,15 @@ function lanelet_boundary = get_lanelets_boundary(predicted_lanelets, lanelet_bo
     % second cell: right boundary
     % third cell: MATLAB polyshape of the boundary
     lanelet_boundary = cell(1,3);
-
-    % extract the target lanelets
-    target_lanelet_boundaries = lanelet_boundaries(predicted_lanelets);
-
-    % find the predecessor lanelet
-    find_first_lanelet_idx = find(predicted_lanelets(1)==lanelets_index);
-
-    if find_first_lanelet_idx==1
-        % loop back to the last lanelet
-        predecessor_fitst_lanelet = lanelets_index(length(lanelets_index));
-    else
-        predecessor_fitst_lanelet = lanelets_index(find_first_lanelet_idx-1);
-    end
     
-    predecessor_fitst_lanelet_boundary = lanelet_boundaries{predecessor_fitst_lanelet};
-
     % determine the boundaries for each vehicle
-    left_bound = cellfun(@(c)c{1}(1:end-1,:)', target_lanelet_boundaries, 'UniformOutput',false);
-    % add the end part of the predecessor lanelet boundary to avoid vehicle
-    % being too close to the starting part of the labelet boundary 
-    end_part_predecessor_left = predecessor_fitst_lanelet_boundary{1}(end-3:end-1,:)';
-    left_bound = [end_part_predecessor_left, left_bound{:}];
+    % Note that the first point of each lanelet boundary is deleted for a
+    % smooth transition between two lanelet boundaries
+    left_bound = cellfun(@(c)c{1}(2:end,:)', target_lanelet_boundaries, 'UniformOutput',false);
+    left_bound = [left_bound{:}];
 
-    right_bound = cellfun(@(c)c{2}(1:end-1,:)', target_lanelet_boundaries, 'UniformOutput',false);
-    end_part_predecessor_right = predecessor_fitst_lanelet_boundary{2}(end-3:end-1,:)';
-    right_bound = [end_part_predecessor_right, right_bound{:}];
+    right_bound = cellfun(@(c)c{2}(2:end,:)', target_lanelet_boundaries, 'UniformOutput',false);
+    right_bound = [right_bound{:}];
     
     lanelet_boundary{1} = left_bound;
     lanelet_boundary{2} = right_bound;
