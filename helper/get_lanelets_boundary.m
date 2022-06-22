78a0bcf8525e482ece7b619ee217b8ef9f7464d4
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-06-22 17:25:38 +0200

    delete input

--- helper/get_lanelets_boundary.m
+++ helper/get_lanelets_boundary.m
@@ -1,4 +1,4 @@
-function lanelet_boundary = get_lanelets_boundary(predicted_lanelets, lanelet_boundaries, lanelets_index, is_sim_lab)
+function lanelet_boundary = get_lanelets_boundary(predicted_lanelets, lanelet_boundaries, is_sim_lab)
 % GET_LANELETS_BOUNDARY Return the lanelet boundaries based on the
 % target_lanelet_boundaries: predicted lanelets boundaries
     
@@ -6,15 +6,17 @@ function lanelet_boundary = get_lanelets_boundary(predicted_lanelets, lanelet_bo
     % second cell: right boundary
     % third cell: MATLAB polyshape of the boundary
     lanelet_boundary = cell(1,3);
-    
+    % extract the target lanelets
+    lanelet_boundaries = lanelet_boundaries(predicted_lanelets);
+
     if is_sim_lab
         % determine the boundaries for each vehicle
         % Note that the first point of each lanelet boundary is deleted for a
         % smooth transition between two lanelet boundaries
-        left_bound = cellfun(@(c)c{1}(2:end,:)', target_lanelet_boundaries, 'UniformOutput',false);
+        left_bound = cellfun(@(c)c{1}(2:end,:)', lanelet_boundaries, 'UniformOutput',false);
         left_bound = [left_bound{:}];
 
-        right_bound = cellfun(@(c)c{2}(2:end,:)', target_lanelet_boundaries, 'UniformOutput',false);
+        right_bound = cellfun(@(c)c{2}(2:end,:)', lanelet_boundaries, 'UniformOutput',false);
         right_bound = [right_bound{:}];
         
         lanelet_boundary{1} = left_bound;
@@ -27,9 +29,6 @@ function lanelet_boundary = get_lanelets_boundary(predicted_lanelets, lanelet_bo
         lanelet_boundary{3} = polyshape(x,y,'Simplify',false);
         assert(lanelet_boundary{3}.NumRegions==1)
     else
-        % extract the target lanelets
-        lanelet_boundaries = lanelet_boundaries(predicted_lanelets);
-
         % determine the boundaries for each vehicle
         left_bound = cellfun(@(c)c{1}', lanelet_boundaries, 'UniformOutput',false);
         left_bound = [left_bound{:}];
