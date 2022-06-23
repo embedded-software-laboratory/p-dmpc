cef64226c7a8e95226edc18e209cd53b6e4f2e05
david.becher <david.becher@rwth-aachen.de>
2022-06-23 22:32:51 +0200

    fixed startup and launch scripts

--- launch/launch_controller_packages.sh
+++ launch/launch_controller_packages.sh
@@ -1,28 +1,28 @@
 #!/bin/bash  
 
-controller_amount = $1;
-force_feedback_enabled = $2;
-force_feedback_dir = ~/home/Documents/ros2_ws;
+controller_amount=$1
+force_feedback_enabled=$2
+force_feedback_dir=~/Documents/ros2_ws;
 
 # source ros
-source /opt/ros/eloquent/setup.bash
+cd launch
+source /opt/ros/eloquent/setup.bash 
 
-if ($controller_amount == 1)
-then
+if [ $controller_amount -eq 1 ]; then
     # only launch steering wheel
-    ros2 launch single_controller_launch.xml
+    ros2 launch single_controller_launch.xml &
 else
     # launch steering wheel and gamepad
-    ros2 launch dual_controller_launch.xml
+    ros2 launch dual_controller_launch.xml &
 fi
 
-if ($force_feedback_enabled = true)
-then
+if [ $force_feedback_enabled -eq 1 ]; then
+    echo $force_feedback_enabled
     cd $force_feedback_dir
     source /opt/ros/eloquent/setup.bash
     source $force_feedback_dir/install/setup.bash
-    ros2 run ros_g29_force_feedback  g29_force_feedback --ros-args --params-file $force_feedback_dir/install/ros_g29_force_feedback/share/ros_g29_force_feedback/config/g29.yaml
+    ros2 run ros_g29_force_feedback  g29_force_feedback --ros-args --params-file $force_feedback_dir/install/ros_g29_force_feedback/share/ros_g29_force_feedback/config/g29.yaml && fg
     # if the line above causes an error, run cat /proc/bus/input/devices, and check if the event handler for the steering wheel
     # is equal to the value in $force_feedback_dir/install/ros_g29_force_feedback/share/ros_g29_force_feedback/config/g29.yaml
-fi 
+fi
 
--- startup_mixed_traffic.m
+++ startup_mixed_traffic.m
@@ -3,14 +3,16 @@ function startup_mixed_traffic(number_of_controllers, force_feedback_enabled)
 % number_of_controllers: specifiy whether a single controller (steering wheel) or two controllers (steering wheel and gamepad) will be used
 % force_feedback_enabled: enable sending commands to steering wheel
 
-    if ~isboolean(force_feedback_enabled)
+    if force_feedback_enabled ~= 0 && force_feedback_enabled ~= 1
         disp("variable force_feedback_enabled has to be true or false");
         return
-    end
+    end 
 
     if number_of_controllers == 1 || number_of_controllers == 2
         pathToScript = fullfile(pwd,'/launch','launch_controller_packages.sh');
-        cmdStr = [pathToScript ' ' number_of_controllers ' ' force_feedback_enabled];
+        number_of_controllers = num2str(number_of_controllers);
+        force_feedback_enabled = num2str(force_feedback_enabled);
+        cmdStr = ['gnome-terminal --' ' ' pathToScript ' ' number_of_controllers ' ' force_feedback_enabled];
         system(cmdStr);
     else
         disp("Invalid amount of controllers, enter 1 or 2");
