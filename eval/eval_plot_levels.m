f9d77078925146f19da04141477f67364e20f717
Julius Kahle <julius.kahle@rwth-aachen.de>
2022-10-26 12:46:23 +0200

    change name of plot data function

--- eval/eval_plot_levels.m
+++ eval/eval_plot_levels.m
@@ -1,6 +1,6 @@
 function eval_plot_levels(res)
 % EVAL_PLOT_LEVELS  Evaluate the computation levels of the result cell `res`.
 
-    plot_levels_data = get_plot_levels_data(res);
+    plot_levels_data = compute_plot_levels_data(res);
     plot_levels_data(plot_levels_data);
 end
