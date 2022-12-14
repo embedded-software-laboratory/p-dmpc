3640b7c7e4b5a8ec693d38c341e7b70936a74784
David <david.guillaume@rwth-aachen.de>
2022-12-14 18:25:14 +0100

    #37  update central controller

--- controller/centralized_controller.m
+++ controller/centralized_controller.m
@@ -1,29 +1,29 @@
-function [info,scenario,iter] = centralized_controller(scenario, iter)
+function [hlc] = centralized_controller(hlc)
 % CENTRALIZED_CONTROLLER    Plan trajectory for one time step using a centralized controller.
 
     % initialize variable to store control results
-    info = ControllResultsInfo(scenario.options.amount, scenario.options.Hp, [scenario.vehicles.ID]);
+    hlc.info = ControllResultsInfo(hlc.scenario.options.amount, hlc.scenario.options.Hp, [hlc.scenario.vehicles.ID]);
     
-    sub_controller = @scenario.sub_controller;
+    sub_controller = @hlc.sub_controller;
     
     % falsifies controller_runtime slightly
     subcontroller_timer = tic;
-    info_v = sub_controller(scenario, iter);
+    info_v = sub_controller(hlc.scenario, hlc.iter);
     if info_v.is_exhausted
         % if graph search is exhausted, this vehicles and all vehicles that have directed or
         % undirected couplings with this vehicle will take fallback 
-        disp(['Graph search exhausted at time step: ' num2str(iter.k) '.'])
+        disp(['Graph search exhausted at time step: ' num2str(hlc.scenario.k) '.'])
         % all vehicles fall back
-        info.vehs_fallback = 1:scenario.options.amount;
-        info.is_exhausted(info.vehs_fallback) = true;
+        hlc.info.vehs_fallback = 1:hlc.scenario.options.amount;
+        hlc.info.is_exhausted(hlc.info.vehs_fallback) = true;
     else
         % prepare output data
-        info = store_control_info(info, info_v, scenario);
+        hlc.info = store_control_info(hlc.info, info_v, hlc.scenario);
     end
 
-    info.runtime_subcontroller_each_veh = toc(subcontroller_timer);
+    hlc.info.runtime_subcontroller_each_veh = toc(subcontroller_timer);
     % for centralize controller, all vehicles are in the same group
-    info.runtime_subcontroller_each_grp = info.runtime_subcontroller_each_veh;
-    info.runtime_subcontroller_max = info.runtime_subcontroller_each_veh;
-    info.runtime_graph_search_max = info.runtime_subcontroller_each_veh;
+    hlc.info.runtime_subcontroller_each_grp = hlc.info.runtime_subcontroller_each_veh;
+    hlc.info.runtime_subcontroller_max = hlc.info.runtime_subcontroller_each_veh;
+    hlc.info.runtime_graph_search_max = hlc.info.runtime_subcontroller_each_veh;
 end
