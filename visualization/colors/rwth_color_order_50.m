1ed0cf792e2d843728e57d400b8f321f13924486
Scheffe <scheffe@embedded.rwth-aachen.de>
2022-12-28 14:02:09 +0100

    rearrange colors

--- visualization/colors/rwth_color_order_50.m
+++ visualization/colors/rwth_color_order_50.m
@@ -1,8 +1,8 @@
 function result = rwth_color_order_50
     result = [ ...
-    137 204 207 %RWTH türkis RGB
     142 186 229 %RWTH blau RGB
     253 212 143 %RWTH orange RGB
+    137 204 207 %RWTH türkis RGB
     188 181 215 %RWTH Lila RGB
     230 150 121 %RWTH rot RGB
     224 230 154 %RWTH maigrün RGB
