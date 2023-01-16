2874872dc14564f2cc2ff31d18e4c2ca0ab8ab74
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-01-16 19:20:53 +0100

    formatting

--- manual_control/manual_control.m
+++ manual_control/manual_control.m
@@ -1,5 +1,5 @@
 function manual_control(vehicle_id, input_device_id, control_mode)
-    
+
     n_manual_vehicle = length(vehicle_id);
 
     assert(length(input_device_id) == n_manual_vehicle);
@@ -8,34 +8,39 @@ function manual_control(vehicle_id, input_device_id, control_mode)
     manual_controller = cell(n_manual_vehicle, 1);
 
     for i = 1:n_manual_vehicle
+
         switch control_mode(i)
             case ControlMode.ManualMode
                 manual_controller{i} = ManualMode(vehicle_id(i), input_device_id(i));
             case ControlMode.SemiAutonomousMode
-                manual_controller{i} = SemiAutonomousMode();%TODO%
+                manual_controller{i} = SemiAutonomousMode(); %TODO %
         end
+
     end
-    
-%     rate = ros2rate(manual_controller{1}.joy_node, ManualControl.rate_hz);
-% only possible from R2022b
-     while true
+
+    %     rate = ros2rate(manual_controller{1}.joy_node, ManualControl.rate_hz);
+    % only possible from R2022b
+    while true
         t_start = tic;
+
         for i = 1:n_manual_vehicle
             input_device_data = manual_controller{i}.decode_input_data();
+
             if isempty(input_device_data)
                 continue
             end
 
             [result, force_feedback] = manual_controller{i}.MessageProcessing(input_device_data);
+
             if isempty(result)
                 continue
             end
-            
+
             manual_controller{i}.apply(result, force_feedback);
         end
+
         t_loop = toc(t_start);
         pause(ManualControl.dt_seconds - t_loop);
     end
 
-    
 end
--- manual_control/motor_throttle_manual.m
+++ manual_control/motor_throttle_manual.m
@@ -1,17 +1,18 @@
-function result = motor_throttle_manual(...
-    accelerator_pedal_position, brake_pedal_position, speed...
-)
-arguments
-    accelerator_pedal_position (1,1) double = -1;
-    brake_pedal_position (1,1) double = -1;
-    speed (1,1) double = 0;
-end
+function result = motor_throttle_manual( ...
+        accelerator_pedal_position, brake_pedal_position, speed ...
+    )
 
-acceleration_desired = compute_desired_acceleration( ...
-    accelerator_pedal_position, brake_pedal_position, speed ...
-);
+    arguments
+        accelerator_pedal_position (1, 1) double = -1;
+        brake_pedal_position (1, 1) double = -1;
+        speed (1, 1) double = 0;
+    end
 
-result = compute_motor_throttle(acceleration_desired, speed);
+    acceleration_desired = compute_desired_acceleration( ...
+        accelerator_pedal_position, brake_pedal_position, speed ...
+    );
+
+    result = compute_motor_throttle(acceleration_desired, speed);
 
 end
 
@@ -37,18 +38,19 @@ function result = compute_max_acceleration(speed)
     % see https://gitlab.lrz.de/tum-cps/commonroad-vehicle-models/-/blob/master/vehicleModels_commonRoad.pdf
     scale = 1/18;
     max_acceleration = 11.5 * scale;
-    speed_no_slip = 7.5 * scale;
-    
+    speed_no_slip = 7.0 * scale;
+
     if (speed < speed_no_slip)
         result = max_acceleration;
     else
         result = max_acceleration * speed_no_slip ./ speed;
     end
+
 end
 
 function result = motor_throttle_from_pedal_and_max_acceleration( ...
-    pedal_position, max_acceleration ...
-)
+        pedal_position, max_acceleration ...
+    )
     min_acceleration = 0;
     d_acceleration = max_acceleration - min_acceleration;
     min_pedal_position = -1;
@@ -61,16 +63,19 @@ end
 function result = compute_motor_throttle(acceleration_desired, speed)
     % https://www.sciencedirect.com/science/article/pii/S2405896320324319
     p5 = -1.42;
-    p6 =  6.90;
-    p7 =  1.34;
+    p6 = 6.90;
+    p7 = 1.34;
 
+    % TODO check and tune s.t. acceleration is reached
     is_braking = (acceleration_desired < 0);
-    is_stop = ( abs(speed) < 0.05);
+    is_stop = (abs(speed) < 0.05);
+
     if (is_braking && is_stop)
         P = 1;
         result = -P * speed;
     else
-        x = ( acceleration_desired - p5 * speed ) / p6;
-        result = sign(x) * nthroot( abs(x) , p7 );
+        x = (acceleration_desired - p5 * speed) / p6;
+        result = sign(x) * nthroot(abs(x), p7);
     end
+
 end
