6ff2c1759cb2513b2bc1aaa4ed198a06478db465
Paul Kailer <kailer@embedded.rwth-aachen.de>
2023-05-06 13:44:57 +0200

    remove unused parameter lane_change_indices

--- helper/IterationData.m
+++ helper/IterationData.m
@@ -15,7 +15,6 @@ classdef IterationData
         occupied_areas % currently occupied areas with normal offset of vehicles
         emergency_maneuvers % occupied area of emergency braking maneuver
         last_trajectory_index % initial trajectory index
-        lane_change_indices % indices of the trajectory points of the lane before the lane change and the lane change lane
         lanes_before_update % lanes before path has been automatically updated in CPM Lab mode
         auto_updated_path % set in rhc_init to memorize when path is updated automatically
         adjacency % (nVeh x nVeh) matrix, entry is 1 if two vehicles drive in two adjacent lanelets and their distance are smaller enough
@@ -65,7 +64,6 @@ classdef IterationData
             obj.occupied_areas = cell(nVeh, 1);
             obj.emergency_maneuvers = cell(nVeh, 1);
             obj.last_trajectory_index = ones(nVeh, 1) * 10;
-            obj.lane_change_indices = zeros(nVeh, 10, 4);
             obj.lanes_before_update = zeros(nVeh, 1, 2);
             obj.auto_updated_path = false(nVeh, 1);
             obj.adjacency = zeros(nVeh, nVeh);
