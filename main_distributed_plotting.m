2d74d6e0328d1608a50e6572c95adf077727a2f7
Julius Kahle <julius.kahle@rwth-aachen.de>
2023-06-21 19:06:15 +0200

    fix: use buffer for plotting messages

--- main_distributed_plotting.m
+++ main_distributed_plotting.m
@@ -1,27 +1,31 @@
 function main_distributed_plotting()
+    close all
     scenario = load('scenario.mat', 'scenario').scenario;
+
     plotter = PlotterOnline(scenario);
     generate_plotting_info_msgs();
     ros2_node = ros2node('/plant_plotting');
+    options = struct("History", "keepall", "Reliability", "reliable", "Durability", "transientlocal");
     veh_ids = scenario.options.veh_ids;
     amount = scenario.options.amount;
-    subscribers = cell(amount, 1);
-
-    for i_veh = 1:amount
-        disp(['init subscriber for vehicle ', num2str(veh_ids(i_veh))])
-        subscribers{i_veh} = create_subscriber_plotting(ros2_node, plotter, veh_ids(i_veh));
-    end
+    global plotting_info_queue;
+    plotting_info_queue = empty_plotting_info_queue();
+    disp(['init subscriber for vehicles ', num2str(veh_ids)]);
+    topic_name_subscribe = ['/plant_plotting'];
+    subscriber = ros2subscriber(ros2_node, topic_name_subscribe, "plotting_info/PlottingInfo", @enqueue_plotting_info, options);
 
     while true
-        pause(0.5)
+
+        msg = dequeue_plotting_info();
+
+        if ~isempty(msg)
+            plotter.ros2_callback(msg);
+        end
+
+        pause(0.001);
+
     end
 
-end
-
-function subscriber = create_subscriber_plotting(ros2_node, plotter, vehicle_id)
-    options = struct("History", "keeplast", "Depth", 40, "Reliability", "reliable", "Durability", "transientlocal");
-    topic_name_subscribe = ['/plant_', num2str(vehicle_id), '_plotting'];
-    subscriber = ros2subscriber(ros2_node, topic_name_subscribe, "plotting_info/PlottingInfo", @plotter.ros2_callback, options);
 end
 
 function generate_plotting_info_msgs()
@@ -32,7 +36,7 @@ function generate_plotting_info_msgs()
         disp('Generating ROS 2 custom message type for distributed plotting...')
 
         try
-            ros2genmsg([file_path,filesep,'plant']);
+            ros2genmsg([file_path, filesep, 'plant']);
         catch ME
             disp(['If all environments for ros2genmsg() are prepared but still failed, try to move the whole folder to a ' ...
                   'shallower path and run again if you use Windows machine, which sadly has a max path limit constraint.'])
@@ -44,4 +48,32 @@ function generate_plotting_info_msgs()
               'message exists. If message types are missing regenerate all by removing the folder matlab_msg_gen...'])
     end
 
+end
+
+function enqueue_plotting_info(msg)
+    global plotting_info_queue;
+    plotting_info_queue(end + 1) = msg;
+end
+
+function msg = dequeue_plotting_info()
+    global plotting_info_queue;
+
+    if ~isempty(plotting_info_queue)
+        msg = plotting_info_queue(1);
+        plotting_info_queue(1) = [];
+    else
+        msg = [];
+    end
+
+end
+
+function plotting_info_queue = empty_plotting_info_queue()
+    plotting_info_queue = struct('trajectory_predictions', [], 'ref_trajectory', [], ...
+        'priorities', [], 'n_obstacles', [], ...
+        'n_dynamic_obstacles', [], 'step', [], ...
+        'veh_indices', [], 'tick_now', [], ...
+        'weighted_coupling_reduced', [], 'directed_coupling', [], ...
+        'belonging_vector', [], 'coupling_info', [], ...
+        'populated_coupling_infos', [], 'MessageType', []);
+    plotting_info_queue(1) = [];
 end
