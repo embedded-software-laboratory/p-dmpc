2f1358390f946ecd5974dbcf532326cfa63fe59c
Julius Kahle <julius.kahle@rwth-aachen.de>
2023-07-27 12:22:33 +0200

    chore: update list of working nucs

--- stop_remote_hlcs.sh
+++ stop_remote_hlcs.sh
@@ -6,7 +6,7 @@ export SSHPASS=
 VEHICLE_IDS=("$@")
 
 # list of working nucs to be used for execution
-WORKING_NUCS=(01 02 03 05 07 08 10 11 12 13 14 16 17 18 19)
+WORKING_NUCS=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19)
 
 # get numbers of vehicles
 N_VEH=${#VEHICLE_IDS[@]}
