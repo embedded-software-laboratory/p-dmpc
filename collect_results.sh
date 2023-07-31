0d355c2d8e44ba50a73bce57ff2b04622bc1bbd6
Julius Kahle <julius.kahle@rwth-aachen.de>
2023-07-31 11:21:22 +0200

    doc: fix comments in bash scripts for distr sim

--- collect_results.sh
+++ collect_results.sh
@@ -10,7 +10,7 @@ WORKING_NUCS=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19)
 # get numbers of vehicles
 N_VEH="$@"
 
-# step tmux sessions on all used hlcs
+# copy result from every hlc and delete directly to avoid two results
 for (( i=0; i<$N_VEH; i++ )); do
     sshpass -e scp -r guest@192.168.1.2${WORKING_NUCS[$i]}:/home/guest/dev/software/high_level_controller/graph_based_planning/results/commonroad_par-rhgs/* /tmp/$dirname
     sshpass -e ssh guest@192.168.1.2${WORKING_NUCS[$i]} 'rm -r /home/guest/dev/software/high_level_controller/graph_based_planning/results/commonroad_par-rhgs'
--- stop_remote_hlcs.sh
+++ stop_remote_hlcs.sh
@@ -11,7 +11,7 @@ WORKING_NUCS=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19)
 # get numbers of vehicles
 N_VEH="$@"
 
-# step tmux sessions on all used hlcs
+# stop tmux sessions on all used hlcs
 for (( i=0; i<$N_VEH; i++ )); do
     sshpass -e ssh -t guest@192.168.1.2${WORKING_NUCS[$i]} 'tmux kill-session -t "matlab-hlc"'
 done
