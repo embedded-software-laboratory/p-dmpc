4b1219db6e7989c7141f9ebf5d103a9885700508
Kevin Weißenfels <kevin.weissenfels@rwth-aachen.de>
2023-09-27 18:47:01 +0200

    read traffic message from each vehicle

--- hlc/controller/@HighLevelController/rhc_init.m
+++ hlc/controller/@HighLevelController/rhc_init.m
@@ -139,7 +139,7 @@ function rhc_init(obj, x_measured, trims_measured)
 
     end
 
-    if obj.scenario.options.is_prioritized && obj.amount == 1
+    if obj.scenario.options.is_prioritized
         %% read messages from other vehicles (There shouldn't be any other vehicles if centralized)
 
         for jVeh = obj.plant.indices_in_vehicle_list
