4f21aaed8292b39f91848e26ce9ed0eac4b71908
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-10-04 21:56:18 +0200

    swap dict with string list

--- hlc/controller/ControllerTiming.m
+++ hlc/controller/ControllerTiming.m
@@ -3,8 +3,8 @@ classdef ControllerTiming < handle
     properties (Access = private)
         controller_start_time (1, 1) uint64 % Start time of HLC
 
-        name_to_timing_per_timestep (1, 1) dictionary % maps string to column in `timings`
-        name_to_timing_once (1, 1) dictionary
+        names_of_timings_per_time_step (1, :) string % maps string to column in `timings`
+        names_of_timings_once (1, :) string
 
         timings_per_timestep (2, :, :) double % start time and duration of timers per timestep
         timings_once (2, :) double % start time and duration of timers executed once
@@ -13,9 +13,27 @@ classdef ControllerTiming < handle
     methods (Access = public)
 
         function obj = ControllerTiming()
-            obj.name_to_timing_once = dictionary(string([]), uint32([]));
-            obj.name_to_timing_per_timestep = dictionary(string([]), uint32([]));
             obj.controller_start_time = tic;
+        end
+
+        function column = name_to_timing_per_timestep(obj, name)
+            column = find(strcmp(obj.names_of_timings_per_time_step, name), 1);
+
+            if isempty(column)
+                obj.names_of_timings_per_time_step(end + 1) = name;
+                column = length(obj.names_of_timings_per_time_step);
+            end
+
+        end
+
+        function column = name_to_timing_once(obj, name)
+            column = find(strcmp(obj.names_of_timings_once, name), 1);
+
+            if isempty(column)
+                obj.names_of_timings_once(end + 1) = name;
+                column = length(obj.names_of_timings_once);
+            end
+
         end
 
         function start_timer(obj, name, timestep)
@@ -23,37 +41,17 @@ classdef ControllerTiming < handle
             arguments
                 obj (1, 1) ControllerTiming
                 name (1, 1) string
-                timestep = [] % optional
+                timestep (1, 1) {mustBeInteger, mustBePositive} = 1 % optional
             end
 
-            if isempty(timestep)
-                % create empty struct for timer start and elapsed time
-                if ~isKey(obj.name_to_timing_once, name)
-
-                    if isempty(values(obj.name_to_timing_once))
-                        obj.name_to_timing_once(name) = 1;
-                    else
-                        obj.name_to_timing_once(name) = max(values(obj.name_to_timing_once)) + 1;
-                    end
-
-                end
-
-                % set start time
-                obj.timings_once(1, obj.name_to_timing_once(name)) = toc(obj.controller_start_time);
+            % create empty struct for timer start and elapsed time
+            % set start time
+            if nargin == 2
+                column = obj.name_to_timing_once(name);
+                obj.timings_once(1, column) = toc(obj.controller_start_time);
             else
-                % create empty struct for timer start and elapsed time
-                if ~isKey(obj.name_to_timing_per_timestep, name)
-
-                    if isempty(values(obj.name_to_timing_per_timestep))
-                        obj.name_to_timing_per_timestep(name) = 1;
-                    else
-                        obj.name_to_timing_per_timestep(name) = max(values(obj.name_to_timing_per_timestep)) + 1;
-                    end
-
-                end
-
-                % set start time
-                obj.timings_per_timestep(1, obj.name_to_timing_per_timestep(name), timestep) = toc(obj.controller_start_time);
+                column = obj.name_to_timing_per_timestep(name);
+                obj.timings_per_timestep(1, column, timestep) = toc(obj.controller_start_time);
             end
 
         end
@@ -63,49 +61,20 @@ classdef ControllerTiming < handle
             arguments
                 obj (1, 1) ControllerTiming
                 name (1, 1) string
-                timestep = [] % optional
+                timestep (1, 1) {mustBeInteger, mustBePositive} = 1 % optional
             end
 
-            if isempty(timestep)
-                elapsed_time = toc(obj.controller_start_time) - obj.get_start_time(name);
+            assert(obj.timer_exists(name), "timer does not exist");
 
-                % save elapsed time
+            end_time = toc(obj.controller_start_time);
+
+            % save elapsed time
+            if nargin == 2
+                elapsed_time = end_time - obj.timings_once(1, obj.name_to_timing_once(name));
                 obj.timings_once(2, obj.name_to_timing_once(name)) = elapsed_time;
             else
-                elapsed_time = toc(obj.controller_start_time) - obj.get_start_time(name, timestep);
-
-                % save elapsed time
+                elapsed_time = end_time - obj.timings_per_timestep(1, obj.name_to_timing_per_timestep(name), timestep);
                 obj.timings_per_timestep(2, obj.name_to_timing_per_timestep(name), timestep) = elapsed_time;
-            end
-
-        end
-
-        function start_time = get_start_time(obj, name, timestep)
-
-            arguments
-                obj (1, 1) ControllerTiming
-                name (1, 1) string
-                timestep = [] % optional
-            end
-
-            if isempty(timestep)
-
-                if ~isKey(obj.name_to_timing_once, name)
-                    error("no start time for specified timer name");
-                end
-
-                start_time = obj.timings_once(1, obj.name_to_timing_once(name));
-            else
-
-                if ~isKey(obj.name_to_timing_per_timestep, name)
-                    error("no start time for specified timer name");
-                end
-
-                start_time = obj.timings_per_timestep(1, obj.name_to_timing_per_timestep(name), timestep);
-            end
-
-            if isempty(start_time) || (start_time == 0)
-                error("start time for timer not set or set to zero");
             end
 
         end
@@ -115,27 +84,15 @@ classdef ControllerTiming < handle
             arguments
                 obj (1, 1) ControllerTiming
                 name (1, 1) string
-                timestep = [] % optional
+                timestep (1, 1) {mustBeInteger, mustBePositive} = 1 % optional
             end
 
-            if isempty(timestep)
+            assert(obj.timer_exists(name), "timer does not exist");
 
-                if ~isKey(obj.name_to_timing_once, name)
-                    error("no time elapsed for specified timer name");
-                end
-
+            if nargin == 2
                 elapsed_time = obj.timings_once(2, obj.name_to_timing_once(name));
             else
-
-                if ~isKey(obj.name_to_timing_per_timestep, name)
-                    error("no time elapsed for specified timer name");
-                end
-
                 elapsed_time = obj.timings_per_timestep(2, obj.name_to_timing_per_timestep(name), timestep);
-            end
-
-            if isempty(elapsed_time) || (elapsed_time == 0)
-                error("elapsed time for timer not set or set to zero");
             end
 
         end
@@ -143,22 +100,23 @@ classdef ControllerTiming < handle
         function elapsed_times = get_all_elapsed_times(obj)
             elapsed_times = struct;
 
-            % save results from timins_once
-            timer_names = keys(obj.name_to_timing_once);
-
-            for i = 1:length(timer_names)
-                name = timer_names{i};
+            % save results from timings_once
+            for name = obj.names_of_timings_once
                 elapsed_times.(name) = obj.timings_once(:, obj.name_to_timing_once(name));
             end
 
             % save results from timings_per_timestep
-            timer_names = keys(obj.name_to_timing_per_timestep);
-
-            for i = 1:length(timer_names)
-                name = timer_names{i};
+            for name = obj.names_of_timings_per_time_step
                 elapsed_times.(name) = obj.timings_per_timestep(:, obj.name_to_timing_per_timestep(name), :);
             end
 
+        end
+
+        function tf = timer_exists(obj, name)
+            tf = ~( ...
+                isempty(find(strcmp(obj.names_of_timings_per_time_step, name), 1)) ...
+                && isempty(find(strcmp(obj.names_of_timings_once, name), 1)) ...
+            );
         end
 
     end
