8cf59e6234e74aee4bcb650bc924e609d2600d52
Kevin Weißenfels <kevin.weissenfels@rwth-aachen.de>
2023-10-06 14:38:54 +0200

    move ros2 message generation for hlc into separate function

--- hlc/communication/create_publishers.m
+++ hlc/communication/create_publishers.m
@@ -7,38 +7,8 @@ function create_publishers(hlc)
     %   hlc: handle of high level controller
     %
 
-    % generate custom message type (for vehicle communication) if not exist
-    msgList = ros2("msg", "list"); % get all ROS 2 message types
-    [file_path, ~, ~] = fileparts(mfilename('fullpath'));
-
-    if ((sum(cellfun(@(c)strcmp(c, 'veh_msgs/Traffic'), msgList)) == 0) || (sum(cellfun(@(c)strcmp(c, 'veh_msgs/Predictions'), msgList)) == 0))
-        % if the message type 'veh_msgs/Traffic' does not exist
-        path_custom_msg = [file_path, filesep, 'cust1'];
-
-        % Generate custom messages. NOTE that Python, CMake, and a C++ compiler are required (see
-        % https://de.mathworks.com/help/ros/gs/ros-system-requirements.html
-        % for more details according to your own MATLAB version).
-        %
-        % Useful functions:
-        % 1. pyenv % check python version used by MATLAB
-        % 2. pyenv('Version','requiredPythonVersionNumber') or pyenv('Version','fullPathOfPythonExe')
-        % 3. !cmake --version % CMake version
-        % 4. mex -setup % set c language compiler
-        %
-        % Note that sometimes ros2genmsg fails although all denpendencies
-        % exist because the path where the custom messages are stored is
-        % too deep. Try to move them to shallower path and try again.
-        disp('Generating ROS 2 custom message type...')
-
-        try
-            ros2genmsg(path_custom_msg)
-        catch ME
-            disp(['If all environments for ros2genmsg() are prepared but still failed, try to move the whole folder to a ' ...
-                  'shallower path and run again if you use Windows machine, which sadly has a max path limit constraint.'])
-            throw(ME)
-        end
-
-    end
+    % generated message types if not already existing
+    hlc_ros2gen();
 
     timer_nodes = tic;
     disp('Creating ROS 2 nodes...');
