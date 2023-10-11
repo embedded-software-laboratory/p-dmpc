19705dd4f28dc91f9e8ec95193288b09e1e3e968
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-10-11 16:29:31 +0200

    add options type

--- scenarios/Scenario.m
+++ scenarios/Scenario.m
@@ -5,7 +5,7 @@ classdef Scenario
         vehicles = []; % array of Vehicle objects
         obstacles = {}; % static obstacles = {[xs;ys],...}
         lanelet_crossing_areas = {}; % crossing area of one vehicle's lanelet with another vehicle's lanelet
-        options;
+        options Config;
 
         model = [];
         r_goal = 0.1; % goal circle
