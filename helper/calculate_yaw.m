20335ed8cf423b7d0fc2df0c469f6f5a39d393bd
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-10-15 19:59:48 +0200

    refactor calculate yaw function

--- helper/calculate_yaw.m
+++ helper/calculate_yaw.m
@@ -1,36 +1,19 @@
 function yaw = calculate_yaw(refPath)
+    % calculate_yaw - Calculates the yaw angle of the reference path
 
-    nrefPoints = length(refPath);
-    yaw = zeros(nrefPoints, 1);
+    yaw = zeros(length(refPath), 1);
 
-    dx1 = refPath(2, 1) - refPath(1, 1);
-    dy1 = refPath(2, 2) - refPath(1, 2);
+    dpos = refPath(3:end, :) - refPath(1:end - 2, :);
+    yaw(2:end - 1) = atan2(dpos(:, 2), dpos(:, 1));
 
-    if dx1 < 0
-        yaw(1, 1) = atan(dy1 / dx1) + pi;
-    else
-        yaw(1, 1) = atan(dy1 / dx1);
-    end
+    yaw(1) = atan2( ...
+        refPath(2, 2) - refPath(1, 2), ...
+        refPath(2, 1) - refPath(1, 1) ...
+    );
 
-    for i = 2:nrefPoints - 1
-        dxi = refPath(i + 1, 1) - refPath(i - 1, 1);
-        dyi = refPath(i + 1, 2) - refPath(i - 1, 2);
-
-        if dxi < 0
-            yaw(i, 1) = atan(dyi / dxi) + pi;
-        else
-            yaw(i, 1) = atan(dyi / dxi);
-        end
-
-    end
-
-    dx_end = refPath(nrefPoints, 1) - refPath(nrefPoints - 1, 1);
-    dy_end = refPath(nrefPoints, 2) - refPath(nrefPoints - 1, 2);
-
-    if dx_end < 0
-        yaw(nrefPoints, 1) = atan(dy_end / dx_end) + pi;
-    else
-        yaw(nrefPoints, 1) = atan(dy_end / dx_end);
-    end
+    yaw(end) = atan2( ...
+        refPath(end, 2) - refPath(end - 1, 2), ...
+        refPath(end, 1) - refPath(end - 1, 1) ...
+    );
 
 end
