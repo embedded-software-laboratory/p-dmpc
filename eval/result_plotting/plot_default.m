6034f66abe440981dd995869db10fe64cbd845f8
fynnmaarten <fynnmaarten.business@gmail.com>
2023-10-16 16:21:49 +0200

    pass result instead of hlc as argument

--- eval/result_plotting/plot_default.m
+++ eval/result_plotting/plot_default.m
@@ -1,15 +1,15 @@
-function plot_default(hlc, options)
+function plot_default(result, options)
 
     arguments
-        hlc (1, 1) HighLevelController;
+        result (1, 1);
         options.do_export (1, 1) logical = false;
     end
 
-    plot_mpa(hlc.mpa, hlc.scenario, ...
+    plot_mpa(result.mpa, result.scenario, ...
         do_export = options.do_export);
-    plot_mpa_over_time(hlc.mpa, hlc.scenario, ...
+    plot_mpa_over_time(result.mpa, result.scenario, ...
         do_export = options.do_export);
-    plot_mpa_local_reachable_sets(hlc.mpa, ...
+    plot_mpa_local_reachable_sets(result.mpa, ...
         do_export = options.do_export);
 
 end
