08a130d5d1dc26553ed53e4805b238ffaec775c4
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-10-17 13:55:52 +0200

    remove  `boundary`, `priority_list`

--- scenarios/Scenario.m
+++ scenarios/Scenario.m
@@ -15,13 +15,11 @@ classdef Scenario
 
         lanelets; % coordinates of all lanelets
         intersection_lanelets; % IDs of intersection lanelets
-        boundary;
         road_raw_data; % raw road data
         road_data_file_path; % path to file of road data
         lanelet_boundary; % boundaries of all lanelets
         lanelet_relationships; % relationship between two adjacent lanelets
         adjacency_lanelets; % (nLanelets x nLanelets) matrix, entry is 1 if two lanelets are adjacent
-        priority_list = 1; % priority list of vehicles; a smaller value for a higher priority
         intersection_center = [2.25, 2]; % (numOfIntersection x 2) matrix, positions of intersection center
         random_stream = RandStream('mt19937ar'); % for reproducibility
     end
