bb17291b386045e60ad4edbd049c36b9bea443f6
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-10-19 12:11:45 +0200

    #166 remove relocated manual_control folder

--- manual_control/launch_j0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-# source ros
-source /opt/ros/eloquent/setup.bash
-
-# launch ros2node
-ros2 launch ${SCRIPT_DIR}/launch_j0.xml
