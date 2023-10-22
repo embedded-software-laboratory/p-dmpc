544fd0c21df15a13f5b0803b545ffc9748663798
Kevin Weißenfels <kevin.weissenfels@rwth-aachen.de>
2023-10-22 20:19:04 +0200

    [get_reference_trajectory] change input parameters

--- helper/get_reference_trajectory.m
+++ helper/get_reference_trajectory.m
@@ -1,4 +1,4 @@
-function [reference, v_ref] = get_reference_trajectory(scenario, mpa, iter, iVeh, x0, y0)
+function [reference, v_ref] = get_reference_trajectory(mpa, total_reference_path, x0, y0, trim, dt_seconds)
     % get_reference_trajectory This function calculate the reference path and
     % speed for the prediction horizon based on the vehicle's current state and
     % the reference path from the scenario
@@ -11,30 +11,30 @@ function [reference, v_ref] = get_reference_trajectory(scenario, mpa, iter, iVeh
     %   v_ref (Hp, 1) reference speed
 
     arguments
-        scenario Scenario
         mpa MotionPrimitiveAutomaton
-        iter IterationData
-        iVeh (1, 1) double % index of the vehicle in the vehicle list
+        total_reference_path (:, 2) double % total reference_path from scenario
         x0 (1, 1) double % current x coordinate of the vehicle
         y0 (1, 1) double % current y coordinate of the vehicle
+        trim (1, 1) double % current trim of the vehicle
+        dt_seconds (1, 1) double % step time
     end
 
     Hp = size(mpa.transition_matrix_single, 3);
 
     % get reference speed and path points
-    v_ref = mpa.get_max_speed(iter.trim_indices(iVeh));
+    v_ref = mpa.get_max_speed(trim);
 
     % determine intermediate speed between every two consecutive speeds
-    v_current = mpa.trims(iter.trim_indices(iVeh)).speed;
+    v_current = mpa.trims(trim).speed;
     v_ref_intermediate = ([v_current; v_ref(1:end - 1)] + v_ref) / 2;
 
     % distance that can be traveled in each step of Hp
-    distance_max = v_ref_intermediate * scenario.options.dt_seconds;
+    distance_max = v_ref_intermediate * dt_seconds;
 
     % Find equidistant points on the reference trajectory.
     reference = sample_reference_trajectory( ...
         Hp, ... % number of prediction steps
-        scenario.vehicles(iVeh).reference_path, ... % total reference path
+        total_reference_path, ... % total reference path
         x0, ... % vehicle position x
         y0, ... % vehicle position y
         distance_max ... % distance traveled in one time step
