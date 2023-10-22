637274dd043c1695e4d38a70c57969f07296b960
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-10-22 13:57:26 +0200

    do not send ready message for HDVs

--- plant/CpmLab.m
+++ plant/CpmLab.m
@@ -201,7 +201,7 @@ classdef CpmLab < Plant
             % Send ready signal for all assigned vehicle ids
             disp('Sending ready signal');
 
-            for iVehicle = [obj.controlled_vehicle_ids, obj.scenario.options.manual_control_config.hdv_ids]
+            for iVehicle = obj.controlled_vehicle_ids
                 ready_msg = ReadyStatus;
                 ready_msg.source_id = strcat('hlc_', num2str(iVehicle));
                 ready_stamp = TimeStamp;
