3f9b8a9345e41063dfc470a9a1c5843fa5238c53
Kevin Weißenfels <kevin.weissenfels@rwth-aachen.de>
2023-10-24 20:35:33 +0200

    extract bound_reachable_sets from get_reachable_sets

--- helper/get_reachable_sets.m
+++ helper/get_reachable_sets.m
@@ -1,25 +1,19 @@
-function reachable_sets = get_reachable_sets(x0, y0, yaw0, local_reachable_sets, predicted_lanelet_boundary, options)
+function reachable_sets = get_reachable_sets(x0, y0, yaw0, local_reachable_sets)
     % GET_REACHABLE_SETS Calculate the reachable sets based on the current pose
-    % and trim of the vehicle by tranlating the local reachable sets. If
-    % vehicle's predicted lanelet boundary is available, the reachable sets
-    % will be constrained by the lanelet boundary.
-    %
-    % Input:
-    %   x0: x-coordinate in the global coordinate system
-    %   y0: y-coordinate in the global coordinate system
-    %   yaw0: yaw angle in the global coordinate system
-    %   trim_current: current trim
-    %   local_reachable_sets: local reachable sets without information about vehicle's pose
-    %   predicted_lanelet_boundary: the boundary of vehicle's predicted lanelets
+    % and trim of the vehicle by translating the local reachable sets.
     %
     % Output:
-    %   reachable_sets: cell(1,Hp), the reachable sets in the defined predition horizon
-    %
+    %   the reachable sets in the defined prediction horizon
+    %   reachable_sets (1, Hp) cell
 
-    %     % take roughly 15% of computation time
-    %     % Process optional input and Name-Value pair options
-    %     [x0, y0, yaw0, local_reachable_sets, predicted_lanelet_boundary, is_allow_non_convex] = ...
-    %         parse_inputs(x0, y0, yaw0, local_reachable_sets, varargin{:});
+    arguments
+        x0 (1, 1) double % current x coordinate of the vehicle
+        y0 (1, 1) double % current y coordinate of the vehicle
+        yaw0 (1, 1) double % current yaw of the vehicle
+
+        % local reachable sets of the vehicle's current trim
+        local_reachable_sets (1, :) cell % (1, Hp)
+    end
 
     Hp = size(local_reachable_sets, 2);
     reachable_sets = cell(1, Hp);
@@ -39,26 +33,6 @@ function reachable_sets = get_reachable_sets(x0, y0, yaw0, local_reachable_sets,
             reachable_set_x, ...
             reachable_set_y, ...
             'Simplify', false ...
-        );
-    end
-
-    % constrain the reachable sets by the boundaries of the predicted lanelets
-    if ( ...
-            options.scenario_type ~= ScenarioType.circle && ...
-            options.bound_reachable_sets ...
-        )
-
-        [reachable_sets] = bound_reachable_sets( ...
-            reachable_sets, ...
-            predicted_lanelet_boundary{3} ...
-        );
-
-    end
-
-    % force convex reachable sets if non-convex polygons are not allowed
-    if ~options.is_allow_non_convex
-        reachable_sets = cellfun(@(c) convhull(c), reachable_sets, ...
-            'UniformOutput', false ...
         );
     end
 
