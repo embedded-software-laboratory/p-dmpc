38259f7e1a5875b32dc43c3db5c321b40025c90d
Johannes Rolf <johannes.rolf@rwth-aachen.de>
2023-10-30 16:54:48 +0100

    runtimeplot for one step but currently only for one vehicle

--- eval/timing_plotting/plot_runtime_for_step.m
+++ eval/timing_plotting/plot_runtime_for_step.m
@@ -9,69 +9,36 @@ function plot_runtime_for_step(result, k)
     % Configure, which field names in the timing object are relevant, dependent on the used controller
     if result.scenario.options.is_prioritized & result.scenario.options.compute_in_parallel
         field_names = [ ...
-                           "plan_single_vehicle", ...
                            "optimizer", ...
                            "fallback", ...
                            "publish_predictions" ...
-                       ];
+                       ]; %"plan_single_vehicle", ...
         timings = result.timings_per_vehicle; % Will be unnecessary as soon as only one timing object exists
     else
         error('The graph is currently only supported for results of prioritized, distributed execution.');
     end
 
-    % axes to plot in
-    ax = axes;
+    colors = ["#0072BD", "#D95319", "#EDB120", "#7E2F8E", "#77AC30", "#4DBEEE", "#A2142F"];
 
-    categories = categorical(field_names);
-    disp(categories);
+    veh_i_to_draw = [1:result.scenario.options.amount; 1:result.scenario.options.amount];
 
-    for i = 1:length(field_names)
-        field_name = field_names(i);
-        disp(field_name);
-        t_start(i) = timings.(field_name)(1, 1, k);
-        duration = timings.(field_name)(2, 1, k);
-        t_end(i) = t_start(i) + duration;
+    for field_i = 1:length(field_names)
+        field_name = field_names(field_i);
 
-        % disp([c; c]);
-        % disp([t_start; t_end]);
-        % plot([t_start, t_start + 1; t_end, t_end + 1], [c; c], 'LineWidth', 5);
+        for veh_i = 1:result.scenario.options.amount
+            t_start = timings.(field_name)(1, 1, k);
+            duration = timings.(field_name)(2, 1, k);
+            time_to_draw(:, veh_i) = [t_start, t_start + duration];
+        end
 
-        % break;
-
-        %     barh(i, t_end);
-        %     hold on;
-        %     bh1 = barh(i, t_start);
-        %     bh1.FaceColor = [1 1 1];
-        %     bh1.LineStyle = 'None';
-        %     bh1.EdgeColor = [1 1 1];
+        plt(:, field_i) = plot(time_to_draw, veh_i_to_draw, 'Color', colors(field_i), 'LineWidth', 5);
+        hold on;
     end
 
-    disp(t_start);
-    disp(t_end);
-
-    plot([t_start; t_end], [categories; categories], 'LineWidth', 5);
-
-    set(gca, 'TickLabelInterpreter', 'none');
-
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % minimum = rand(10, 3);
-    % maximum = rand(10, 3) + minimum;
-
-    % bH = bar(maximum);
-    % hold on;
-    % bH1 = bar(minimum);
-
-    % for ii = bH1
-    %     ii.FaceColor = [1 1 1];
-    %     ii.LineStyle = 'None';
-    %     ii.EdgeColor = [1 1 1];
-    % end
-
-    % for ii = bH
-    %     ii.LineStyle = 'None';
-    %     ii.EdgeColor = [1 1 1];
-    % end
-
-    % axes('Position', aH.Position, 'XTick', [], 'YTick', [], 'Color', 'None');
+    legend(plt(1, :), cellstr(field_names), 'Interpreter', 'none');
+    xlabel('Time [ms]');
+    ylabel('Vehicle');
+    yticks(1:result.scenario.options.amount);
+    ylim([1 - 0.2, result.scenario.options.amount + 0.2])
 
 end
