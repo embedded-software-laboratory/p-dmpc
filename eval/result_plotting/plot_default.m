75f7b6678d4c301c99594e7e8eccce16fa6e5a8f
fynnmaarten <fynnmaarten.business@gmail.com>
2023-10-30 14:06:19 +0100

    consistent naming of results in eval functions

--- eval/result_plotting/plot_default.m
+++ eval/result_plotting/plot_default.m
@@ -1,19 +1,19 @@
-function plot_default(result, options)
+function plot_default(results, options)
 
     arguments
-        result (1, 1) struct;
+        results (1, 1) struct;
         options.do_export (1, 1) logical = true;
     end
 
-    plot_mpa(result.mpa, result.scenario, do_export = options.do_export);
-    plot_mpa_over_time(result.mpa, result.scenario, do_export = options.do_export);
-    plot_mpa_local_reachable_sets(result.mpa, result.scenario, do_export = options.do_export);
+    plot_mpa(results.mpa, results.scenario, do_export = options.do_export);
+    plot_mpa_over_time(results.mpa, results.scenario, do_export = options.do_export);
+    plot_mpa_local_reachable_sets(results.mpa, results.scenario, do_export = options.do_export);
 
-    plot_computation_time(result, do_export = options.do_export);
+    plot_computation_time(results, do_export = options.do_export);
 
-    plot_trajectories(result, do_export = options.do_export);
+    plot_trajectories(results, do_export = options.do_export);
 
-    plot_scenario(result.scenario, do_export = options.do_export);
+    plot_scenario(results.scenario, do_export = options.do_export);
     step_indices = [1, 5, 9];
-    plot_experiment_snapshots(result, step_indices, do_export = options.do_export);
+    plot_experiment_snapshots(results, step_indices, do_export = options.do_export);
 end
