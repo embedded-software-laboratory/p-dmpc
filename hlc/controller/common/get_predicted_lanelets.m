54cbd86cdb3e3069fb94d5a45b146361774a3091
Kevin Weißenfels <kevin.weissenfels@rwth-aachen.de>
2023-11-06 10:32:02 +0100

    [get_predicted_lanelets()] fix: rename to reference_trajectory_struct

--- hlc/controller/common/get_predicted_lanelets.m
+++ hlc/controller/common/get_predicted_lanelets.m
@@ -1,4 +1,4 @@
-function [predicted_lanelets] = get_predicted_lanelets(scenario, iVeh, reference_path_struct)
+function [predicted_lanelets] = get_predicted_lanelets(scenario, iVeh, reference_trajectory_struct)
     % GET_PREDICTED_LANELETS This function calculate the predicted lanelets
     % based on vehicle's reference path.
     %
@@ -10,13 +10,13 @@ function [predicted_lanelets] = get_predicted_lanelets(scenario, iVeh, reference
         scenario Scenario
         % index of the vehicle in the vehicle list
         iVeh (1, 1) double
-        % reference_path_struct struct with the fields
-        %   path (Hp, 2) [x1 y1; x2 y2; ...]
+        % reference_trajectory_struct struct with the fields
+        %   trajectory (Hp, 2) [x1 y1; x2 y2; ...]
         %   points_index (Hp, 1) point indices
-        reference_path_struct (1, 1) struct
+        reference_trajectory_struct (1, 1) struct
     end
 
-    ref_points_index = reference_path_struct.points_index;
+    ref_points_index = reference_trajectory_struct.points_index;
     %     ref_points_index = [curTrajectoryIndex;ref_points_index]; % add current index of vehicle on its trajectory to consider the current position of the vehicle
 
     % predict several points more such that the predicted lanelets can cover all reachable set. Only in this way, the bounded reachable sets will not be cutoff at its front
