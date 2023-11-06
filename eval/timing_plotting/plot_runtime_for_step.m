a39305c663c3d1e75f6bcda986c7689a6ba67b07
Johannes Rolf <johannes.rolf@rwth-aachen.de>
2023-11-06 18:08:03 +0100

    Normalize plot_runtime_for_step to the start of the current time step

--- eval/timing_plotting/plot_runtime_for_step.m
+++ eval/timing_plotting/plot_runtime_for_step.m
@@ -1,12 +1,10 @@
 function plot_runtime_for_step(results, k)
-    %% PLOT_RUNTIME_FOR_STEP plots the timing of the specified timestep
+    %% PLOT_RUNTIME_FOR_STEP plots the timing of the specified timestep where t=0 is the start of the first hlc_init measurement
 
     arguments
         results (1, :) cell; % cell array with the result structs of all vehicles
         k uint64;
     end
-
-    disp(results{1, 1});
 
     % useful variable for shorter notations
     options = results{1, 1}.scenario.options;
@@ -14,12 +12,21 @@ function plot_runtime_for_step(results, k)
     % Configure, which field names in the timing object are relevant, dependent on the used controller
     if options.is_prioritized & options.compute_in_parallel
         field_names = [ ...
+                           "hlc_step", ...
+                           "traffic_situation_update", ...
                            "optimizer", ...
                            "fallback", ...
                            "publish_predictions" ...
                        ]; %"plan_single_vehicle", ...
     else
         error('The graph is currently only supported for results of prioritized, distributed execution.');
+    end
+
+    % Find minimum in start of first measurement, i.e., "hlc_init", to normalize time by that.
+    t0 = realmax;
+
+    for veh_i = 1:options.amount
+        t0 = min(t0, results{veh_i}.timings_general.hlc_step(1, 1, k));
     end
 
     % Configure the colors that shall be used (in this order)
@@ -29,8 +36,15 @@ function plot_runtime_for_step(results, k)
         field_name = field_names(field_i);
 
         for veh_i = 1:options.amount
-            timings = results{1, veh_i}.timings_per_vehicle(veh_i);
-            t_start = timings.(field_name)(1, 1, k);
+
+            if isfield(results{1, veh_i}.timings_per_vehicle(veh_i), field_name)
+                timings = results{1, veh_i}.timings_per_vehicle(veh_i);
+            else
+                timings = results{1, veh_i}.timings_general(1);
+            end
+
+            disp(timings)
+            t_start = timings.(field_name)(1, 1, k) - t0; % Normalize (see above)
             duration = timings.(field_name)(2, 1, k);
             time_to_draw(:, veh_i) = [t_start, t_start + duration];
         end
