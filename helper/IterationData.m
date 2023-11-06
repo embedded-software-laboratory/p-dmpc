f78ec90cbfbfb6d4341a742cbdd00b9062c73bb9
Kevin Weißenfels <kevin.weissenfels@rwth-aachen.de>
2023-11-06 20:48:26 +0100

    [IterationData] remove unused property vehicle_to_lanelet

--- helper/IterationData.m
+++ helper/IterationData.m
@@ -20,7 +20,6 @@ classdef IterationData
         dynamic_obstacle_area % (nObs x Hp) cell of areas [x; y]
         dynamic_obstacle_shape % (1 x 2) array [Length, Width] for all obstacles
         dynamic_obstacle_fullres % (nObs x ?) cell array
-        vehicle_to_lanelet
         weighted_coupling % (nVeh x nVeh) matrix, coupling weights of all coupling vehicle pair; higher value indicates stronger coupling
         weighted_coupling_reduced % reduced coupling weights by forbidding vehicles entering their lanelet crossing areas
         coupling_info % couling information of each coupling pair
@@ -58,7 +57,6 @@ classdef IterationData
             obj.dynamic_obstacle_area = scenario.dynamic_obstacle_area;
             obj.dynamic_obstacle_shape = {};
             obj.dynamic_obstacle_fullres = {};
-            obj.vehicle_to_lanelet = zeros(nVeh, 1);
             obj.directed_coupling = zeros(nVeh, nVeh);
             obj.directed_coupling_reduced = zeros(nVeh, nVeh);
             obj.weighted_coupling = zeros(nVeh, nVeh);
