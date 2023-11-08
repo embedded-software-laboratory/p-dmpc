c2d98578557c4384658aa625652846834010e1ef
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-11-08 15:18:12 +0100

    [prioritizer] add test

---
+++ tests/componenttests/test_prioritizer.m
@@ -0,0 +1,56 @@
+function tests = test_prioritizer
+    tests = functiontests(localfunctions);
+end
+
+function TEST_topologically_unique_permutations(testCase)
+    % setup
+    prioritizer = ConstantPrioritizer();
+    n_agents = 6;
+    rand_stream = RandStream("mt19937ar", "Seed", 0);
+    adjacency = rand(rand_stream, n_agents, n_agents) > 0.5;
+    adjacency = triu(adjacency, 1) + triu(adjacency, 1)';
+
+    prioritizer.compute_unique_priorities(adjacency);
+
+    % expected values
+    all_permuations = perms(1:n_agents);
+
+    % none are wrong
+    testCase.verifyTrue(all(ismember(prioritizer.all_priorities', all_permuations, "rows")));
+
+    % none are topological duplicates
+    topologically_unique_permutations_actual = zeros(0, n_agents);
+
+    for i = 1:prioritizer.n_priorities
+        permutation = prioritizer.all_priorities(:, i)';
+        directed_coupling = Prioritizer.directed_coupling_from_priorities(adjacency, permutation);
+        coupling_dag = digraph(directed_coupling);
+        topological_order = toposort(coupling_dag, Order = 'stable');
+
+        testCase.verifyTrue( ...
+            ~ismember(topological_order, topologically_unique_permutations_actual, "rows") ...
+        );
+        topologically_unique_permutations_actual = [topologically_unique_permutations_actual; topological_order]; %#ok<AGROW>
+
+    end
+
+    % all topologically unique permutations are contained
+    % compute topologically unique permutations in a different manner
+    topologically_unique_permutations_expected = zeros(0, n_agents);
+
+    for i = 1:size(all_permuations, 1)
+        permutation = all_permuations(i, :);
+        directed_coupling = Prioritizer.directed_coupling_from_priorities(adjacency, permutation);
+        coupling_dag = digraph(directed_coupling);
+        topological_order = toposort(coupling_dag, Order = 'stable');
+
+        if ~ismember(topological_order, topologically_unique_permutations_expected, "rows")
+            topologically_unique_permutations_expected = [topologically_unique_permutations_expected; topological_order]; %#ok<AGROW>
+        end
+
+    end
+
+    testCase.verifyTrue(all(ismember(topologically_unique_permutations_actual, topologically_unique_permutations_expected, "rows")));
+    testCase.verifyEqual(size(topologically_unique_permutations_actual, 1), size(topologically_unique_permutations_expected, 1));
+
+end
