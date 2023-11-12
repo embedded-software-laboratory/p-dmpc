737d94a13aae4961f8b82d693ce85a4a00dbb6cc
Kevin Weißenfels <kevin.weissenfels@rwth-aachen.de>
2023-11-12 16:37:14 +0100

    [commonroad_random] store random options in return arg

--- scenarios/commonroad_random.m
+++ scenarios/commonroad_random.m
@@ -12,6 +12,8 @@ function [options_array, scenario_array] = commonroad_random(options, amounts, s
         seeds (1, :) double
     end
 
+    path_id_max = 41; % maximum defined path id
+
     disp('Creating options and scenarios...')
     options_array(length(amounts), length(seeds)) = Config();
     scenario_array(length(amounts), length(seeds)) = Scenario();
@@ -19,11 +21,14 @@ function [options_array, scenario_array] = commonroad_random(options, amounts, s
     for i_amount = 1:length(amounts)
 
         for i_seed = 1:length(seeds)
+            random_stream = RandStream('mt19937ar', 'Seed', seeds(i_seed));
+
             options_random = options;
             options_random.amount = amounts(i_amount);
-            random_stream = RandStream('mt19937ar', 'Seed', seeds(i_seed));
-            path_ids = sort(randsample(random_stream, 1:40, options_random.amount), 'ascend');
-            options_random.path_ids = path_ids;
+            options_random.path_ids = randperm(random_stream, path_id_max, options_random.amount);
+
+            options_array(i_amount, i_seed) = options_random;
+
             scenario = commonroad_scenario(options_random.amount, options_random.path_ids);
 
             % FIXME this property is not supported anymore
