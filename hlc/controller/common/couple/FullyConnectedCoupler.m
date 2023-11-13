9d12cf6fc561623d34500d88b74033f7dea535c9
Johannes Rolf <johannes.rolf@rwth-aachen.de>
2023-11-13 14:37:08 +0100

    Remove superfluous comment

--- hlc/controller/common/couple/FullyConnectedCoupler.m
+++ hlc/controller/common/couple/FullyConnectedCoupler.m
@@ -3,7 +3,7 @@ classdef FullyConnectedCoupler < Coupler
     methods
 
         function [adjacency] = couple(~, iter)
-            adjacency = ones(iter.amount) - eye(iter.amount); % create matrix with 0s on the diagonal and 1s everywhere else
+            adjacency = ones(iter.amount) - eye(iter.amount);
         end
 
     end
