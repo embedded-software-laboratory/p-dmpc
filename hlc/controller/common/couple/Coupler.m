2b9193c2a1f8ea054aaf8422c4226dcedeaa913f
Johannes Rolf <johannes.rolf@rwth-aachen.de>
2023-11-13 14:32:33 +0100

    Delete CouplerFactory and move static function for object creation into Coupler

--- hlc/controller/common/couple/Coupler.m
+++ hlc/controller/common/couple/Coupler.m
@@ -16,6 +16,25 @@ classdef (Abstract) Coupler < handle
 
     end
 
+    methods (Static)
+
+        function coupler = get_coupler(coupling)
+            %GET_COUPLER creates a coupler according to the set option
+            arguments
+                coupling CouplerStrategies;
+            end
+
+            switch (coupling)
+                case CouplerStrategies.reachable_set_coupling
+                    coupler = ReachableSetCoupler();
+                case CouplerStrategies.full_coupling
+                    coupler = FullyConnectedCoupler();
+            end
+
+        end
+
+    end
+
     methods
 
         function obj = Coupler()
