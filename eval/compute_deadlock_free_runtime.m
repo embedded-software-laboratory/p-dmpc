17ec9316e4a60f913a5539876c8531ce4bcb98a0
fynnmaarten <fynnmaarten.business@gmail.com>
2023-11-13 16:07:56 +0100

    add is_deadlock function

--- eval/compute_deadlock_free_runtime.m
+++ eval/compute_deadlock_free_runtime.m
@@ -1,18 +1,22 @@
-function [nSteps, t] = compute_deadlock_free_runtime(result)
-    % COMPUTE_DEADLOCK_FREE_RUNTIME computes the deadlock-free runtime of a result
+function [n_steps, t] = compute_deadlock_free_runtime(experiment_result)
+    % COMPUTE_DEADLOCK_FREE_RUNTIME computes the deadlock-free runtime of a experiment_result
     % struct
 
     % TODO detect deadlock from standstill
     % option 1: "heuristic" -- stopping for certain time
     % option 2: check all permutations of priorities for involved
     % vehicles. If no movement, then deadlock
-    nSteps = find(result.is_deadlock, 1, 'first');
+    arguments
+        experiment_result (1, 1) ExperimentResult
+    end
 
-    if isempty(nSteps)
-        t = result.t_total;
-        nSteps = result.nSteps;
+    n_steps = find(is_deadlock(experiment_result), 1, 'first');
+
+    if isempty(n_steps)
+        t = experiment_result.t_total;
+        n_steps = experiment_result.n_steps;
     else
-        t = nSteps * result.options.dt_seconds;
+        t = n_steps * experiment_result.options.dt_seconds;
     end
 
 end
