8d381e93f350081a100b24445318927a29983e10
Johannes Rolf <johannes.rolf@rwth-aachen.de>
2023-11-15 11:42:20 +0100

    Enforce naming convention in sample_reference_trajectory by renaming TrajectoryIndex to the more meaningful name point_index

--- hlc/controller/common/sample_reference_trajectory.m
+++ hlc/controller/common/sample_reference_trajectory.m
@@ -21,7 +21,7 @@ function reference_trajectory_struct = sample_reference_trajectory(n_samples, re
     reference_trajectory_struct.path = zeros(n_samples, 2);
     reference_trajectory_struct.points_index = zeros(n_samples, 1);
 
-    [~, ~, xp, yp, ~, ~, TrajectoryIndex] = get_arc_distance_to_endpoint( ...
+    [~, ~, xp, yp, ~, ~, point_index] = get_arc_distance_to_endpoint( ...
         x_current, ...
         y_current, ...
         reference_path(:, 1), ...
@@ -34,30 +34,30 @@ function reference_trajectory_struct = sample_reference_trajectory(n_samples, re
     % If the first and the last Point of the reference path are the same(here within a small enough distance),
     % we think the reference is in a loop.
     is_loop = norm((reference_path(1, :) - reference_path(end, :)), 2) < 1e-8;
-    is_vehicle_at_end = TrajectoryIndex == nLinePieces;
-    TrajectoryIndexLast = TrajectoryIndex - 1;
+    is_vehicle_at_end = point_index == nLinePieces;
+    point_index_last = point_index - 1;
 
     % If the reference_path is in a loop and the refPoint is the last one, change
-    % the TrajectoryIndex to 1
+    % the point_index to 1
     if is_loop && is_vehicle_at_end
-        TrajectoryIndex = 1;
+        point_index = 1;
     end
 
     for i = 1:n_samples
-        remainingLength = norm(current_reference_point - reference_path(TrajectoryIndex, :), 2);
+        remainingLength = norm(current_reference_point - reference_path(point_index, :), 2);
 
-        if remainingLength > step_distances(i) || TrajectoryIndex == nLinePieces
+        if remainingLength > step_distances(i) || point_index == nLinePieces
 
             % lanelets have overlapping points, which cannot be used to normalize
-            while (reference_path(TrajectoryIndex, 1) == reference_path(TrajectoryIndexLast, 1) && reference_path(TrajectoryIndex, 2) == reference_path(TrajectoryIndexLast, 2) && TrajectoryIndexLast > 1)
-                TrajectoryIndexLast = TrajectoryIndexLast - 1;
+            while (reference_path(point_index, 1) == reference_path(point_index_last, 1) && reference_path(point_index, 2) == reference_path(point_index_last, 2) && point_index_last > 1)
+                point_index_last = point_index_last - 1;
             end
 
             %normalize is used to construct a unit vector wrt the line segment
             %if the remaining lenght is larger than step size, then make a
             %step further first until the remaining length is less than the
             %step size
-            current_reference_point = current_reference_point + step_distances(i) * normalize(reference_path(TrajectoryIndex, :) - reference_path(TrajectoryIndexLast, :));
+            current_reference_point = current_reference_point + step_distances(i) * normalize(reference_path(point_index, :) - reference_path(point_index_last, :));
 
         else
 
@@ -68,27 +68,27 @@ function reference_trajectory_struct = sample_reference_trajectory(n_samples, re
                 % first refPoint after one step_distances is found
 
                 reflength = remainingLength;
-                current_reference_point = reference_path(TrajectoryIndex, :);
+                current_reference_point = reference_path(point_index, :);
 
-                TrajectoryIndexLast = TrajectoryIndex;
-                TrajectoryIndex = min(TrajectoryIndex + 1, nLinePieces);
+                point_index_last = point_index;
+                point_index = min(point_index + 1, nLinePieces);
 
-                is_vehicle_at_end = TrajectoryIndex == nLinePieces;
+                is_vehicle_at_end = point_index == nLinePieces;
 
                 if (is_loop && is_vehicle_at_end)
-                    TrajectoryIndex = 1;
+                    point_index = 1;
                 end
 
-                remainingLength = remainingLength + norm(current_reference_point - reference_path(TrajectoryIndex, :), 2);
+                remainingLength = remainingLength + norm(current_reference_point - reference_path(point_index, :), 2);
             end
 
-            current_reference_point = current_reference_point + (step_distances(i) - reflength) * normalize(reference_path(TrajectoryIndex, :) - reference_path(TrajectoryIndexLast, :));
-            %             disp(['trajectoryIndex is :',num2str(TrajectoryIndex)])
+            current_reference_point = current_reference_point + (step_distances(i) - reflength) * normalize(reference_path(point_index, :) - reference_path(point_index_last, :));
+            %             disp(['point_index is :',num2str(point_index)])
         end
 
         % record step
         reference_trajectory_struct.path(i, :) = current_reference_point;
-        reference_trajectory_struct.points_index(i, :) = TrajectoryIndex;
+        reference_trajectory_struct.points_index(i, :) = point_index;
 
     end
 
