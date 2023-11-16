09471f39b837b847f43dedcacbbb329d02f64540
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-11-16 20:25:07 +0100

    refactor: rename ID to i_node

--- hlc/optimizer/graph_search/tree/Tree.m
+++ hlc/optimizer/graph_search/tree/Tree.m
@@ -6,28 +6,27 @@ classdef Tree < handle
 
     methods (Abstract)
 
-        get_node(obj, ID)
+        get_node(obj, i_node)
         size(obj)
 
         number_of_vehicles(obj)
 
-        get_x(obj, i_vehicle, ID)
-        get_y(obj, i_vehicle, ID)
-        get_yaw(obj, i_vehicle, ID)
-        get_trim(obj, i_vehicle, ID)
+        get_x(obj, i_vehicle, i_node)
+        get_y(obj, i_vehicle, i_node)
+        get_yaw(obj, i_vehicle, i_node)
+        get_trim(obj, i_vehicle, i_node)
 
     end
 
     methods
 
-        function ID = get_parent(obj, ID)
-            %% GET_PARENT  Return the parent of the given node.
-            ID = obj.parent(ID);
+        function i_node = get_parent(obj, i_node)
+            i_node = obj.parent(i_node);
         end
 
-        function result = path_to_root(obj, ID)
-            %% PATH_TO_ROOT  Path from node ID to the Tree root.
-            result = ID;
+        function result = path_to_root(obj, i_node)
+            %% PATH_TO_ROOT  Path from node i_node to the Tree root.
+            result = i_node;
 
             while result(end) ~= 1
                 result(end + 1) = obj.parent(result(end)); %#ok<AGROW>
