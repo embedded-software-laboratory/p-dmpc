e8065152b0a07b022cac3ec5485ccac12a67366e
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-11-17 15:01:32 +0100

    [prioritizer] remove unused function get_CL_list

--- hlc/controller/prioritized/priority/Prioritizer.m
+++ hlc/controller/prioritized/priority/Prioritizer.m
@@ -36,45 +36,6 @@ classdef (Abstract) Prioritizer < handle
 
     methods
 
-        function CL_list = get_CL_list(obj, CL_based_hierarchy)
-            % Assign the computation level of each vehicle
-            %
-            % INPUT:
-            %   CL_based_hierarchy: a struct contains in which computation
-            %   level each vehicle is
-            %
-            % OUTPUT:
-            %   priority_list: values of vehicles' priorities, smaller
-            %   values indicate higher priorities
-
-            nVeh = length([CL_based_hierarchy.members]);
-            CL_list = zeros(1, nVeh);
-
-            if obj.is_assign_unique_priority
-                % each vehicle has unique priority
-                prio = 1;
-
-                for level_i = 1:length(CL_based_hierarchy)
-                    vehs_in_level_i = CL_based_hierarchy(level_i).members; % vehicles in the selected computation level
-
-                    for veh_i = vehs_in_level_i
-                        CL_list(veh_i) = prio;
-                        prio = prio + 1;
-                    end
-
-                end
-
-            else
-                % vehicles in the same computation level have the same priority
-                for level_i = 1:length(CL_based_hierarchy)
-                    vehs_in_level_i = CL_based_hierarchy(level_i).members;
-                    CL_list(vehs_in_level_i) = level_i;
-                end
-
-            end
-
-        end
-
         function priority_list = get_priority_list(~, weighted_coupling)
             [isValid, L] = kahn(weighted_coupling);
             priority_list = zeros(1, size(L, 2));
