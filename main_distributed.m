f9163ed676dfac69ea8e04c823139aaca9ead4a2
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-11-22 18:39:21 +0100

    [main] fix matlab code quality warning

--- main_distributed.m
+++ main_distributed.m
@@ -2,8 +2,8 @@ function [experiment_result, scenario] = main_distributed(vehicle_id)
 
     %startup();
 
-    if verLessThan('matlab', '9.12')
-        warning("Code is developed in MATLAB 2022a, prepare for backward incompatibilities.")
+    if isMATLABReleaseOlderThan('R2023b')
+        warning("Code is developed in MATLAB R2023b, prepare for backward incompatibilities.")
     end
 
     % read config from disk
