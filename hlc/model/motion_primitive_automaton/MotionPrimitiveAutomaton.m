673c1c5efd3ec8963ad99420df1c3f815cfe8e4b
Patrick Scheffe <scheffe@embedded.rwth-aachen.de>
2023-11-24 15:36:41 +0100

    [mpa] catch error when MPA file is not readable

--- hlc/model/motion_primitive_automaton/MotionPrimitiveAutomaton.m
+++ hlc/model/motion_primitive_automaton/MotionPrimitiveAutomaton.m
@@ -66,15 +66,21 @@ classdef MotionPrimitiveAutomaton
             mpa_full_path = fullfile(folder_target, mpa_instance_name);
 
             % if the needed MPA is already exist in the library, simply load
-            % it, otherwise it will be calculated and saved to the library.
-            % Note: if MPA properties are changed, then reload all MPAs!
-            if isfile(mpa_full_path) && options.is_load_mpa && nargout == 1
-                % if number of function output arguments is not one, do not load offline MPA as the second output is not available offline
-                fprintf('Loading MPA... ');
-                load(mpa_full_path, "mpa");
-                obj = mpa;
-                fprintf('done.\n')
-                return
+            % it, otherwise it will be built and saved to the library.
+            % Note: if MPA properties are changed, then rebuild all MPAs!
+
+            if options.is_load_mpa && isfile(mpa_full_path)
+
+                try
+                    fprintf('Loading MPA... ');
+                    load(mpa_full_path, "mpa");
+                    obj = mpa;
+                    fprintf('done.\n')
+                    return
+                catch
+                    fprintf('could not load, building new MPA.\n')
+                end
+
             end
 
             obj.recursive_feasibility = options.recursive_feasibility;
@@ -446,13 +452,11 @@ classdef MotionPrimitiveAutomaton
             threshold_Hp = 10;
 
             if Hp > threshold_Hp
-                disp(['Computing reachable sets now...' newline ...
-                          'Since the prediction horizon is ' num2str(Hp) ' (more than ' num2str(threshold_Hp) '), it may take several minutes.'])
+                fprintf(['Computing reachable sets now...' newline ...
+                             'Since the prediction horizon is ' num2str(Hp) ' (more than ' num2str(threshold_Hp) '), it may take several minutes.'])
             else
-                disp('Computing local offline reachable sets now...')
+                fprintf('Computing local offline reachable sets now...')
             end
-
-            disp('Note this only needs to be done once as later they will be saved to motion-primitive-automaton library.')
 
             Hp_half = ceil(Hp / 2); % for time step greater then half of the prediction horizon, dynamic programming is used to save computation time
 
@@ -677,7 +681,7 @@ classdef MotionPrimitiveAutomaton
 
             duration_computation = toc(offline_computation_start);
             textprogressbar('done');
-            disp(['Finished in ' num2str(duration_computation) ' seconds.'])
+            fprintf('done (%.2f s).\n', duration_computation);
 
         end
 
