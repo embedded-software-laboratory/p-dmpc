662bae0bb6361e1931977460be583136ea1dbf70
Scheffe <scheffe@embedded.rwth-aachen.de>
2023-12-22 12:09:45 +0100

    [nuc] run hlc in cpmlab on nucs

--- nuc_control/reboot_remote.sh
+++ nuc_control/reboot_remote.sh
@@ -8,10 +8,9 @@ VEHICLE_IDS=("$@")
 # list of working nucs to be used for execution
 WORKING_NUCS=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20)
 
-# get numbers of vehicles
-N_VEH="$@"
+# get numbers of nucs
+N_NUCS=${#WORKING_NUCS[@]}
 
-# stop tmux sessions on all used hlcs
-for (( i=0; i<$N_VEH; i++ )); do
-    sshpass -e ssh -t guest@192.168.1.2${WORKING_NUCS[$i]} 'tmux kill-session -t "matlab-hlc"'
+for (( i=0; i<$N_NUCS; i++ )); do
+    sshpass -e ssh -t guest@192.168.1.2${WORKING_NUCS[$i]} 'sudo reboot'
 done
