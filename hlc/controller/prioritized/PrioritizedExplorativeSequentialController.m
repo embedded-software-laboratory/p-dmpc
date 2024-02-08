b49b6c0e71d3e58e62a4993071312b4a59a2ec1a
Scheffe <scheffe@embedded.rwth-aachen.de>
2024-02-08 12:31:23 +0100

    always keep priorities if feasible

--- hlc/controller/prioritized/PrioritizedExplorativeSequentialController.m
+++ hlc/controller/prioritized/PrioritizedExplorativeSequentialController.m
@@ -45,6 +45,7 @@ classdef PrioritizedExplorativeSequentialController < PrioritizedSequentialContr
             for hlc = obj.hlcs
                 hlc.receive_solution_cost();
                 hlc.choose_solution();
+                hlc.remember_priorities();
             end
 
         end
