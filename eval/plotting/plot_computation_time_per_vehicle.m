56d785b394b82ea061f9c64407430a80bb0d04b1
Scheffe <scheffe@embedded.rwth-aachen.de>
2024-02-27 12:01:45 +0100

    [eval] rename runtime plot to computation time plot

--- eval/plotting/plot_computation_time_per_vehicle.m
+++ eval/plotting/plot_computation_time_per_vehicle.m
@@ -11,12 +11,12 @@ function plot_computation_time_per_vehicle(experiment_result, optional)
     computation_time = computation_time';
 
     bar(1:size(computation_time, 1), computation_time);
-    xlabel('Time step $k$', Interpreter = 'LaTex');
+    xlabel('Time step', Interpreter = 'LaTex');
     ylabel('Computation Time [s]', Interpreter = 'LaTex');
 
     if optional.do_export
         results_folder = FileNameConstructor.experiment_result_folder_path(experiment_result.options);
-        filepath = fullfile(results_folder, sprintf('computation_time_result_%d.pdf', i));
+        filepath = fullfile(results_folder, 'computation_time_per_vehicle.pdf');
         set_figure_properties(optional.fig, ExportFigConfig.paper('paperheight', 12))
         export_fig(optional.fig, filepath);
         if (~optional.fig.Visible); close(optional.fig); end
--- tests/systemtests/systemtests.m
+++ tests/systemtests/systemtests.m
@@ -159,9 +159,9 @@ classdef systemtests < matlab.unittest.TestCase
             % Test plotting of runtime of one timestep within one experiment
             result_one_experiment_normalized = normalize_timing_results(experiment_result_3);
 
-            plot_runtime_for_step(result_one_experiment_normalized, 5, do_export = true);
+            plot_computation_time_for_step(result_one_experiment_normalized, 5, do_export = true);
             testCase.verifyTrue(true);
-            plot_runtime_for_step(result_one_experiment_normalized, 5, do_export = false);
+            plot_computation_time_for_step(result_one_experiment_normalized, 5, do_export = false);
             testCase.verifyTrue(true);
 
             close all;
