e7cda977a2733104cce5e8a56f9322b2ed10ba24
Scheffe <scheffe@embedded.rwth-aachen.de>
2024-03-12 14:40:39 +0100

    [mcts] fix removing edge to node without children

--- hlc/optimizer/graph_search/MonteCarloTreeSearch.m
+++ hlc/optimizer/graph_search/MonteCarloTreeSearch.m
@@ -35,8 +35,9 @@ classdef MonteCarloTreeSearch < OptimizerInterface
             %
             assert(iter.amount == 1);
             Hp = size(iter.v_ref, 2);
-            n_expansions_max = 1000;
-            obj.random_numbers = rand(obj.rand_stream, 1, n_expansions_max + Hp);
+            n_expansions_max = 200;
+            % overapproximate number of traversals
+            obj.random_numbers = rand(obj.rand_stream, 1, Hp*n_expansions_max);
             n_successor_trims_max = mpa.maximum_branching_factor();
             % initialize variable to store control results
             info = ControlResultsInfo(iter.amount, Hp);
@@ -64,6 +65,7 @@ classdef MonteCarloTreeSearch < OptimizerInterface
 
             iVeh = 1;
             n_expansions = 0;
+            n_traversals = 0;
             is_finished = false;
 
             reference_trajectory_points = squeeze(iter.reference_trajectory_points(iVeh, :, 1:2))';
@@ -77,7 +79,7 @@ classdef MonteCarloTreeSearch < OptimizerInterface
 
                 for i_step = 1:Hp
                     is_valid = false;
-                    n_expansions = n_expansions + 1;
+                    n_traversals = n_traversals + 1;
 
                     % Select node to expand randomly
                     trim_positions = find(children(:, node_id));
@@ -85,13 +87,13 @@ classdef MonteCarloTreeSearch < OptimizerInterface
 
                     if n_trims ~= 0
                         % choose successor trim randomly
-                        child_position = trim_positions(ceil(obj.random_numbers(n_expansions) * n_trims));
+                        child_position = trim_positions(ceil(obj.random_numbers(n_traversals) * n_trims));
                     else
 
                         if node_id ~= 1
                             % remove edge to node without children
                             parent_id = parents(node_id);
-                            children(children(parent_id) == node_id, parent_id) = 0;
+                            children(children(:, parent_id) == node_id, parent_id) = 0;
                             break
                         else
                             is_finished = true;
@@ -128,6 +130,7 @@ classdef MonteCarloTreeSearch < OptimizerInterface
                         node_id = children(child_position, node_id);
                         continue
                     end
+                    n_expansions = n_expansions + 1;
 
                     node_parent = node_id;
 
