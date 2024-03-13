01b0355a26d0b4a0599dfdaf7bfd72d9900d6585
Patrick Scheffe <scheffe@embedded.rwth-aachen.de>
2024-03-13 16:52:08 +0100

    [couple] fixup coupling with InterX

--- hlc/controller/common/couple/ReachableSetCoupler.m
+++ hlc/controller/common/couple/ReachableSetCoupler.m
@@ -11,12 +11,28 @@ classdef ReachableSetCoupler < Coupler
             adjacency = zeros(options.amount, options.amount);
 
             for veh_i = 1:(options.amount - 1)
+                x_i = reachable_sets{veh_i, end}(1, :);
+                y_i = reachable_sets{veh_i, end}(2, :);
 
                 for veh_j = (veh_i + 1):options.amount
 
                     if InterX(reachable_sets{veh_i, end}, reachable_sets{veh_j, end})
                         adjacency(veh_i, veh_j) = 1;
                         adjacency(veh_j, veh_i) = 1;
+                    else
+                        % check if two points are equal -- colinear lines are
+                        % missed by InterX
+                        x_j = reachable_sets{veh_j, end}(1, :);
+                        y_j = reachable_sets{veh_j, end}(2, :);
+
+                        [x_mat_i, x_mat_j] = meshgrid(x_i, x_j);
+                        [y_mat_i, y_mat_j] = meshgrid(y_i, y_j);
+
+                        if any(x_mat_i == x_mat_j & y_mat_i == y_mat_j, 'all')
+                            adjacency(veh_i, veh_j) = 1;
+                            adjacency(veh_j, veh_i) = 1;
+                        end
+
                     end
 
                 end
