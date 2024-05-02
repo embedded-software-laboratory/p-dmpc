e3a5d03137a6ce1c63ed650f3d36a14b2fee3788
Scheffe <scheffe@embedded.rwth-aachen.de>
2024-05-02 16:56:42 +0200

    [priority] reorder enum

--- config/enums/PriorityStrategies.m
+++ config/enums/PriorityStrategies.m
@@ -1,12 +1,12 @@
 classdef PriorityStrategies
 
     enumeration
-        coloring_priority
         constant_priority
         random_priority
         FCA_priority
-        optimal_priority
+        coloring_priority
         explorative_priority
+        optimal_priority
     end
 
 end
