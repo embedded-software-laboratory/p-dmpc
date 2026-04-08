340c4a00834783cfd34cdda9153e46128d5155d6
Patrick Scheffe <patrick.scheffe@rwth-aachen.de>
2026-04-08 13:17:13 +0200

    [eval] rework time per step plot
    groups vehicles by subgraph
    adds corresponding subgraph to plot

--- eval/3-plotting/plot_computation_time_for_step.m
+++ eval/3-plotting/plot_computation_time_for_step.m
@@ -18,9 +18,62 @@ function plot_computation_time_for_step(experiment_result, k, optional)
         error('The graph is currently only supported for results of prioritized, distributed execution.');
     end
 
+    % find computation order for sorted plotting
+    i_step = k;
+    n_vehicles = experiment_result.n_hlc;
+
     all_field_names = fieldnames(experiment_result.timing(1));
     optimize_field_names_indices = ~cellfun(@isempty, regexp(all_field_names, '^optimize\w+'));
     optimize_field_names = string(all_field_names(optimize_field_names_indices))';
+
+    % n_vehicles x n_steps x n_permutations
+    optimize_start = zeros([n_vehicles, 1, numel(optimize_field_names)]); % 3d because of similarity from add_optimize_time
+    optimize_duration = zeros(size(optimize_start));
+
+    for i_field = 1:numel(optimize_field_names)
+        optimize_timing = vertcat(experiment_result.timing.(optimize_field_names(i_field)));
+        optimize_start(:, :, i_field) = optimize_timing(1:2:end, i_step);
+        optimize_duration(:, :, i_field) = optimize_timing(2:2:end, i_step);
+    end
+
+    % Create directed_coupling_sequential for each optimize permutation
+    % Current number of levels: nnz of optimize field duration
+    permutations = squeeze(optimize_duration(1, 1, :) ~= 0);
+    n_permutations = nnz(permutations);
+    levels_per_vehicle = zeros(n_vehicles, n_permutations);
+    directed_coupling_sequential = false([n_vehicles, n_vehicles, n_permutations]);
+
+    if n_permutations == 1
+        directed_coupling_sequential(:, :, 1) = experiment_result.iteration_data(i_step).directed_coupling_sequential;
+        levels_per_vehicle(:, 1) = kahn( ...
+            directed_coupling_sequential(:, :, 1) ...
+        );
+
+    else
+
+        % latin_square: n_vehicles x n_permutations
+        % columns are levels
+        % entry in row indicates which permutation the vehicle solved at
+        % level
+        [~, latin_square] = sort(squeeze(optimize_start(:, 1, permutations)), 2);
+
+        adjacency_sequential = experiment_result.iteration_data(i_step).directed_coupling_sequential ...
+            + experiment_result.iteration_data(i_step).directed_coupling_sequential';
+
+        for i_perm = 1:n_permutations
+            % level_matrix: n_vehicles x n_levels
+            level_matrix = latin_square == i_perm;
+            priorities = level_matrix * (1:n_permutations)';
+            directed_coupling_sequential(:, :, i_perm) = Prioritizer.directed_coupling_from_priorities( ...
+                adjacency_sequential, ...
+                priorities ...
+            );
+
+            levels_per_vehicle(:, i_perm) = level_matrix * (1:size(latin_square, 2))';
+        end
+
+    end
+
     field_names = [ ...
                        "measure", ...
                        "analyze_reachability", ...
@@ -37,41 +90,107 @@ function plot_computation_time_for_step(experiment_result, k, optional)
     measure_start_points = measure_timings(1:2:end, :);
     t0 = max(measure_start_points(:, k));
 
-    directed_coupling = experiment_result.iteration_data(k).directed_coupling_sequential;
-    groups = conncomp(digraph(directed_coupling), Type = 'weak');
-    % L = kahn(directed_coupling);
+    groups = conncomp(digraph(directed_coupling_sequential(:, :, 1)), Type = 'weak');
 
-    [~, vehicle_id_order] = sort(groups);
+    [n_members, idx_group] = groupcounts(groups');
 
-    for field_i = 1:length(field_names)
-        field_name = field_names(field_i);
-        time_to_draw = nan(2, options.amount);
+    [~, groups_by_members] = sort(n_members, "descend");
 
-        for veh_i = 1:options.amount
+    group_order = idx_group(groups_by_members);
 
-            timings = experiment_result.timing(veh_i);
+    tl = tiledlayout(options.amount, 2 * n_permutations, TileSpacing = "tight", Padding = "tight");
 
-            t_start = timings.(field_name)(1, k) - t0; % Normalize (see above)
-            duration = timings.(field_name)(2, k);
-            time_to_draw(:, vehicle_id_order == veh_i) = ...
-                [t_start, t_start + duration] * 10^3; % Scale to ms
+    maximum_time = 0;
+
+    export_fig_config = ExportFigConfig.paper();
+
+    for i_group = group_order'
+
+        vehicle_list = 1:options.amount;
+        vehicle_ids_in_group = vehicle_list(groups == i_group);
+
+        nexttile([numel(vehicle_ids_in_group) n_permutations]);
+
+        for field_i = 1:length(field_names)
+            field_name = field_names(field_i);
+            time_to_draw = nan(2, numel(vehicle_ids_in_group));
+
+            for i_vehicle = 1:numel(vehicle_ids_in_group)
+
+                veh_i = vehicle_ids_in_group(i_vehicle);
+                timings = experiment_result.timing(veh_i);
+
+                t_start = timings.(field_name)(1, k) - t0; % Normalize (see above)
+                duration = timings.(field_name)(2, k);
+
+                [~, id_order] = sort(levels_per_vehicle(vehicle_ids_in_group, 1));
+
+                time_to_draw(:, id_order == i_vehicle) = ...
+                    [t_start, t_start + duration] * 10^3; % Scale to ms
+            end
+
+            plot(time_to_draw, [1:numel(vehicle_ids_in_group); 1:numel(vehicle_ids_in_group)], 'SeriesIndex', field_i, ...
+                LineWidth = 5, Tag = 'box_as_line');
+
+            hold on;
+
         end
 
-        plot_handle(:, field_i) = plot(time_to_draw, [1:options.amount; 1:options.amount], 'SeriesIndex', field_i, ...
-            LineWidth = 5, Tag = 'box_as_line'); %#ok<AGROW>
-        hold on;
+        maximum_time = max([time_to_draw(2, :), maximum_time]);
+
+        yticks(1:numel(vehicle_ids_in_group));
+        yticklabels(vehicle_ids_in_group(id_order))
+
+        ylim([1 - 0.5, numel(vehicle_ids_in_group) + 0.5]);
+
+        color_order = rwth_color_order();
+
+        for i_permutation = 1:n_permutations
+
+            n_colors = size(color_order, 1);
+            i_color = mod(6 + i_permutation - 1, n_colors) + 1;
+
+            nexttile([numel(vehicle_ids_in_group) 1]);
+            sg = plot( ...
+                digraph( ...
+                directed_coupling_sequential( ...
+                vehicle_list(groups == i_group), ...
+                vehicle_list(groups == i_group), ...
+                i_permutation ...
+            ) ...
+            ), ...
+                NodeLabel = vehicle_list(vehicle_list(groups == i_group)), ...
+                NodeColor = color_order(i_color, :), ...
+                EdgeColor = color_order(i_color, :), ...
+                MarkerSize = 2, ...
+                ArrowSize = 4, ...
+                NodeFontSize = export_fig_config.fontsize, ...
+                NodeFontName = export_fig_config.fontname ...
+            );
+            sg.YData = levels_per_vehicle(vehicle_list(groups == i_group), i_permutation)';
+            set(gca, 'YDir', 'reverse');
+        end
+
     end
 
-    legend(plot_handle(1, :), strrep(cellstr(field_names), '_', ' '), Location = 'best');
-    xlabel('Time [ms]');
-    xlim([-5, ceil(max(time_to_draw(2, :)))] + 5);
-    ylabel('Vehicle ID');
-    yticks(1:options.amount);
-    yticklabels(vehicle_id_order)
+    % legend(plot_handle(1, :), strrep(cellstr(field_names), '_', ' '), Location = "bestoutside");
+    % xlabel('Time [ms]');
+    % xlim([-5, ceil(max(time_to_draw(2, :)))] + 5);
+    % ylabel('Vehicle ID');
 
-    ylim([1 - 0.2, options.amount + 0.2]);
+    % ylim([1 - 0.2, options.amount + 0.2]);
+    for ax = tl.Children(1 + n_permutations:1 + n_permutations:end)'
+        ax.XLim = [0, 5 + ceil(maximum_time)];
+        ax.XGrid = 'on';
 
-    set_figure_properties(optional.fig, ExportFigConfig.document());
+        if ax ~= tl.Children(n_permutations + 1)
+            ax.XTickLabel = [];
+        end
+
+        ax.YDir = 'reverse';
+    end
+
+    % set_figure_properties(optional.fig, ExportFigConfig.document());
 
     if ~hold_before
         hold off;
